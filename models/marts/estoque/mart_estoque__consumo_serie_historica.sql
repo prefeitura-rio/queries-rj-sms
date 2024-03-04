e5c6e9b7482a681f03a437c1f337e31777aee1f2
ThiagoTrabach <thiago.trabach@gmail.com>
2024-03-04 17:01:51 -0300

    fix cardinality issue

--- models/marts/estoque/mart_estoque__consumo_serie_historica.sql
+++ models/marts/estoque/mart_estoque__consumo_serie_historica.sql
@@ -27,7 +27,7 @@ estabelecimento as (select * from {{ ref('dim_estabelecimento') }}),
 
 material_dimensao as (select * from {{ ref('dim_material') }}),
 
-material_alternativo as (select distinct id_cnes, id_material, material_descricao from {{ ref('fct_estoque_posicao') }})
+material_alternativo as (select distinct id_cnes, id_material, material_descricao from {{ ref('mart_estoque__posicao_atual') }})
 
 select 
     d.*,
@@ -43,5 +43,3 @@ left join estabelecimento as e on d.id_cnes = e.id_cnes
 left join material_dimensao as md on d.id_material = md.id_material
 left join material_alternativo as ma on d.id_material = ma.id_material and d.id_cnes = ma.id_cnes
 order by d.id_cnes, d.id_material, d.row_num
-
-
