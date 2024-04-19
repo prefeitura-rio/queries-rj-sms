3500129b87f64f11460de938059b37ecf1ade758
ThiagoTrabach <thiago.trabach@gmail.com>
2024-04-19 16:30:50 -0300

    Update material hierarchy column names in _sheets_schema.yml and raw_sheets__material_mestre.sql

--- models/marts/core/dimensions/dim_material.sql
+++ models/marts/core/dimensions/dim_material.sql
@@ -5,50 +5,81 @@
 }}
 
 with
-    
-    material as (
-        select
-            concat(cd_grupo, cd_classe) as cd_grupo_classe,
-            concat(cd_grupo, cd_classe, cd_subclasse) as cd_grupo_classe_subclasse,
-            *
-        from {{ source("sigma", "material") }}
-    ),
-    grupo as (select cd_grupo, ds_grupo from {{ source("sigma", "grupo") }}),
-    classe as (
-        select concat(cd_grupo, cd_classe) as cd_grupo_classe, ds_classe
-        from {{ source("sigma", "classe") }}
-    ),
-    subclasse as (
-        select
-            concat(cd_grupo, cd_classe, cd_subclasse) as cd_grupo_classe_subclasse,
-            ds_subclasse
-        from {{ source("sigma", "subclasse") }}
-    )
 
-# TODO: remover hotfix
-, material_distinct as (select distinct * from material)
+    material_mestre as (select * from {{ ref("raw_sheets__material_mestre") }})
 
 select
     -- Primary Key
-    cd_material as id_material,
-
-    -- Foreign Keys
-    mat.cd_grupo as id_grupo,
-    mat.cd_classe as id_classe,
-    mat.cd_subclasse as id_subclasse,
+    id_material,
 
     -- Common columns
-    grupo.ds_grupo as hierarquia_n1_grupo,
-    classe.ds_classe as hierarquia_n2_classe,
-    subclasse.ds_subclasse as hierarquia_n3_subclasse,
-    if(mat.cd_grupo_classe = "6505", "Medicamento", "Insumo") as natureza,
-    concat(mat.nm_padronizado, " ", mat.nm_complementar_material) as nome,
-    mat.ds_detalhe_material as nome_complementar,
-    mat.unidade,
-    mat.st_status as status,
-    mat.remume
+    descricao,
+    concentracao,
+    forma_farmaceutica,
+    apresentacao,
+    trim(
+        concat(
+            descricao,
+            ", ",
+            coalesce(concentracao, " "),
+            " ",
+            coalesce(apresentacao, forma_farmaceutica, " ")
+        )
+    ) as nome,
+    hierarquia_n1_categoria,
+    hierarquia_n2_subcategoria,
+    controlado_indicador,
+    controlado_tipo,
+    remume_indicador,
+    remume_listagens,
+    remume_listagem_basico_indicador,
+    remume_listagem_uso_interno_indicador,
+    remume_listagem_hospitalar_indicador,
+    remume_listagem_antiseptico_indicador,
+    remume_listagem_estrategico_indicador,
+    remume_disponibilidades,
+    remume_disponibilidade_cms_indicador,
+    remume_disponibilidade_cf_indicador,
+    remume_disponibilidade_cse_indicador,
+    remume_disponibilidade_policlinica_indicador,
+    remume_disponibilidade_hospital_indicador,
+    remume_disponibilidade_maternidade_indicador,
+    remume_disponibilidade_caps_indicador,
+    remume_disponibilidade_upa_indicador,
+    remume_disponibilidade_cer_indicador,
+    remume_disponibilidade_unidades_especificas_lista,
+    cadastrado_sistema_vitacare_indicador
+from material_mestre
+order by
+    remume_indicador desc,
+    hierarquia_n1_categoria desc,
+    descricao asc,
+    concentracao asc,
+    forma_farmaceutica asc,
+    apresentacao asc
 
-from material_distinct as mat
-left join grupo using (cd_grupo)
-left join classe using (cd_grupo_classe)
-left join subclasse using (cd_grupo_classe_subclasse)
+
+-- select
+-- -- Primary Key
+-- cd_material as id_material,
+--
+-- -- Foreign Keys
+-- mat.cd_grupo as id_grupo,
+-- mat.cd_classe as id_classe,
+-- mat.cd_subclasse as id_subclasse,
+--
+-- -- Common columns
+-- grupo.ds_grupo as hierarquia_n1_grupo,
+-- classe.ds_classe as hierarquia_n2_classe,
+-- subclasse.ds_subclasse as hierarquia_n3_subclasse,
+-- if(mat.cd_grupo_classe = "6505", "Medicamento", "Insumo") as natureza,
+-- concat(mat.nm_padronizado, " ", mat.nm_complementar_material) as nome,
+-- mat.ds_detalhe_material as nome_complementar,
+-- mat.unidade,
+-- mat.st_status as status,
+-- mat.remume
+--
+-- from material_distinct as mat
+-- left join grupo using (cd_grupo)
+-- left join classe using (cd_grupo_classe)
+-- left join subclasse using (cd_grupo_classe_subclasse)
