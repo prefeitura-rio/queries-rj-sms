bed1f9cf96aa395c67ec3bfbe69eed2a00d1dd10
ThiagoTrabach <thiago.trabach@gmail.com>
2024-04-23 12:29:06 -0300

    Refactor dim_material.sql by removing unused code

--- models/marts/core/dimensions/dim_material.sql
+++ models/marts/core/dimensions/dim_material.sql
@@ -57,29 +57,3 @@ order by
     concentracao asc,
     forma_farmaceutica asc,
     apresentacao asc
-
-
--- select
--- -- Primary Key
--- cd_material as id_material,
---
--- -- Foreign Keys
--- mat.cd_grupo as id_grupo,
--- mat.cd_classe as id_classe,
--- mat.cd_subclasse as id_subclasse,
---
--- -- Common columns
--- grupo.ds_grupo as hierarquia_n1_grupo,
--- classe.ds_classe as hierarquia_n2_classe,
--- subclasse.ds_subclasse as hierarquia_n3_subclasse,
--- if(mat.cd_grupo_classe = "6505", "Medicamento", "Insumo") as natureza,
--- concat(mat.nm_padronizado, " ", mat.nm_complementar_material) as nome,
--- mat.ds_detalhe_material as nome_complementar,
--- mat.unidade,
--- mat.st_status as status,
--- mat.remume
---
--- from material_distinct as mat
--- left join grupo using (cd_grupo)
--- left join classe using (cd_grupo_classe)
--- left join subclasse using (cd_grupo_classe_subclasse)
