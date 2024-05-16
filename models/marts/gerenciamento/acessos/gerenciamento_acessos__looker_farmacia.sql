7489630c0eacb0d65602374b320de66c48907a39
ThiagoTrabach <thiago.trabach@gmail.com>
2024-05-16 11:33:06 -0300

    feat: Update access table in mart_estoque__consumo_serie_historica_looker_row_level_access.sql and mart_estoque__posicao_atual_agregado_looker_row_level_access.sql

--- models/marts/gerenciamento/acessos/gerenciamento_acessos__looker_farmacia.sql
+++ models/marts/gerenciamento/acessos/gerenciamento_acessos__looker_farmacia.sql
@@ -6,9 +6,67 @@
 }}
 
 with
-    source as (
+    acessos as (
         select * from {{ ref("raw_sheets__gerenciamento_acesso_looker_farmacia") }}
-    )
+    ),
 
+    estabelecimentos as (
+        select id_cnes, area_programatica, tipo_sms_agrupado, nome_limpo
+        from {{ ref("dim_estabelecimento") }}
+    ),
+
+    relacao_acesso_estabelecimento as (
+        select
+            a.email,
+            a.cpf,
+            a.nome,
+            a.sub_secretaria__oss,
+            a.area,
+            a.cargo,
+            a.status_do_acesso,
+            a.escopo_bi,
+            a.acesso_relacao_aps,
+            a.acesso_relacao_estabelecimentos,
+            e.id_cnes,
+            e.area_programatica,
+            e.tipo_sms_agrupado,
+            e.nome_limpo,
+        from acessos as a
+        cross join estabelecimentos as e
+        where
+            e.area_programatica in unnest(a.acesso_relacao_aps)
+            and (
+                e.id_cnes in unnest(a.acesso_relacao_estabelecimentos)
+                or e.tipo_sms_agrupado in unnest(a.acesso_relacao_estabelecimentos)
+            )
+    ),
+
+    relacao_acesso_tpc as (
+        select
+            email,
+            cpf,
+            nome,
+            sub_secretaria__oss,
+            area,
+            cargo,
+            status_do_acesso,
+            escopo_bi,
+            acesso_relacao_aps,
+            acesso_relacao_estabelecimentos,
+            'tpc' as id_cnes,
+            'TPC' as area_programatica,
+            'TPC' as tipo_sms_agrupado,
+            'TPC' as nome_limpo,
+        from acessos
+        where 'TPC' in unnest(acesso_relacao_estabelecimentos)
+    ),
+
+    relacao_union as (
+        select * from relacao_acesso_tpc
+        union all
+        select * from relacao_acesso_estabelecimento
+    )
 select *
-from source
+from relacao_union
+where status_do_acesso = "ativo"
+order by email, area_programatica, tipo_sms_agrupado, nome_limpo
--- models/raw/sheets/raw_sheets__gerenciamento_acesso_looker_farmacia.sql
+++ models/raw/sheets/raw_sheets__gerenciamento_acesso_looker_farmacia.sql
@@ -28,13 +28,24 @@ with
             parse_date(
                 '%d/%m/%Y', ultima_renovacao_do_acesso
             ) as ultima_renovacao_do_acesso,
-            escopo_bi,
+            split(substr(escopo_bi, 1, length(escopo_bi)), ';') as escopo_bi,
             escopo_ap,
             coalesce(acesso_tpc, "nao") as acesso_tpc,
             coalesce(acesso_aps, "nao") as acesso_aps,
             coalesce(acesso_upas, "nao") as acesso_upas,
             coalesce(acesso_hospitais, "nao") as acesso_hospitais,
-            acesso_unidade_especifica
+            acesso_unidade_especifica,
+            split(
+                substr(acesso_relacao_aps, 1, length(acesso_relacao_aps)), ';'
+            ) as acesso_relacao_aps,
+            split(
+                substr(
+                    acesso_relacao_estabelecimentos,
+                    1,
+                    length(acesso_relacao_estabelecimentos)-1
+                ),
+                ';'
+            ) as acesso_relacao_estabelecimentos,
         from source
     )
 select *
