87dc6b2f9b7456039b781a7b8a18f36a470d53e6
ThiagoTrabach <thiago.trabach@gmail.com>
2024-06-11 11:16:38 -0300

    refactor: Update database and table names in _sih_sources.yml and raw_sih__indicadores_hospitalares.sql
    This commit updates the database and table names in the _sih_sources.yml and raw_sih__indicadores_hospitalares.sql files. The database name is changed from "rj-sms-dev" to "rj-sms" and the table name is changed from "indicadores_hospitalares_parquet" to "indicadores_hospitalares". This change aligns the names with the current configuration and improves consistency in the codebase.

--- models/raw/sih/raw_sih__indicadores_hospitalares.sql
+++ models/raw/sih/raw_sih__indicadores_hospitalares.sql
@@ -1,12 +1,12 @@
 {{
     config(
-        schema="brutos_sih_staging",
+        schema="brutos_sih",
         alias="indicadores_hospitalares",
     )
 }}
 
 with source as (
-      select * from {{ source('brutos_sih_staging', 'indicadores_hospitalares_parquet') }}
+      select * from {{ source('brutos_sih_staging', 'indicadores_hospitalares') }}
 ),
 renamed as (
     select
@@ -48,8 +48,8 @@ renamed as (
         val_tot,
         val_uti,
         us_tot,
-        parse_date('%Y%m%d', dt_inter) as dt_inter,
-        parse_date('%Y%m%d', dt_saida) as dt_saida,
+        dt_inter,
+        dt_saida,
         diag_princ,
         diag_secun,
         cobranca,
