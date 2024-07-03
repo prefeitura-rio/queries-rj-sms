265ccd41f5b866e0e3fd8c4c2a79824b3711e0dd
Pedro Marques <pedrovitor.marques@gmail.com>
2024-07-03 18:07:12 -0300

    [WIP] Optimizing process

--- models/raw/prontuario_vitai/raw_prontuario_vitai__paciente.sql
+++ models/raw/prontuario_vitai/raw_prontuario_vitai__paciente.sql
@@ -7,21 +7,15 @@
 
 with 
 recent_events as (
-    select
-        *,
-        row_number() over (partition by gid order by data_hora desc) as row_num
-    from
-        {{ ref("raw_prontuario_vitai__paciente_eventos_recentes") }}
+    select *, row_number() over (partition by gid order by data_hora desc) as row_num
+    from {{ ref("raw_prontuario_vitai__paciente_eventos_recentes") }}
 ), 
 latest_events as (
-    select
-        *
-    from
-        recent_events
-    where
-        row_num = 1
+    select *
+    from recent_events
+    where row_num = 1
 )
-select * 
+select *
 from latest_events
 {% if is_incremental() %}
 where data_hora >= (SELECT max(data_hora) FROM {{ this }})
--- models/raw/prontuario_vitai/raw_prontuario_vitai__paciente_eventos_recentes.sql
+++ models/raw/prontuario_vitai/raw_prontuario_vitai__paciente_eventos_recentes.sql
@@ -1,19 +1,46 @@
 {{ config(
     alias="paciente_eventos_recentes",
-    materialized='table',
+    materialized='incremental',
     partition_by={"field": "datalake__imported_at", "data_type": "timestamp"},
     partition_expiration_days=7,
     clustering_by=["gid"],
     tags=["vitai_db"]
 ) }}
 
-with recent_events as (
-    select
-        *
-    from
-        {{ ref("raw_prontuario_vitai__paciente_eventos") }}
-    where
-        datalake__imported_at >= timestamp_sub(current_timestamp(), interval 7 day)
-)
+{% set yesterday = (modules.datetime.date.today() - modules.datetime.timedelta(days=1)).isoformat() %}
 
-select * from recent_events
+select
+    safe_cast(gid as string) as gid,
+    safe_cast(racacor as string) as raca_cor,
+    safe_cast(nome_alternativo as string) as nome_alternativo,
+    safe_cast(estabelecimento_gid as string) as estabelecimento_gid,
+    safe_cast(complemento as string) as complemento,
+    safe_cast(cns as string) as cns,
+    safe_cast(datanascimento as date format "YYYY-MM-DD") as data_nascimento,
+    safe_cast(transex as string) as trans_genero,
+    safe_cast(tipologradouro as string) as tipo_logradouro,
+    safe_cast(nomelogradouro as string) as nome_logradouro,
+    safe_cast(uf as string) as uf,
+    safe_cast(nacionalidade as string) as nacionalidade,
+    safe_cast(ocupacaocbo as string) as ocupacao_cbo,
+    safe_cast(munipicio as string) as municipio,
+    safe_cast(telefone as string) as telefone,
+    safe_cast(nomemae as string) as nome_mae,
+    safe_cast(sexo as string) as sexo,
+    safe_cast(naturalidade as string) as naturalidade,
+    safe_cast(datahora as timestamp) as data_hora,
+    safe_cast(paisnascimento as string) as pais_nascimento,
+    safe_cast(bairro as string) as bairro,
+    safe_cast(dtobito as date format "YYYY-MM-DD") as data_obito,
+    safe_cast(numero as string) as numero,
+    safe_cast(idcidadao as string) as id_cidadao,
+    safe_cast(cpf as string) as cpf,
+    safe_cast(numero_prontuario as string) as numero_prontuario,
+    safe_cast(nome as string) as nome,
+    safe_cast(cliente as string) as cliente,
+    safe_cast(datalake__imported_at as timestamp) as datalake__imported_at,
+    safe_cast(data_particao as date format "YYYY-MM-DD") as data_particao
+from {{ source("brutos_prontuario_vitai_staging", "paciente_eventos") }}
+{% if is_incremental() %}
+where data_particao > '{{yesterday}}'
+{% endif %}
