e2c6da65df0b09f2039b2817e383dff3f997d479
ThiagoTrabach <thiago.trabach@gmail.com>
2024-08-07 06:11:16 -0300

    feat: Update column name in raw_datasus__cbo.sql
    The column name `_data_snapshot` has been changed to `created_at` in the `raw_datasus__cbo.sql` file. This update improves the clarity and consistency of the column naming convention.

--- models/raw/datasus/raw_datasus__cbo.sql
+++ models/raw/datasus/raw_datasus__cbo.sql
@@ -12,7 +12,7 @@ with
             safe_cast(cbo as string) as id_cbo,
             safe_cast(ds_cbo as string) as descricao,
             safe_cast(_data_carga as date format 'DD/MM/YYY') as data_carga,
-            safe_cast(_data_snapshot as date format 'DD/MM/YYY') as _data_snapshot,
+            safe_cast(_data_snapshot as date format 'DD/MM/YYY') as created_at,
         from source
     )
 select *
