89781a61de437869d6d5c1fa7b929e869e613d84
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-08 15:13:44 -0300

    fix: creating model merging historical and routine vitacare patient

--- models/raw/prontuario_vitacare/raw_prontuario_vitacare__paciente_historico.sql
+++ models/raw/prontuario_vitacare/raw_prontuario_vitacare__paciente_historico.sql
@@ -6,7 +6,10 @@
 }}
 
 select
-    -- Chaves
+    -- PK
+    safe_cast(null as string) as id,
+
+    -- Outras Chaves
     safe_cast(NULLIF(N_CPF, 'None') as string) as cpf,
     safe_cast(NULLIF(N_DNV, 'None') as string) as dnv,
     safe_cast(NULLIF(NIS, 'None') as string) as nis,
@@ -16,6 +19,7 @@ select
     safe_cast(NULLIF(NOME_DA_PESSOA_CADASTRADA, 'None') as string) as nome,
     safe_cast(NULLIF(NOME_SOCIAL_DA_PESSOA_CADASTRADA, 'None') as string) as nome_social,
     safe_cast(NULLIF(NOME_DA_MAE_PESSOA_CADASTRADA, 'None') as string) as nome_mae,
+    safe_cast(null as string) as nome_pai,
     safe_cast(NULLIF(OBITO, 'None') as date format 'DD/MM/YYYY') as data_obito,
     safe_cast(NULLIF(SEXO, 'None') as string) as sexo,
     safe_cast(NULLIF(ORIENTACAO_SEXUAL, 'None') as string) as orientacao_sexual,
@@ -31,19 +35,22 @@ select
     safe_cast(NULLIF(DATA_DE_NASCIMENTO, 'None') as date format 'DD/MM/YYYY') as data_nascimento,
     safe_cast(NULLIF(PAIS_DE_NASCIMENTO, 'None') as string) as pais_nascimento,
     safe_cast(NULLIF(MUNICIPIO_DE_NASCIMENTO, 'None') as string) as municipio_nascimento,
+    safe_cast(null as string) as estado_nascimento,
 
     -- Informações da Unidade
-    -- AP
+    safe_cast(NULLIF(AP, 'None') as string) as ap,
     safe_cast(NULLIF(CODIGO_MICROAREA, 'None') as string) as microarea,
     safe_cast(NULLIF(NUMERO_CNES_UNIDADE, 'None') as string) as cnes_unidade,
+    safe_cast(null as string) as nome_unidade,
     safe_cast(NULLIF(CODIGO_DA_EQUIPE_DE_SAUDE, 'None') as string) as codigo_equipe_saude,
     safe_cast(NULLIF(CODIGO_INE_EQUIPE_DE_SAUDE, 'None') as string) as codigo_ine_equipe_saude,
-    safe_cast(NULLIF(N_DA_FAMILIA, 'None') as string) as numero_familia,
+    safe_cast(null as timestamp) as data_atualizacao_vinculo_equipe,
     safe_cast(NULLIF(N_DO_PRONTUARIO, 'None') as string) as numero_prontuario,
+    safe_cast(NULLIF(N_DA_FAMILIA, 'None') as string) as numero_familia,
+    safe_cast(null as string) as cadastro_permanente,
     safe_cast(NULLIF(SITUACAO_USUARIO, 'None') as string) as situacao_usuario,
-    safe_cast(NULLIF(DATA_CADASTRO, 'None') as date format 'DD/MM/YYYY') as data_cadastro_inicial,
-    safe_cast(NULLIF(DATA_ULTIMA_ATUALIZACAO_DO_CADASTRO, 'None') as date format 'DD/MM/YYYY') as data_ultima_atualizacao_cadastral,
-    
+    safe_cast(NULLIF(DATA_CADASTRO, 'None') as timestamp format 'DD/MM/YYYY') as data_cadastro_inicial,
+    safe_cast(NULLIF(DATA_ULTIMA_ATUALIZACAO_DO_CADASTRO, 'None') as timestamp format 'DD/MM/YYYY') as data_ultima_atualizacao_cadastral,
 
     -- Endereço
     safe_cast(NULLIF(TIPO_DE_DOMICILIO, 'None') as string) as endereco_tipo_domicilio,
@@ -51,8 +58,11 @@ select
     safe_cast(NULLIF(CEP_LOGRADOURO, 'None') as string) as endereco_cep,
     safe_cast(NULLIF(LOGRADOURO, 'None') as string) as endereco_logradouro,
     safe_cast(NULLIF(BAIRRO_DE_MORADIA, 'None') as string) as endereco_bairro,
+    safe_cast(null as string) as endereco_estado,
+    safe_cast(null as string) as endereco_municipio,
 
     -- Metadata columns
+    safe_cast(null as date) as data_particao,
     safe_cast(NULLIF(updated_at, 'None') as timestamp) as updated_at,
     safe_cast(NULLIF(imported_at, 'None') as timestamp) as imported_at
 from {{ source("brutos_prontuario_vitacare_staging", "paciente_historico_eventos") }}
