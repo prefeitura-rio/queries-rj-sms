e2fbe3bfd7c5b6adb9bcc07b66799333966274d3
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-08 11:22:55 -0300

    fix: improve column names in patient smsrio + basic null cleaning

--- models/raw/plataforma_smsrio/raw_plataforma_smsrio__paciente.sql
+++ models/raw/plataforma_smsrio/raw_plataforma_smsrio__paciente.sql
@@ -2,7 +2,7 @@
     config(
         alias="paciente",
         materialized="incremental",
-        unique_key="paciente_cpf",
+        unique_key="id",
         partition_by={
             "field": "data_particao",
             "data_type": "date",
@@ -22,40 +22,53 @@ with
         {% if is_incremental() %} where data_particao > '{{seven_days_ago}}' {% endif %}
     ),
     events_ranked_by_freshness as (
-        select *, row_number() over (partition by patient_cpf order by source_updated_at desc) as rank
+        select *, row_number() over (partition by source_id order by source_updated_at desc) as rank
         from events_from_window
     ),
     latest_events as (select * from events_ranked_by_freshness where rank = 1)
 select
-    safe_cast(patient_cpf as string) as paciente_cpf,
-    safe_cast(data__nome as string) as nome,
-    safe_cast(data__nome_mae as string) as nome_mae,
-    safe_cast(data__nome_pai as string) as nome_pai,
-    safe_cast(data__dt_nasc as string) as dt_nasc,
-    safe_cast(data__sexo as string) as sexo,
-    safe_cast(data__racaCor as string) as raca_cor,
-    safe_cast(data__nacionalidade as string) as nacionalidade,
-    safe_cast(data__obito as string) as obito,
-    safe_cast(data__dt_obito as string) as dt_obito,
-    safe_cast(data__end_tp_logrado_cod as string) as end_tp_logrado_cod,
-    safe_cast(data__end_logrado as string) as end_logrado,
-    safe_cast(data__end_numero as string) as end_numero,
-    safe_cast(data__end_comunidade as string) as end_comunidade,
-    safe_cast(data__end_complem as string) as end_complem,
-    safe_cast(data__end_bairro as string) as end_bairro,
-    safe_cast(data__end_cep as string) as end_cep,
-    safe_cast(data__cod_mun_res as string) as cod_mun_res,
-    safe_cast(data__uf_res as string) as uf_res,
-    safe_cast(data__cod_mun_nasc as string) as cod_mun_nasc,
-    safe_cast(data__uf_nasc as string) as uf_nasc,
-    safe_cast(data__cod_pais_nasc as string) as cod_pais_nasc,
-    safe_cast(data__email as string) as email,
-    safe_cast(data__timestamp as string) as timestamp,
-    safe_cast(data__cns_provisorio as string) as cns_provisorio,
-    safe_cast(data__telefones as string) as telefones,
-    safe_cast(source_updated_at as string) as updated_at,
-    safe_cast(ano_particao as string) as ano_particao,
-    safe_cast(mes_particao as string) as mes_particao,
-    safe_cast(data_particao as date) as data_particao
+    -- Chave Primária
+    safe_cast(NULLIF(source_id, '') as string) as id,
+    
+    -- Outras Chaves
+    safe_cast(NULLIF(patient_cpf, '') as string) as cpf,
+    safe_cast(NULLIF(NULLIF(data__cns_provisorio, ''), 'None') as string) as cns_lista,
+
+    -- Informações Pessoais
+    safe_cast(NULLIF(NULLIF(data__nome, ''), 'None') as string) as nome,
+    safe_cast(NULLIF(NULLIF(data__nome_mae, ''), 'None') as string) as nome_mae,
+    safe_cast(NULLIF(NULLIF(NULLIF(data__nome_pai, ''), 'None'), 'SEM INFORMACAO') as string) as nome_pai,
+    safe_cast(NULLIF(NULLIF(data__sexo, ''), 'None') as string) as sexo,
+    safe_cast(NULLIF(NULLIF(data__obito, ''), 'None') as string) as obito,
+    safe_cast(NULLIF(NULLIF(data__dt_obito, ''), 'None') as date) as data_obito,
+    safe_cast(NULLIF(NULLIF(data__racaCor, ''), 'None') as string) as raca_cor,
+
+    -- Contato
+    safe_cast(NULLIF(NULLIF(data__email, ''),'None') as string) as email,
+    safe_cast(NULLIF(data__telefones, '') as string) as telefone_lista,
+
+    -- Nascimento
+    safe_cast(NULLIF(NULLIF(data__nacionalidade, ''), 'None') as string) as nacionalidade,
+    safe_cast(NULLIF(NULLIF(data__dt_nasc, ''), 'None') as date) as data_nascimento,
+    safe_cast(NULLIF(NULLIF(data__cod_mun_nasc, ''), 'None') as string) as codigo_municipio_nascimento,
+    safe_cast(NULLIF(NULLIF(data__uf_nasc, ''), 'None') as string) as uf_nascimento,
+    safe_cast(NULLIF(NULLIF(data__cod_pais_nasc, ''), 'None') as string) as codigo_pais_nascimento,
+
+    -- Endereço
+    safe_cast(NULLIF(NULLIF(data__end_tp_logrado_cod, ''), 'None') as string) as endereco_tipo_logradouro,
+    safe_cast(NULLIF(NULLIF(data__end_cep, ''), 'None') as string) as endereco_cep,
+    safe_cast(NULLIF(NULLIF(data__end_logrado, ''), 'None') as string) as endereco_logradouro,
+    safe_cast(NULLIF(NULLIF(data__end_numero, ''), 'None') as string) as endereco_numero,
+    safe_cast(NULLIF(NULLIF(data__end_comunidade, ''), 'None') as string) as endereco_comunidade,
+    safe_cast(NULLIF(NULLIF(data__end_complem, ''), 'None') as string) as endereco_complemento,
+    safe_cast(NULLIF(NULLIF(data__end_bairro, ''), 'None') as string) as endereco_bairro,
+    safe_cast(NULLIF(NULLIF(data__cod_mun_res, ''), 'None') as string) as endereco_municipio_codigo,
+    safe_cast(NULLIF(NULLIF(data__uf_res, ''), 'None') as string) as endereco_uf,
+
+    -- Metadata columns
+    safe_cast(NULLIF(ano_particao, '') as string) as ano_particao,
+    safe_cast(NULLIF(mes_particao, '') as string) as mes_particao,
+    safe_cast(NULLIF(data_particao, '') as date) as data_particao,
+    safe_cast(NULLIF(source_updated_at, '') as timestamp) as updated_at
 from latest_events
 
