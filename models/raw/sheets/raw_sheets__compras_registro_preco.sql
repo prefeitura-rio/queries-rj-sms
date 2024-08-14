9132f6f63d98bce957270d41167630b3ee6abe1c
ThiagoTrabach <thiago.trabach@gmail.com>
2024-08-14 19:34:59 -0300

    feat: Add compras_atas_processos_vigentes to _sheets_sources.yml

--- models/raw/sheets/raw_sheets__compras_registro_preco.sql
+++ models/raw/sheets/raw_sheets__compras_registro_preco.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        schema="brutos_sheets",
+        alias="compras_registro_preco",
+    )
+}}
+
 with source as (
       select * from {{ source('brutos_sheets_staging', 'compras_registro_preco') }}
 ),
@@ -13,11 +20,11 @@ renamed as (
         -- _2024_08_13,
         status,
         rp_vigente,
-        REGEXP_CONTAINS(rp_vigente, r'^\d{3}/\d{4}$') AS rp_vigente_indicador,
+        if(REGEXP_CONTAINS(rp_vigente, r'^\d{3}/\d{4}$'), 'sim', 'nao') AS rp_vigente_indicador,
         lower({{ clean_name_string('emergencial') }}) as emergencial,
         lower({{ clean_name_string('em_andamento_licitatorio') }}) as em_andamento_licitatorio,
         lower({{ clean_name_string('impedimento') }}) as impedimento,
-        parse_date('%d/%m/%Y',if(vencimento_da_ata = "0", Null, vencimento_da_ata)) as vencimento_da_ata_date,
+        parse_date('%d/%m/%Y',if(vencimento_da_ata = "0", Null, vencimento_da_ata)) as vencimento_data,
         {{ clean_name_string('situacao') }} as situacao,
         {{ clean_name_string('atualizacao') }} as atualizacao,
         fornecedor_principal,
@@ -27,4 +34,3 @@ renamed as (
     where cod_sigma is not null
 )
 select * from renamed
-  
