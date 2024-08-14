6a1055622c9c23937dc9edba1033f7bb40e39848
ThiagoTrabach <thiago.trabach@gmail.com>
2024-08-14 19:35:13 -0300

    Refactor mart_estoque__report_gestao.sql for improved readability and performance

--- models/marts/estoque/report/gestao/mart_estoque__report_gestao.sql
+++ models/marts/estoque/report/gestao/mart_estoque__report_gestao.sql
@@ -33,6 +33,8 @@ with
         where tipo_sms_agrupado = "APS"
     ),
 
+    registro_preco as (select * from {{ ref("raw_sheets__compras_atas_processos_vigentes") }}),
+
     -- - transformations
     -- dias de estoque
     cmd as (
@@ -98,11 +100,11 @@ with
     ),
 
     -- - final query
-    final as (
+    sources_joined as (
         select
             m.id_material,
             m.nome,
-            coalesce(pqrs.pqrs_categoria, "S") as pqrs_categoria, -- itens sem qualquer movimento não são classificados
+            coalesce(pqrs.pqrs_categoria, "S") as pqrs_categoria,  -- itens sem qualquer movimento não são classificados
             m.hierarquia_n1_categoria,
             m.hierarquia_n2_subcategoria,
             m.cadastrado_sistema_vitacare_indicador,
@@ -110,34 +112,61 @@ with
             p.qtd_tpc,
             round(p.cmd, 0) as cmd,
             coalesce(
-                round({{ dbt_utils.safe_divide("p.qtd_aps", "p.cmd") }}, 2), 0
-            ) as cobertura_aps,
+                round({{ dbt_utils.safe_divide("p.qtd_aps", "p.cmd") }}, 2),
+                if(cmd is null, null, 0)
+            ) as cobertura_aps_dias,
             coalesce(
-                round({{ dbt_utils.safe_divide("p.qtd_tpc", "p.cmd") }}, 2), 0
-            ) as cobertura_tpc,
+                round({{ dbt_utils.safe_divide("p.qtd_tpc", "p.cmd") }}, 2),
+                if(cmd is null, null, 0)
+            ) as cobertura_tpc_dias,
             coalesce(
                 round({{ dbt_utils.safe_divide("p.qtd_aps + p.qtd_tpc", "p.cmd") }}, 2),
-                0
-            ) as cobertura_total,
-            if(
-                p.qtd_aps = 0, 10 , za.zeradas_ap) as zeradas_ap,
+                if(cmd is null, null, 0)
+            ) as cobertura_total_dias,
+            if(p.qtd_aps = 0, 10, za.zeradas_ap) as zeradas_ap,
             if(
                 p.qtd_aps = 0,
                 (select count(distinct id_cnes) from posicao_aps),
                 zu.zerados_ubs
             ) as zerados_ubs,  -- correção para incluir unidades com estoques positivos porém vencidos
-        -- zu.zerados_ubs as zerados_ubs_sem_correcao,
+            -- zu.zerados_ubs as zerados_ubs_sem_correcao,
             m.farmacia_popular_disponibilidade_indicador,
+            coalesce(rp.rp_vigente_indicador, "nao") as rp_vigente_indicador,
+            rp.vencimento_data,
+            CONCAT(UPPER(SUBSTR(status, 1, 1)), LOWER(SUBSTR(status, 2))) AS registro_preco_status,
         from medicamentos as m
         left join posicao_pivoted as p using (id_material)
         left join ubs_zeradas as zu using (id_material)
         left join ap_zeradas as za using (id_material)
         left join curva_pqrs as pqrs using (id_material)
-        order by cobertura_total asc
+        left join registro_preco as rp using (id_material)
+        order by cobertura_total_dias asc
     )
 
-select *
-from final
+select *, 
+    case
+        when cobertura_total_dias is null then "3. Atenção"
+        when rp_vigente_indicador = "nao" and cobertura_total_dias = 0 then "1. Muito Crítico"
+        when rp_vigente_indicador = "nao" and cobertura_total_dias < 30 then "1. Muito Crítico"
+        when rp_vigente_indicador = "nao" and cobertura_total_dias >= 30 then "2. Crítico"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias = 0 then "2. Crítico"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias > 0 and zeradas_ap > 0 then "3. Atenção"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias < 10 then "3. Atenção"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias > 0 and zeradas_ap = 0 then "4. Ok"
+        else "Verificar"
+    end as status,
+    case
+        when cobertura_total_dias is null then "3.3. Sem histórico de CMD"
+        when rp_vigente_indicador = "nao" and cobertura_total_dias = 0 then "1.1. Zerado e sem RP"
+        when rp_vigente_indicador = "nao" and cobertura_total_dias < 30 then "1.2. Estoque baixo e sem RP"
+        when rp_vigente_indicador = "nao" and cobertura_total_dias >= 30 then "2.2. Estoque alto, mas sem RP"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias = 0 then "2.1. Zerado com RP"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias > 0 and zeradas_ap > 0 then  "3.2. Estoque desbalanceado: AP(s) zerada(s)"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias < 10 then "3.1. Estoque baixo: menos de dez dias na rede"
+        when rp_vigente_indicador = "sim" and cobertura_total_dias > 0 and zeradas_ap = 0 then ""
+        else ""
+    end as motivo_status
+from sources_joined
 where
     hierarquia_n1_categoria = "Medicamento"
     and cadastrado_sistema_vitacare_indicador = "sim"
