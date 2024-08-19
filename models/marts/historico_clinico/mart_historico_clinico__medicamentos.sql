c6f2df6afcb20caf3bb88a53d815d2ffac49434d
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-19 10:32:13 -0300

    feat: Update SQL script for medicamentos-cronicos table in historico_clinico

--- models/marts/historico_clinico/mart_historico_clinico__medicamentos.sql
+++ models/marts/historico_clinico/mart_historico_clinico__medicamentos.sql
@@ -8,17 +8,19 @@
 
 
 with 
-    bruto_atendimento as (
+    atendimentos_recentes as (
         select * 
         from {{ ref('raw_prontuario_vitacare__atendimento') }} 
+        where updated_at >= date_sub(current_date(), interval 1 year)
     ),
     prescricoes as (
         select 
             cpf,
             replace(json_extract_scalar(prescricoes_json, "$.cod_medicamento"), "-", "") as id,
             upper(json_extract_scalar(prescricoes_json, "$.nome_medicamento")) as nome,
-            cast(json_extract_scalar(prescricoes_json, "$.uso_continuado") as boolean) as uso_continuo
-        from bruto_atendimento,
+            cast(json_extract_scalar(prescricoes_json, "$.uso_continuado") as boolean) as uso_continuo,
+            updated_at as datahora_prescricao
+        from atendimentos_recentes,
             unnest(json_extract_array(prescricoes)) as prescricoes_json
     ),
     materiais as (
@@ -31,6 +33,7 @@ with
             prescricoes.id,
             coalesce(materiais.descricao, prescricoes.nome) as nome,
             materiais.concentracao,
+            prescricoes.datahora_prescricao
         from prescricoes
             inner join materiais on prescricoes.id = materiais.id_material
         where prescricoes.uso_continuo = true
@@ -41,7 +44,8 @@ select
         struct(
             id,
             nome,
-            concentracao
+            concentracao,
+            datahora_prescricao
         )
     ) as medicamentos
 from uso_continuado
