11d6a7b89b54da5e4faf71b6c7928caf6bade205
ThiagoTrabach <thiago.trabach@gmail.com>
2024-08-19 19:48:50 -0300

    feat: Update generate_schema_name macro to handle dev environment

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,14 +1,31 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+    {% if target.name == "dev"  -%}
 
-        {{ default_schema }}
+        {%- set default_schema = target.schema -%}
+        {%- if custom_schema_name is none -%}
 
-    {%- else -%}
+            {{ default_schema }}
 
-        {{ custom_schema_name | trim }}
+        {%- else -%}
+
+            {{ env_var('DBT_USER') }}__{{ custom_schema_name | trim }}
+            
+        {%- endif -%}
+
+    
+    {%- else %}
+
+        {%- set default_schema = target.schema -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
 
     {%- endif -%}
-
 {%- endmacro %}
