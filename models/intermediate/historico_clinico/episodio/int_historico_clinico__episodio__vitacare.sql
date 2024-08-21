27277303872d3477d549747972c7cbb46b811aa8
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-21 16:19:21 -0300

    Refactor estabelecimento nome field to use nome_limpo in int_historico_clinico__episodio__vitacare.sql

--- models/intermediate/historico_clinico/episodio/int_historico_clinico__episodio__vitacare.sql
+++ models/intermediate/historico_clinico/episodio/int_historico_clinico__episodio__vitacare.sql
@@ -47,7 +47,7 @@ with
             id_cnes as pk,
             struct(
                 id_cnes,
-                nome_limpo as nome,
+                {{ proper_estabelecimento('nome_limpo') }} as nome,
                 tipo_sms as estabelecimento_tipo
             ) as estabelecimento
         from {{ ref("dim_estabelecimento") }}
