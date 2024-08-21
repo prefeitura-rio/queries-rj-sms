248039044583aa913f105f86ffdcecf7d916a95a
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-21 12:00:13 -0300

    clustering encounters by patient cpf

--- models/marts/historico_clinico/mart_historico_clinico__episodio.sql
+++ models/marts/historico_clinico/mart_historico_clinico__episodio.sql
@@ -3,6 +3,7 @@
         schema="saude_historico_clinico",
         alias="episodio_assistencial",
         materialized="table",
+        cluster_by = "paciente_cpf",
     )
 }}
 
@@ -13,37 +14,43 @@ with
     ),
     vitacare as (
         select * from {{ ref("int_historico_clinico__episodio__vitacare") }}
+    ),
+    merged as (
+        select
+            paciente.cpf as paciente_cpf,
+            paciente, 
+            tipo,
+            subtipo,
+            entrada_datahora,
+            saida_datahora,
+            motivo_atendimento,
+            desfecho_atendimento,
+            condicoes,
+            null as prescricoes,
+            estabelecimento, 
+            profissional_saude_responsavel,
+            prontuario,
+            metadados
+        from vitai
+
+        union all
+
+        select 
+            paciente.cpf as paciente_cpf,
+            paciente, 
+            tipo,
+            subtipo,
+            entrada_datahora,
+            saida_datahora,
+            motivo_atendimento,
+            desfecho_atendimento,
+            condicoes,
+            prescricoes,
+            estabelecimento, 
+            profissional_saude_responsavel,
+            prontuario,
+            metadados
+        from vitacare
     )
-select 
-    paciente, 
-    tipo,
-    subtipo,
-    entrada_datahora,
-    saida_datahora,
-    motivo_atendimento,
-    desfecho_atendimento,
-    condicoes,
-    null as prescricoes,
-    estabelecimento, 
-    profissional_saude_responsavel,
-    prontuario,
-    metadados
-from vitai
-
-union all
-
-select 
-    paciente, 
-    tipo,
-    subtipo,
-    entrada_datahora,
-    saida_datahora,
-    motivo_atendimento,
-    desfecho_atendimento,
-    condicoes,
-    prescricoes,
-    estabelecimento, 
-    profissional_saude_responsavel,
-    prontuario,
-    metadados
-from vitacare
+select *
+from merged
