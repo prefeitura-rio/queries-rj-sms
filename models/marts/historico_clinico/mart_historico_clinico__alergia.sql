84fc07f6f7119c191dcee703a1c4f6491b42fe70
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-21 11:33:13 -0300

    Merging allergies from vitai and vitacare

--- models/marts/historico_clinico/mart_historico_clinico__alergia.sql
+++ models/marts/historico_clinico/mart_historico_clinico__alergia.sql
@@ -8,12 +8,34 @@
 
 with 
     vitai as (
-        select * from {{ ref("int_historico_clinico__alergia__vitai") }}
+        select
+            id_paciente,
+            cns,
+            cpf,
+            alergia
+        from {{ ref("int_historico_clinico__alergia__vitai") }},
+            unnest(alergias) as alergia
+    ),
+    vitacare as (
+        select
+            safe_cast(null as string) as id_paciente,
+            safe_cast(null as string) as cns,
+            cpf,
+            alergia
+        from {{ ref("int_historico_clinico__alergia__vitacare") }},
+            unnest(alergias) as alergia
+    ),
+    total as (
+        select * from vitai
+        union all
+        select * from vitacare
     )
-
-select 
+select
     id_paciente,
     cns,
     cpf,
-    alergias
-from vitai
+    array_agg(alergia) as alergias,
+    safe_cast(current_datetime() as datetime) as processed_at
+from total
+group by id_paciente, cns, cpf
+
