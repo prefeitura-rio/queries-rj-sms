93f032be2ccb1c6fe61e52c22b60d624f76d5442
ThiagoTrabach <thiago.trabach@gmail.com>
2024-08-26 15:50:43 -0300

    feat: Add new table raw_osinfo__saldos

--- models/raw/osinfo/raw_osinfo__saldos.sql
+++ models/raw/osinfo/raw_osinfo__saldos.sql
@@ -11,17 +11,17 @@ with
 
 -- --- Saldos
 select
-    s.id_saldo_dados as saldoid,
-    s.id_saldo_item as saldoitemid,
-    si.saldo_item as saldoitem,
-    s.referencia_mes_receita as mesreferenciareceita,
-    s.referencia_ano_receita as anoreferenciareceita,
+    s.id_saldo_dados as id,
+    s.id_saldo_item as id_item,
+    si.saldo_item as item_nome,
+    s.referencia_mes_receita as receita_referencia_mes,
+    s.referencia_ano_receita as receita_referencia_ano,
     s.valor as valor,
-    s.id_instrumento_contratual as contratoid,
-    c.numero_contrato as numerocontrato,
-    c.cod_organizacao as codigoorganizacao,
-    s.id_conta_bancaria as contabancariaid,
-    s.arq_img_ext as imagemextrato,
+    s.id_instrumento_contratual as id_contrato,
+    c.numero_contrato as contrato_numero,
+    c.cod_organizacao as id_organizacao,
+    s.id_conta_bancaria,
+    s.arq_img_ext as extrato_imagem,
 from saldo_dados s
 inner join saldo_item si on s.id_saldo_item = si.id_saldo_item
 inner join contrato c on s.id_instrumento_contratual = c.id_contrato
