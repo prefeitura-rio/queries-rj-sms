73bf9c0b5019c29db7ac2330fa018aeefb378e7f
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-26 11:31:35 -0300

    HCI app models

---
+++ models/marts/historico_clinico_app/mart_historico_clinico_app__episodio.sql
@@ -0,0 +1,43 @@
+{{
+    config(
+        alias="episodio_assistencial",
+        materialized="table",
+        cluster_by="cpf",
+        schema="projeto_hci_aplicacao",
+    )
+}}
+
+with
+    todos_episodios as (
+        select
+            *
+        from {{ ref('mart_historico_clinico__episodio') }}
+    ),
+    formatado as (
+        select
+            entrada_datahora as entry_datetime,
+            saida_datahora as exit_datetime,
+            estabelecimento.nome as location,
+            tipo as type,
+            subtipo as subtype,
+            array(
+                select descricao
+                from unnest(condicoes)
+                where descricao is not null
+            ) as active_cids,
+            profissional_saude_responsavel[safe_offset(0)] as responsible,
+            motivo_atendimento as clinical_motivation,
+            desfecho_atendimento as clinical_outcome,
+            case 
+                when estabelecimento.estabelecimento_tipo is null then []
+                when estabelecimento.estabelecimento_tipo in ('CLINICA DA FAMILIA','CENTRO MUNICIPAL DE SAUDE') then ['CF/CMS']
+                else array(
+                select estabelecimento.estabelecimento_tipo
+                )
+            end as filter_tags
+        from todos_episodios
+    )
+
+select
+    *
+from formatado
