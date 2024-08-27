29a0141f7369235862a56dd72e36db147cd1c265
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-08-27 18:48:24 -0300

    New HCI App Models format with exhibition rules

--- models/marts/historico_clinico_app/mart_historico_clinico_app__episodio.sql
+++ models/marts/historico_clinico_app/mart_historico_clinico_app__episodio.sql
@@ -12,11 +12,97 @@ with
         select
             *
         from {{ ref('mart_historico_clinico__episodio') }}
-        where paciente_cpf is not null
     ),
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+    -- REGRAS DE EXIBIÇÃO
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+    -- Regra 1: Sem Identificador de Paciente
+    regra_sem_identificador as (
+        select 
+            todos_episodios.id_atendimento,
+            safe_cast(
+                case
+                    when paciente.cpf is null then true
+                    else false
+                end
+            as boolean) as tem_exibicao_limitada,
+            safe_cast(
+                case
+                    when paciente.cpf is null then "Paciente sem CPF"
+                    else null
+                end
+            as string) as motivo
+        from todos_episodios
+    ),
+    -- Regras 2: Sem Informações Básicas
+    regra_sem_dados_basicos as (
+        select 
+            todos_episodios.id_atendimento,
+            safe_cast(
+                case
+                    when 
+                        todos_episodios.tipo != "Exame" and
+                        array_length(todos_episodios.condicoes) = 0 and 
+                        todos_episodios.motivo_atendimento is null and
+                        todos_episodios.desfecho_atendimento is null
+                        then true
+                    else false
+                end
+            as boolean) as tem_exibicao_limitada,
+            safe_cast(
+                case
+                    when 
+                        todos_episodios.tipo != "Exame" and
+                        array_length(todos_episodios.condicoes) = 0 and 
+                        todos_episodios.motivo_atendimento is null and
+                        todos_episodios.desfecho_atendimento is null
+                        then "Episódio Não Informativo"
+                    else null
+                end
+            as string) as motivo
+        from todos_episodios
+    ),
+    -- Regra 3: Pacientes Não Restrito
+    regra_paciente_restrito as (
+        select
+            todos_episodios.id_atendimento,
+            todos_pacientes.exibicao.indicador as indicador,
+            safe_cast(
+                case
+                    when todos_pacientes.exibicao.indicador = false then "Paciente Restrito"
+                    else null
+                end
+            as string) as motivo
+        from todos_episodios
+            inner join {{ ref('mart_historico_clinico_app__paciente') }} as todos_pacientes
+                on todos_episodios.paciente_cpf = todos_pacientes.cpf
+    ),
+    -- Juntando Regras
+    todas_regras as (
+        select * from regra_sem_identificador
+        union all
+        select * from regra_sem_dados_basicos
+        union all
+        select * from regra_paciente_restrito
+    ),
+    -- Agrupando Regras
+    regras_exibicao as (
+        select 
+            id_atendimento,
+            struct(
+                not(logical_or(tem_exibicao_limitada)) as indicador,
+                array_agg(motivo ignore nulls) as motivos
+            ) as exibicao
+        from todas_regras
+        group by id_atendimento
+    ),
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+    -- FORMATAÇÃO
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
     formatado as (
         select
             paciente_cpf as cpf,
+            id_atendimento,
             safe_cast(entrada_datahora as string) as entry_datetime,
             safe_cast(saida_datahora as string) as exit_datetime,
             safe_cast(estabelecimento.nome as string) as location,
@@ -47,7 +133,11 @@ with
             end as filter_tags
         from todos_episodios
     )
-
+---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+-- JUNTANDO INFORMAÇÕES DE EXIBICAO
+---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
 select
-    *
+    formatado.*,
+    regras_exibicao.* except(id_atendimento)
 from formatado
+    left join regras_exibicao using (id_atendimento)
--- models/marts/historico_clinico_app/mart_historico_clinico_app__paciente.sql
+++ models/marts/historico_clinico_app/mart_historico_clinico_app__paciente.sql
@@ -13,6 +13,49 @@ with
             *
         from {{ ref('mart_historico_clinico__paciente') }}
     ),
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+    --  REGRAS DE EXIBIÇÃO
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+    -- Regra 1: Menor de Idade
+    regra_menor_de_idade as (
+        select 
+            todos_pacientes.cpf,
+            safe_cast(
+                case
+                    when todos_pacientes.dados.data_nascimento is null then false
+                    when DATE_DIFF(current_date(), todos_pacientes.dados.data_nascimento, YEAR) >= 18 then false
+                    when DATE_DIFF(current_date(), todos_pacientes.dados.data_nascimento, YEAR) < 18 then true
+                end
+            as boolean) as tem_exibicao_limitada,
+            safe_cast(
+                case
+                    when todos_pacientes.dados.data_nascimento is null then null
+                    when DATE_DIFF(current_date(), todos_pacientes.dados.data_nascimento, YEAR) >= 18 then null
+                    when DATE_DIFF(current_date(), todos_pacientes.dados.data_nascimento, YEAR) < 18 then "Menor de Idade"
+                end
+            as string) as motivo
+        from todos_pacientes
+    ),
+    -- Juntando Regras
+    todas_regras as (
+        select * from regra_menor_de_idade
+        -- union all
+        -- (...)
+    ),
+    -- Agrupando Regras
+    regras_exibicao as (
+        select 
+            cpf,
+            struct(
+                not(logical_or(tem_exibicao_limitada)) as indicador,
+                array_agg(motivo ignore nulls) as motivos
+            ) as exibicao
+        from todas_regras
+        group by cpf
+    ),
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+    --  FORMATAÇÃO
+    ---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
     formatado as (
         select 
             dados.nome as registration_name,
@@ -52,7 +95,15 @@ with
             dados.identidade_validada_indicador as validated
         from todos_pacientes
     )
-
+---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+--  JUNTANDO INFORMAÇÕES DE EXIBICAO
+---=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
 select
-    *
-from formatado
+    regras_exibicao.cpf,
+    formatado.* except(cpf),
+    regras_exibicao.exibicao
+from regras_exibicao
+    left join formatado on (
+        regras_exibicao.cpf = formatado.cpf and 
+        regras_exibicao.exibicao.indicador = true
+    )
