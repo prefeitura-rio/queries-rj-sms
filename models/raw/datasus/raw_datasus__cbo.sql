87d5b06bc06d4083180450af388f980451281f8d
ThiagoTrabach <thiago.trabach@gmail.com>
2024-09-02 09:57:44 -0300

    Fix queries for datasus cbo and cbo_fam tables

--- models/raw/datasus/raw_datasus__cbo.sql
+++ models/raw/datasus/raw_datasus__cbo.sql
@@ -11,8 +11,8 @@ with
         select
             safe_cast(cbo as string) as id_cbo,
             safe_cast(ds_cbo as string) as descricao,
-            safe_cast(_data_carga as date format 'DD/MM/YYY') as data_carga,
-            safe_cast(_data_snapshot as date format 'DD/MM/YYY') as _data_snapshot,
+            safe_cast(imported_at as date) as data_carga,
+            safe_cast(created_at as date) as _data_snapshot,
         from source
     )
 select *
