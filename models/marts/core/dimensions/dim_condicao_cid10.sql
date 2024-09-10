d69cfaa833f8378df17c682bd9dd1061dace6961
ThiagoTrabach <thiago.trabach@gmail.com>
2024-09-10 16:33:14 -0300

    Refactor raw schema and add new dimension for CID-10 conditions

---
+++ models/marts/core/dimensions/dim_condicao_cid10.sql
@@ -0,0 +1,62 @@
+{{
+    config(
+        alias="condicao_cid10",
+        labels={
+            "dado_publico": "sim",
+            "dado_pessoal": "nao",
+            "dado_anonimizado": "nao",
+            "dado_sensivel_saude": "nao",
+        },
+    )
+}}
+
+with
+    source as (select * from {{ ref("raw_datasus__cid10") }}),
+
+    grupo_agrupado as (
+        select
+            id_subcategoria,
+            subcategoria_descricao,
+            ordem,
+            id_categoria,
+            categoria_descricao,
+            categoria_descricao_abv,
+            id_capitulo,
+            capitulo_descricao,
+            array_agg(
+                struct(
+                    safe_cast(grupo_descricao as string) as descricao,
+                    safe_cast(grupo_descricao_abv as string) as abreviatura,
+                    safe_cast(grupo_descricao_len as int) as comprimento
+                )
+            ) as grupo
+        from source
+        group by 1,2,3,4,5,6,7,8),
+
+    final as (
+
+        select
+            safe_cast(id_subcategoria as string) as id,
+            safe_cast(subcategoria_descricao as string) as descricao,
+            safe_cast(ordem as int) as ordem,
+
+            struct(
+                safe_cast(id_categoria as string) as id,
+                safe_cast(categoria_descricao as string) as descricao,
+                safe_cast(categoria_descricao_abv as string) as abreviatura
+            ) as categoria,
+
+            struct(
+                safe_cast(id_capitulo as string) as id,
+                safe_cast(capitulo_descricao as string) as descricao
+            ) as capitulo,
+
+            grupo
+
+        from grupo_agrupado
+        order by ordem
+
+    )
+
+select *
+from final
--- models/raw/datasus/raw_datasus__cid10.sql
+++ models/raw/datasus/raw_datasus__cid10.sql
@@ -1,31 +1,19 @@
 {{
     config(
         alias="cid10",
-        schema = "brutos_datasus",
-        labels = {
-            'dado_publico': 'sim',
-            'dado_pessoal': 'nao',
-            'dado_anonimizado': 'nao',
-            'dado_sensivel_saude': 'nao',
-        }
+        schema="brutos_datasus",
+        labels={
+            "dado_publico": "sim",
+            "dado_pessoal": "nao",
+            "dado_anonimizado": "nao",
+            "dado_sensivel_saude": "nao",
+        },
     )
 }}
-select 
-    safe_cast(id_subcategoria as string) as id_subcategoria,
-    safe_cast(id_categoria as string) as id_categoria,
-    safe_cast(id_capitulo as string) as id_capitulo,
-    safe_cast(ordem as int) as ordem,
-    safe_cast(subcategoria_descricao as string) as subcategoria_descricao,
-    safe_cast(categoria_descricao as string) as categoria_descricao,
-    safe_cast(categoria_descricao_abv as string) as categoria_descricao_abv,
-    safe_cast(capitulo_descricao as string) as capitulo_descricao,
-    array_agg(
-        struct(
-            safe_cast(grupo_descricao as string) as grupo_descricao,
-            safe_cast(grupo_descricao_abv as string) as grupo_descricao_abv,
-            safe_cast(grupo_descricao_len as int) as grupo_descricao_len
-        )
-    ) as grupo
-from 
-{{ source("brutos_datasus_staging", "cid10") }}
-group by 1,2,3,4,5,6,7,8
+
+with
+    source as (select * from {{ source("brutos_datasus_staging", "cid10") }})
+
+select *
+from source
+order by ordem
