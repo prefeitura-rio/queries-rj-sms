d918e1486c69d8fd12e937f04f934aaa49357c86
miloskimatheus <miloskimatheus@gmail.com>
2024-10-16 12:31:14 -0300

    removing not used selects

--- models/marts/core/dimensions/rio/dim_leito_sus_rio_historico.sql
+++ models/marts/core/dimensions/rio/dim_leito_sus_rio_historico.sql
@@ -16,12 +16,6 @@ with
         select max(data_particao) as versao from {{ ref("raw_cnes_web__tipo_unidade") }}
     ),
 
-    estabelecimentos_mrj_sus as (
-        select *
-        from {{ ref("dim_estabelecimento_sus_rio_historico") }}
-        where safe_cast(data_particao as string) = (select versao from versao_atual)
-    ),
-
     leitos_mapping_cnesftp as (
         select *
         from
