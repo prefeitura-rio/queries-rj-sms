a7a0bf0f42795cedcf49aa7d649c0a3f0a861c21
ThiagoTrabach <thiago.trabach@gmail.com>
2024-10-16 08:38:32 -0300

    chore: lint sql code and change column's order

--- models/marts/core/dimensions/rio/dim_leito_sus_rio_historico.sql
+++ models/marts/core/dimensions/rio/dim_leito_sus_rio_historico.sql
@@ -3,64 +3,81 @@
         enabled=true,
         schema="saude_cnes",
         alias="leito_sus_rio_historico",
-        partition_by = {
-            'field': 'data_particao', 
-            'data_type': 'date',
-            'granularity': 'day'
-        }
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "day",
+        },
     )
 }}
 
 with
-versao_atual as (
-    select MAX(data_particao) as versao 
-    from {{ ref("raw_cnes_web__tipo_unidade") }}
-), 
+    versao_atual as (
+        select max(data_particao) as versao from {{ ref("raw_cnes_web__tipo_unidade") }}
+    ),
 
-estabelecimentos_mrj_sus as (
-    select * from {{ ref("dim_estabelecimento_sus_rio_historico") }} where safe_cast(data_particao as string) = (select versao from versao_atual)
-),
+    estabelecimentos_mrj_sus as (
+        select *
+        from {{ ref("dim_estabelecimento_sus_rio_historico") }}
+        where safe_cast(data_particao as string) = (select versao from versao_atual)
+    ),
 
-leitos_mapping_cnesftp as (
-    select * from unnest([
-        STRUCT(1 as tipo_leito, "CIRURGICO" as tipo_leito_descr),
-        STRUCT(2, "CLINICO"),
-        STRUCT(3, "COMPLEMENTAR"),
-        STRUCT(4, "OBSTETRICO"),
-        STRUCT(5, "PEDIATRICO"),
-        STRUCT(6, "OUTROS"),
-        STRUCT(7, "HOSPITAL / DIA")
-    ])
-),
+    leitos_mapping_cnesftp as (
+        select *
+        from
+            unnest(
+                [
+                    struct(1 as tipo_leito, "CIRURGICO" as tipo_leito_descr),
+                    struct(2, "CLINICO"),
+                    struct(3, "COMPLEMENTAR"),
+                    struct(4, "OBSTETRICO"),
+                    struct(5, "PEDIATRICO"),
+                    struct(6, "OUTROS"),
+                    struct(7, "HOSPITAL / DIA")
+                ]
+            )
+    ),
 
-leitos_mapping_cnesweb as (
-    select
-        id_leito_especialidade as tipo_especialidade_leito,
-        leito_especialidade as tipo_especialidade_leito_descr,
-        data_particao
-    from {{ ref("raw_cnes_web__leito") }}
-    where data_particao = (select versao from versao_atual)
-),
+    leitos_mapping_cnesweb as (
+        select
+            id_leito_especialidade as tipo_especialidade_leito,
+            leito_especialidade as tipo_especialidade_leito_descr,
+            data_particao
+        from {{ ref("raw_cnes_web__leito") }}
+        where data_particao = (select versao from versao_atual)
+    ),
 
-leitos_mrj_sus as (
-    select 
-        parse_date('%Y-%m-%d', web.data_particao) as data_particao,
-        lt.ano_competencia,
-        lt.mes_competencia,
-        lt.id_cnes,
-        lt.tipo_leito,
-        lt.tipo_especialidade_leito,
-        lt.quantidade_total,
-        lt.quantidade_contratado,
-        lt.quantidade_sus,
+    leitos_mrj_sus as (
+        select
 
-        ftp.tipo_leito_descr,
-        web.tipo_especialidade_leito_descr
+            lt.id_cnes,
+            lt.tipo_leito,
+            lt.tipo_especialidade_leito,
+            lt.quantidade_total,
+            lt.quantidade_contratado,
+            lt.quantidade_sus,
 
-    from  {{ref("int_leito_sus_rio_historico__brutos_filtrados")}} as lt
-    left join leitos_mapping_cnesftp as ftp on safe_cast(lt.tipo_leito as int64) = ftp.tipo_leito
-    left join leitos_mapping_cnesweb as web on safe_cast(lt.tipo_especialidade_leito as int64) = safe_cast(web.tipo_especialidade_leito as int64)
-    order by ano_competencia asc, mes_competencia asc, id_cnes asc, tipo_leito_descr asc, tipo_especialidade_leito_descr asc
-)
+            ftp.tipo_leito_descr,
+            web.tipo_especialidade_leito_descr,
+            lt.ano_competencia,
+            lt.mes_competencia,
+            parse_date('%Y-%m-%d', web.data_particao) as data_particao,
 
-select * from leitos_mrj_sus
+        from {{ ref("int_leito_sus_rio_historico__brutos_filtrados") }} as lt
+        left join
+            leitos_mapping_cnesftp as ftp
+            on safe_cast(lt.tipo_leito as int64) = ftp.tipo_leito
+        left join
+            leitos_mapping_cnesweb as web
+            on safe_cast(lt.tipo_especialidade_leito as int64)
+            = safe_cast(web.tipo_especialidade_leito as int64)
+        order by
+            ano_competencia asc,
+            mes_competencia asc,
+            id_cnes asc,
+            tipo_leito_descr asc,
+            tipo_especialidade_leito_descr asc
+    )
+
+select *
+from leitos_mrj_sus
--- models/marts/core/dimensions/rio/dim_profissional_sus_rio_historico.sql
+++ models/marts/core/dimensions/rio/dim_profissional_sus_rio_historico.sql
@@ -3,86 +3,85 @@
         enabled=true,
         schema="saude_cnes",
         alias="profissional_sus_rio_historico",
-        partition_by = {
-            'field': 'data_particao', 
-            'data_type': 'date',
-            'granularity': 'day'
-        }
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "day",
+        },
     )
 }}
 
 with
-versao_atual as (
-    select MAX(data_particao) as versao 
-    from {{ ref("raw_cnes_web__tipo_unidade") }}
-), 
+    versao_atual as (
+        select max(data_particao) as versao from {{ ref("raw_cnes_web__tipo_unidade") }}
+    ),
 
-estabelecimentos_mrj_sus as (
-    select * from {{ ref("dim_estabelecimento_sus_rio_historico") }} where safe_cast(data_particao as string) = (select versao from versao_atual)
-),
+    estabelecimentos_mrj_sus as (
+        select *
+        from {{ ref("dim_estabelecimento_sus_rio_historico") }}
+        where safe_cast(data_particao as string) = (select versao from versao_atual)
+    ),
 
-profissionais_mrj as (
-    select * from {{ref("int_profissional_sus_rio_historico__brutos_filtrados")}}
-),
+    profissionais_mrj as (
+        select * from {{ ref("int_profissional_sus_rio_historico__brutos_filtrados") }}
+    ),
 
-cbo as (
-    select * from {{ ref("raw_datasus__cbo") }}
-),
+    cbo as (select * from {{ ref("raw_datasus__cbo") }}),
 
-cbo_fam as (
-    select * from {{ ref("raw_datasus__cbo_fam") }}
-),
+    cbo_fam as (select * from {{ ref("raw_datasus__cbo_fam") }}),
 
-tipo_vinculo as (
-    select
-        concat(id_vinculacao, tipo) as codigo_tipo_vinculo,
-        descricao,
-        data_particao
-    from {{ ref("raw_cnes_web__tipo_vinculo") }}
-    WHERE data_particao = (SELECT versao FROM versao_atual)
-),
+    tipo_vinculo as (
+        select
+            concat(id_vinculacao, tipo) as codigo_tipo_vinculo, descricao, data_particao
+        from {{ ref("raw_cnes_web__tipo_vinculo") }}
+        where data_particao = (select versao from versao_atual)
+    ),
 
-vinculo as (
-    select
-        id_vinculacao,
-        descricao,
-    from {{ ref("raw_cnes_web__vinculo") }}
-    WHERE data_particao = (SELECT versao FROM versao_atual)
-),
+    vinculo as (
+        select id_vinculacao, descricao,
+        from {{ ref("raw_cnes_web__vinculo") }}
+        where data_particao = (select versao from versao_atual)
+    ),
 
-profissional_dados_hci as (
-    select distinct cns, cpf from {{ ref("mart_historico_clinico__paciente") }}
-),
+    profissional_dados_hci as (
+        select distinct cns, cpf from {{ ref("mart_historico_clinico__paciente") }}
+    ),
 
-final AS (
-    SELECT
-        parse_date('%Y-%m-%d', tipo_vinculo.data_particao) as data_particao,
-        p.ano_competencia,
-        p.mes_competencia,
-        p.id_cnes,
-        p.data_registro,
-        hci.cpf,
-        p.profissional_cns as cns,
-        p.profissional_nome as nome,
-        vinculacao.descricao AS vinculacao,
-        tipo_vinculo.descricao AS vinculo_tipo,
-        p.id_cbo,
-        ocup.descricao AS cbo,
-        p.id_cbo_familia,
-        ocupf.descricao AS cbo_familia,
-        id_registro_conselho,
-        id_tipo_conselho,
-        carga_horaria_outros,
-        carga_horaria_hospitalar,
-        carga_horaria_ambulatorial,
-        carga_horaria_total,
-            
-    FROM profissionais_mrj AS p
-    LEFT JOIN profissional_dados_hci AS hci ON SAFE_CAST(p.profissional_cns AS INT64) = (SELECT SAFE_CAST(cns AS INT64) FROM UNNEST(hci.cns) AS cns LIMIT 1)
-    LEFT JOIN cbo AS ocup ON p.id_cbo = ocup.id_cbo
-    LEFT JOIN cbo_fam AS ocupf ON LEFT(p.id_cbo_familia, 4) = ocupf.id_cbo_familia
-    LEFT JOIN tipo_vinculo ON p.id_tipo_vinculo = tipo_vinculo.codigo_tipo_vinculo
-    LEFT JOIN vinculo AS vinculacao ON p.id_vinculacao = vinculacao.id_vinculacao
-)
+    final as (
+        select
 
-select * from final
+            p.id_cnes,
+            p.data_registro,
+            hci.cpf,
+            p.profissional_cns as cns,
+            p.profissional_nome as nome,
+            vinculacao.descricao as vinculacao,
+            tipo_vinculo.descricao as vinculo_tipo,
+            p.id_cbo,
+            ocup.descricao as cbo,
+            p.id_cbo_familia,
+            ocupf.descricao as cbo_familia,
+            id_registro_conselho,
+            id_tipo_conselho,
+            carga_horaria_outros,
+            carga_horaria_hospitalar,
+            carga_horaria_ambulatorial,
+            carga_horaria_total,
+
+            p.ano_competencia,
+            p.mes_competencia,
+            parse_date('%Y-%m-%d', tipo_vinculo.data_particao) as data_particao,
+
+        from profissionais_mrj as p
+        left join
+            profissional_dados_hci as hci
+            on safe_cast(p.profissional_cns as int64)
+            = (select safe_cast(cns as int64) from unnest(hci.cns) as cns limit 1)
+        left join cbo as ocup on p.id_cbo = ocup.id_cbo
+        left join cbo_fam as ocupf on left(p.id_cbo_familia, 4) = ocupf.id_cbo_familia
+        left join tipo_vinculo on p.id_tipo_vinculo = tipo_vinculo.codigo_tipo_vinculo
+        left join vinculo as vinculacao on p.id_vinculacao = vinculacao.id_vinculacao
+    )
+
+select *
+from final
