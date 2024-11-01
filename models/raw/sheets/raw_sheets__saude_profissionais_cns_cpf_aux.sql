a766fc8eb39e19ab62d432988e135d3a23c36b8f
miloskimatheus <miloskimatheus@gmail.com>
2024-11-01 18:38:49 -0300

    lpadding

--- models/raw/sheets/raw_sheets__saude_profissionais_cns_cpf_aux.sql
+++ models/raw/sheets/raw_sheets__saude_profissionais_cns_cpf_aux.sql
@@ -12,9 +12,16 @@ with
         where cpf is not null and cpf != "" and cpf != "cpf"
     ),
 
+    tratados as (
+        select
+            lpad(safe_cast(cns as string), 15, "0") as cns,
+            lpad(safe_cast(cpf as string), 11, "0") as cpf,
+
+    ),
+
     cns_unicos as (
         select cns, array_agg(distinct cpf) as cpf, count(*) as qtd_cpfs
-        from source
+        from tratados
         group by cns
         order by qtd_cpfs desc
     )
