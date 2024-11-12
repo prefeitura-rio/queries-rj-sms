bbc2cdd1a4b09fee7212c38ac63bd75fdb2e8b4c
ThiagoTrabach <thiago.trabach@gmail.com>
2024-11-12 12:03:44 -0300

    refactor: modify dbt inovacation for faster build

--- recce.sh
+++ recce.sh
@@ -22,7 +22,7 @@ git checkout "$BRANCH_NAME"
 
 echo ""
 echo ">>>>> Executando dbt e gerando artefatos para os ambientes de trabalho atuais"
-dbt build -s "@state:modified" --state target-base/
+dbt build -s "state:modified+" --defer --state target-base/
 dbt docs generate
 
 echo ""
