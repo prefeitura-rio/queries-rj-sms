d31274b6e473c09e7bf31abbf4684616d1d490c2
ThiagoTrabach <thiago.trabach@gmail.com>
2024-12-05 19:28:28 -0300

    fix target path to compiled models

--- tools/run-diff.sh
+++ tools/run-diff.sh
@@ -14,7 +14,7 @@ git checkout master
 
 echo ""
 echo ">>>>> Gerando artefatos para o ambiente base em 'target-base'"
-dbt docs generate --target prod --target-path .target-base/
+dbt docs generate --target prod --target-path target-base/
 
 echo ""
 echo ">>>>> Checkout na branch '$BRANCH_NAME'"
@@ -22,4 +22,4 @@ git checkout "$BRANCH_NAME"
 
 echo ""
 echo ">>>>> Executando dbt"
-dbt run -s "state:modified+" --defer --state .target-base/
+dbt run -s "state:modified+" --defer --state target-base/
