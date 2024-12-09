5f8cc28b244be70f57e6b686379749723e09e88a
Pedro Marques <pedrovitor.marques@gmail.com>
2024-12-09 19:46:54 -0300

    refactor: update run-diff scripts to accept additional parameters and change target path for dbt artifacts

--- tools/run-diff.ps1
+++ tools/run-diff.ps1
@@ -1,23 +1,33 @@
-# Verifica se um argumento foi passado
-if (-not $args[0]) {
-    Write-Host "Uso: .\script.ps1 <nome-da-branch>"
+param (
+    [string]$BranchName,
+    [string]$Target = "dev", # Define o valor padrão como 'dev'
+    [string]$FullRefresh = $null
+)
+
+if (-not $BranchName) {
+    Write-Host "Uso: .\script.ps1 -BranchName <nome-da-branch> [-Target <dev|ci>] [-FullRefresh full_refresh]"
     exit 1
 }
 
-$BranchName = $args[0]
+if ($FullRefresh -eq "full_refresh") {
+    $FullRefreshFlag = "--full-refresh"
+} else {
+    $FullRefreshFlag = ""
+}
 
 Write-Host ""
 Write-Host ">>>>> Checkout na branch 'master'"
 git checkout master
+git pull
 
 Write-Host ""
-Write-Host ">>>>> Gerando artefatos para o ambiente base em 'target-base'"
-dbt docs generate --target prod --target-path .target-base/
+Write-Host ">>>>> Gerando artefatos para o ambiente base em '.state/'"
+dbt docs generate --target prod --target-path .state/
 
 Write-Host ""
 Write-Host ">>>>> Checkout na branch '$BranchName'"
 git checkout $BranchName
 
 Write-Host ""
-Write-Host ">>>>> Executando dbt"
-dbt run -s "state:modified+" --defer --state .target-base/ --target ci
+Write-Host ">>>>> Executando dbt com target '$Target'"
+dbt run -s "state:modified+" --defer --state .state/ --target $Target $FullRefreshFlag
--- tools/run-diff.sh
+++ tools/run-diff.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# Verifica se um argumento foi passado
+# Verifica se pelo menos um argumento foi passado
 if [ -z "$1" ]; then
-  echo "Uso: $0 <nome-da-branch>"
+  echo "Uso: $0 <nome-da-branch> [target=dev] [full_refresh]"
   exit 1
 fi
 
 BRANCH_NAME=$1
+TARGET=${2:-dev} # Define o valor padrão como 'dev'
+FULL_REFRESH=""
+
+# Configura a flag full_refresh se o terceiro parâmetro for passado
+if [ "$3" == "full_refresh" ]; then
+  FULL_REFRESH="--full-refresh"
+fi
 
 echo ""
 echo ">>>>> Checkout na branch 'master'"
@@ -15,12 +22,12 @@ git pull
 
 echo ""
 echo ">>>>> Gerando artefatos para o ambiente base em 'target-base'"
-dbt docs generate --target prod --target-path target-base/
+dbt docs generate --target prod --target-path .state/
 
 echo ""
 echo ">>>>> Checkout na branch '$BRANCH_NAME'"
 git checkout "$BRANCH_NAME"
 
 echo ""
-echo ">>>>> Executando dbt"
-dbt run -s "state:modified+" --defer --state target-base/  --full-refresh
+echo ">>>>> Executando dbt com target '$TARGET'"
+dbt run -s "state:modified+" --defer --state .state/ --target "$TARGET" $FULL_REFRESH
