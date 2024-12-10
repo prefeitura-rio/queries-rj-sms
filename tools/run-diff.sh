95d478223c9a97df89673d3f0df3c0194ba30516
Pedro Marques <pedrovitor.marques@gmail.com>
2024-12-10 07:59:50 -0300

    refactor: update run-diff.sh script to improve command execution logging, enhance error messages, and standardize output steps

--- tools/run-diff.sh
+++ tools/run-diff.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
-set -euo pipefail
 
 # Verifica se pelo menos um argumento foi passado
-if [ -z "$1" ]; then
-  echo "Uso: $0 <nome-da-branch> [target=dev] [full_refresh]"
+if [ $# -lt 1 ]; then
+  echo "Uso: $0 <BranchName> [Target=dev] [FullRefresh]"
   exit 1
 fi
 
-BRANCH_NAME=$1
-TARGET=${2:-dev} # Define o valor padrão como 'dev'
-FULL_REFRESH=""
+# Captura parâmetros
+BranchName="$1"
+Target="${2:-dev}" # Define o valor padrão como 'dev'
+FullRefresh="${3:-}"
 
-# Configura a flag full_refresh se o terceiro parâmetro for passado
-if [ "$3" == "full_refresh" ]; then
-  FULL_REFRESH="--full-refresh"
+# Configura a flag full_refresh
+if [ "$FullRefresh" == "full_refresh" ]; then
+  FullRefreshFlag="--full-refresh"
+else
+  FullRefreshFlag=""
 fi
 
+# Função para executar comandos e verificar falhas manualmente
+run_command() {
+  local command="$1"
+  echo "Executing: $command"
+  eval "$command"
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Command failed: $command"
+    echo "Terminating process."
+    return 1
+  fi
+}
+
+# Etapas do script
 echo ""
-echo ">>>>> Checkout na branch 'master'"
-git checkout master
-git pull
+echo "STEP 1"
+echo ">>>> CHECKING OUT 'master' BRANCH"
+run_command "git checkout master" 
+run_command "git pull" 
 
 echo ""
-echo ">>>>> Gerando artefatos para o ambiente base em 'target-base'"
-dbt docs generate --target prod --target-path .state/
+echo "STEP 2"
+echo ">>>> GENERATING STATE '.state/' BASED ON 'master' BRANCH"
+run_command "dbt docs generate --target prod --target-path .state/" 
 
 echo ""
-echo ">>>>> Checkout na branch '$BRANCH_NAME'"
-git checkout "$BRANCH_NAME"
+echo "STEP 3"
+echo ">>>> CHECKING OUT BRANCH '$BranchName'"
+run_command "git checkout $BranchName" 
 
 echo ""
-echo ">>>>> Executando dbt com target '$TARGET'"
-dbt run -s "state:modified+" --defer --state .state/ --target "$TARGET" $FULL_REFRESH
+echo "STEP 4"
+echo ">>>> EXECUTING DBT MATERIALIZATIONS"
+echo ">>>>>>> TARGET: $Target"
+echo ">>>>>>> FULL REFRESH: $FullRefreshFlag"
+run_command "dbt run -s 'state:modified+' --defer --state .state/ --target $Target $FullRefreshFlag"
+
+echo "Script completed successfully."
