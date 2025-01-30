7fb37f47a63e9d599de1cf750731f416fa44aaa4
vitmesquita <vitoria.leite.ismart@gmail.com>
2025-01-30 18:09:23 -0300

    changing test severity

--- tests/generic/assert_episodios_com_paciente_cadastrado.sql
+++ tests/generic/assert_episodios_com_paciente_cadastrado.sql
@@ -12,12 +12,25 @@ child as (
     select
         {{ column_name }} as id
     from {{ model }}
+),
+error_threshold as (
+    select
+        cast(count(distinct id) * 0.1 as int) as threshold
+    from parent
+),
+test as (
+    select count(*) as not_matched_rows
+    from child 
+    where child.id is not null
+    and child.id not in (select id from parent)
 )
-
-select distinct id
-from child
-where id is not null
-  and id not in (select id from parent)
+select 
+    case 
+        when not_matched_rows = 0 then 0
+        when not_matched_rows > 0 and not_matched_rows <= (select threshold from error_threshold) then 1
+        when not_matched_rows > (select threshold from error_threshold) then 2
+    end as response 
+from test
 
 {% endtest %}
 
