8a195eb9a1c968852a826c682a8e0f3fb327d9ac
miloskimatheus <miloskimatheus@gmail.com>
2025-02-21 16:25:16 -0300

    improves model documentation; adds tags; transform values to its correct types

--- models/raw/minhasaude_mongodb/raw_minhasaude_mongodb__modulos_perfil_acessos.sql
+++ models/raw/minhasaude_mongodb/raw_minhasaude_mongodb__modulos_perfil_acessos.sql
@@ -1,15 +1,36 @@
-{{ config(alias="modulos_perfil_acessos") }}
-
+{{
+    config(
+        schema="brutos_minhasaude_mongodb",
+        alias="modulos_perfil_acessos",
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "month",
+        },
+    )
+}}
 
 with
     source as (
-        select *
-        from
-            {{
-                source(
-                    "brutos_minhasaude_mongodb_staging", "modulos_perfil_acessos"
-                )
-            }}
+        select
+            _id,
+
+            rota,
+            nome,
+            descricao,
+
+            safe_cast(createdat as datetime) as createdat,
+            safe_cast(updatedat as datetime) as updatedat,
+
+            __v,
+
+            safe_cast(data_extracao as datetime) as data_extracao,
+            safe_cast(ano_particao as int64) as ano_particao,
+            safe_cast(mes_particao as int64) as mes_particao,
+            safe_cast(data_particao as date) as data_particao
+        from {{ source("brutos_minhasaude_mongodb_staging", "modulos_perfil_acessos") }}
     )
+
 select *
 from source
+where data_extracao = (select max(data_extracao) from source)
--- models/raw/minhasaude_mongodb/raw_minhasaude_mongodb__perfil_acessos.sql
+++ models/raw/minhasaude_mongodb/raw_minhasaude_mongodb__perfil_acessos.sql
@@ -1,15 +1,41 @@
-{{ config(alias="perfil_acessos") }}
-
+{{
+    config(
+        schema="brutos_minhasaude_mongodb",
+        alias="perfil_acessos",
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "month",
+        },
+    )
+}}
 
 with
     source as (
-        select *
-        from
-            {{
-                source(
-                    "brutos_minhasaude_mongodb_staging", "perfil_acessos"
-                )
-            }}
+        select
+            _id,
+            idmodulo,
+            idusuario,
+
+            nome,
+            lpad(cpf, 11, '0') as cpf,
+            lpad(cns, 15, '0') as cns,
+
+            safe_cast(logingovbr as bool) as logingovbr,
+            origem,
+
+            safe_cast(createdat as datetime) as createdat,
+            safe_cast(updatedat as datetime) as updatedat,
+
+            __v,
+
+            safe_cast(data_extracao as datetime) as data_extracao,
+            safe_cast(ano_particao as int64) as ano_particao,
+            safe_cast(mes_particao as int64) as mes_particao,
+            safe_cast(data_particao as date) as data_particao
+        from {{ source("brutos_minhasaude_mongodb_staging", "perfil_acessos") }}
     )
+
 select *
 from source
+where data_extracao = (select max(data_extracao) from source)
