d8775471a1f3ddd95cacd6b367c8ed2974eb7695
vitmesquita <vitoria.leite.ismart@gmail.com>
2025-05-13 10:30:20 -0300

    adding validation test

---
+++ tests/raw/mart_sheets__usuarios_permitidos__validacao.sql
@@ -0,0 +1,33 @@
+-- Testa se tabela manual esta preenchida corretamente
+with
+    acessos_manuais as (
+        select * from {{ ref("raw_sheets__usuarios_permitidos_hci") }}
+    ),
+    estabelecimentos as (
+        select
+            id_cnes,
+            area_programatica,
+            tipo_sms_simplificado,
+            nome_limpo as unidade_nome
+        from {{ ref("dim_estabelecimento") }}
+    ),
+
+    acessos_cnes_obrigatorio_sem_cnes as (
+        select * 
+        from acessos_manuais 
+        where unidade is null 
+        and nivel_de_acesso in ('only_from_same_cnes','only_from_same_ap')
+    ),
+
+    acessos_cnes_obrigatorio_sem_cnes_valido as (
+        select acessos_manuais.* 
+        from acessos_manuais
+        left join estabelecimentos
+            on acessos_manuais.unidade = estabelecimentos.id_cnes
+        where nivel_de_acesso in ('only_from_same_cnes','only_from_same_ap') 
+        and estabelecimentos.area_programatica is null
+    )
+
+select * from acessos_cnes_obrigatorio_sem_cnes
+union all
+select * from acessos_cnes_obrigatorio_sem_cnes_valido
