860d39c038cea3ce65429178d8424f2e232928b2
vitmesquita <vitoria.leite.ismart@gmail.com>
2025-05-23 17:14:36 -0300

    add restricted patient model and enrich flag

---
+++ models/intermediate/dit/historico_clinico/episodio/int_historico_clinico__paciente__restrito.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        schema="intermediario_historico_clinico",
+        alias="paciente_restrito",
+        materialized="table",
+    )
+}}
+with source as (
+    select * 
+    from {{ source("intermediario_historico_clinico_staging", "pacientes_restritos") }}
+)
+
+select * 
+from source
+where flag_gemini = '1'
+or flag_gemini = '0'
