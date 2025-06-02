e4de3a869b0fb5c757268b0b098795da3374f090
miloskimatheus <miloskimatheus@gmail.com>
2025-06-02 18:13:00 -0300

    refact: moves area_programatica raw model to sheets folder

--- models/raw/sheets/raw_sheets__area_programatica_bairros_aps.sql
+++ models/raw/sheets/raw_sheets__area_programatica_bairros_aps.sql
@@ -3,7 +3,7 @@
 with
     source as (
         select bairro, ap, ap_titulo
-        from {{ source("brutos_sheets_staging", "bairros_rio ") }}
+        from {{ source("brutos_sheets_staging", "bairros_rio") }}
     )
 
 select *
