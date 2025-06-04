fed4e9f7bc01fabe3007fb79f00a96cb0dd2199a
Daniel Lira <daniellira@Daniels-MacBook-Air.local>
2025-06-04 11:16:06 -0300

    feat: indicadores model

---
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__indicador.sql
@@ -0,0 +1,52 @@
+{{
+    config(
+        alias="indicadores", 
+        materialized="table",
+        schema="brutos_prontuario_vitacare_historico",
+    )
+}}
+
+WITH
+
+    source_indicadores AS (
+        SELECT 
+            CONCAT(
+                NULLIF({{ remove_double_quotes('id_cnes') }}, ''), 
+                '.',
+                NULLIF({{ remove_double_quotes('acto_id') }}, '')
+            ) AS id_prontuario_global,
+            *
+        FROM {{ source('brutos_vitacare_historic_staging', 'INDICADORES') }} 
+    ),
+
+
+      -- Using window function to deduplicate indicadores
+    indicadores_deduplicados AS (
+        SELECT
+            *
+        FROM (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (PARTITION BY id_prontuario_global ORDER BY extracted_at DESC) AS rn
+            FROM source_indicadores
+        )
+        WHERE rn = 1
+    ),
+
+    fato_indicadores AS (
+        SELECT
+            -- PKs e Chaves
+            id_prontuario_global,
+            {{ remove_double_quotes('acto_id') }} AS id_prontuario_local,
+            {{ remove_double_quotes('id_cnes') }} AS cnes_unidade,
+
+            {{ remove_double_quotes('indicadores_nome') }} AS indicadores_nome,
+            {{ remove_double_quotes('valor') }} AS valor,
+   
+            {{ remove_double_quotes('extracted_at') }} AS extracted_at
+        FROM indicadores_deduplicados
+    )
+
+SELECT
+    *
+FROM fato_indicadores
