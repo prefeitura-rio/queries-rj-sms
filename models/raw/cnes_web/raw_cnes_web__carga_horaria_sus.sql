51f712d4749234e976d7454a2a4a9428702037ed
herianc <herian.cavalcante@live.com>
2025-06-04 10:30:14 -0300

    Revert "fix: deduplicating the carga_horaria_sus table"
    This reverts commit 09a805acb422776bc17ee1201f85a19fb1baed39.

--- models/raw/cnes_web/raw_cnes_web__carga_horaria_sus.sql
+++ models/raw/cnes_web/raw_cnes_web__carga_horaria_sus.sql
@@ -13,6 +13,7 @@ source as (
     from {{ source("brutos_cnes_web_staging", "tbCargaHorariaSus") }}
 ),
 
+
 renamed as (
     select
         {{ process_null('co_unidade') }} as id_unidade,
@@ -21,8 +22,8 @@ renamed as (
         if (tp_sus_nao_sus = 'S', true, false) as atende_sus_indicador,
         {{ process_null('ind_vinculacao') }} as vinculacao,
         {{ process_null('qt_carga_horaria_ambulatorial') }} as carga_horaria_ambulatorial,
-        {{ process_null('qt_carga_hor_hosp_sus') }} as carga_horaria_hospitalar,
         {{ process_null('qt_carga_horaria_outros') }} as carga_horaria_outros,
+        {{ process_null('qt_carga_hor_hosp_sus') }} as carga_horaria_hospitalar,
         {{ process_null('co_conselho_classe') }} as conselho_tipo,
         {{ process_null('nu_registro') }} as id_registro_conselho,
         {{ process_null('sg_uf_crm') }} as sigla_uf_crm,
@@ -33,18 +34,10 @@ renamed as (
         safe_cast(dt_atualizacao as date format 'DD/MM/YYYY') as data_atualizacao,
         {{ process_null('ano_particao') }} as ano_particao,
         {{ process_null('mes_particao') }} as mes_particao,
-        concat(ano_particao, '-', mes_particao, '-', '01') as data_particao,
         cast(_data_carga as datetime) as data_carga,
         {{ process_null('_data_snapshot')}} as data_snapshot
 
     from source
-    qualify row_number() over (
-        partition by 
-            id_unidade, 
-            id_profissional_sus, 
-            vinculacao
-        order by data_particao desc
-    ) = 1
 )
 
 select * from renamed
