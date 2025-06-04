e732f5a9afdbe675e554fe12dbc87d6988cbe8ec
Daniel Lira <daniellira@Daniels-MacBook-Air.local>
2025-06-04 10:29:32 -0300

    feat: condicoes model

---
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__condicao.sql
@@ -0,0 +1,53 @@
+{{
+    config(
+        alias="condicoes", 
+        materialized="table",
+        schema="brutos_prontuario_vitacare_historico",
+    )
+}}
+
+WITH
+
+    source_condicoes AS (
+        SELECT 
+            CONCAT(
+                NULLIF({{ remove_double_quotes('id_cnes') }}, ''), 
+                '.',
+                NULLIF({{ remove_double_quotes('acto_id') }}, '')
+            ) AS id_prontuario_global,
+            *
+        FROM {{ source('brutos_vitacare_historic_staging', 'CONDICOES') }} 
+    ),
+
+
+      -- Using window function to deduplicate condicoes
+    condicoes_deduplicados AS (
+        SELECT
+            *
+        FROM (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (PARTITION BY id_prontuario_global ORDER BY extracted_at DESC) AS rn
+            FROM source_condicoes
+        )
+        WHERE rn = 1
+    ),
+
+    fato_condicoes AS (
+        SELECT
+            -- PKs e Chaves
+            id_prontuario_global,
+            {{ remove_double_quotes('acto_id') }} AS id_prontuario_local,
+            {{ remove_double_quotes('id_cnes') }} AS cnes_unidade,
+
+            {{ remove_double_quotes('cod_cid10') }} AS cod_cid10,
+            {{ remove_double_quotes('estado') }} AS estado,
+            {{ remove_double_quotes('data_diagnostico') }} AS data_diagnostico,
+   
+            {{ remove_double_quotes('extracted_at') }} AS extracted_at
+        FROM condicoes_deduplicados
+    )
+
+SELECT
+    *
+FROM fato_condicoes
