e0e0f14525f0f31c7dd5ea4d60a2364db885792d
Daniel Lira <daniellira@Daniels-MacBook-Air.local>
2025-06-04 09:54:27 -0300

    atendimentos model

---
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare__atendimento.sql
@@ -0,0 +1,71 @@
+{{
+    config(
+        alias="atendimentos", 
+        materialized="table",
+        schema="brutos_prontuario_vitacare_historico",
+    )
+}}
+
+WITH
+
+    source_atendimentos AS (
+        SELECT 
+            CONCAT(
+                NULLIF({{ remove_double_quotes('id_cnes') }}, ''), 
+                '.',
+                NULLIF({{ remove_double_quotes('acto_id') }}, '')
+            ) AS id_prontuario_global,
+            *
+        FROM {{ source('brutos_vitacare_historic_staging', 'ATENDIMENTOS') }} 
+    ),
+
+
+      -- Using window function to deduplicate atendimentos
+    atendimentos_deduplicados AS (
+        SELECT
+            *
+        FROM (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (PARTITION BY id_prontuario_global, cod_vacina, lote ORDER BY extracted_at DESC) AS rn
+            FROM source_atendimentos
+        )
+        WHERE rn = 1
+    ),
+
+    fato_atendimentos AS (
+        SELECT
+            -- PKs e Chaves
+            id_prontuario_global,
+            {{ remove_double_quotes('acto_id') }} AS id_prontuario_local,
+            {{ remove_double_quotes('id_cnes') }} AS cnes_unidade,
+            {{ remove_double_quotes('unidade_ap') }} AS unidade_ap,
+            {{ remove_double_quotes('patient_cpf') }} AS patient_cpf,
+            {{ remove_double_quotes('patient_code') }} AS patient_code,
+            {{ remove_double_quotes('profissional_cns') }} AS profissional_cns,
+            {{ remove_double_quotes('profissional_cpf') }} AS profissional_cpf,
+            {{ remove_double_quotes('profissional_nome') }} AS profissional_nome,
+            {{ remove_double_quotes('diprofissional_cboff') }} AS profissional_cbo,
+            {{ remove_double_quotes('profissional_cbo_descricao') }} AS profissional_cbo_descricao,
+            {{ remove_double_quotes('profissional_equipe_nome') }} AS profissional_equipe_nome,
+            {{ remove_double_quotes('profissional_equipe_cod_ine') }} AS profissional_equipe_cod_ine,
+            {{ remove_double_quotes('datahora_inicio_atendimento') }} AS datahora_inicio_atendimento,
+            {{ remove_double_quotes('datahora_fim_atendimento') }} AS datahora_fim_atendimento,
+            {{ remove_double_quotes('datahora_marcacao_atendimento') }} AS datahora_marcacao_atendimento,
+            {{ remove_double_quotes('eh_coleta') }} AS eh_coleta,
+            {{ remove_double_quotes('subjetivo_motivo') }} AS subjetivo_motivo,
+            {{ remove_double_quotes('plano_observacoes') }} AS plano_observacoes,
+            {{ remove_double_quotes('avaliacao_observacoes') }} AS avaliacao_observacoes,
+            {{ remove_double_quotes('objetivo_descricao') }} AS objetivo_descricao,
+            {{ remove_double_quotes('notas_observacoes') }} AS notas_observacoes,
+            {{ remove_double_quotes('ut_id') }} AS ut_id,
+            {{ remove_double_quotes('realizado') }} AS realizado,
+            {{ remove_double_quotes('tipo_atendimento') }} AS tipo_atendimento,
+            
+            {{ remove_double_quotes('extracted_at') }} AS extracted_at
+        FROM atendimentos_deduplicados
+    )
+
+SELECT
+    *
+FROM fato_atendimentos
