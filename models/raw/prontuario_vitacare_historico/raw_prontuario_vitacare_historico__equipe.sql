028473fe53061a4d6e468d24d637a399a0cd3ee2
Daniel Lira <daniellira@Daniels-MacBook-Air.local>
2025-06-04 10:52:52 -0300

    feat: equipe model

---
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__equipe.sql
@@ -0,0 +1,53 @@
+{{
+    config(
+        alias="equipes", 
+        materialized="table",
+        schema="brutos_prontuario_vitacare_historico",
+    )
+}}
+
+WITH
+
+    source_equipes AS (
+        SELECT 
+            CONCAT(
+                NULLIF({{ remove_double_quotes('id_cnes') }}, ''), 
+                '.',
+                NULLIF({{ remove_double_quotes('id') }}, '')
+            ) AS id_equipe_global,
+            *
+        FROM {{ source('brutos_vitacare_historic_staging', 'EQUIPES') }} 
+    ),
+
+
+      -- Using window function to deduplicate equipes
+    equipes_deduplicados AS (
+        SELECT
+            *
+        FROM (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (PARTITION BY id_equipe_global ORDER BY extracted_at DESC) AS rn
+            FROM source_equipes
+        )
+        WHERE rn = 1
+    ),
+
+    fato_equipes AS (
+        SELECT
+            -- PKs e Chaves
+            id_equipe_global,
+            {{ remove_double_quotes('id_cnes') }} AS cnes_unidade,
+            
+            REPLACE({{ remove_double_quotes('id') }}, '.0', '') AS id,
+            {{ remove_double_quotes('codigo') }} AS codigo,
+            {{ remove_double_quotes('nome') }} AS nome,
+            {{ remove_double_quotes('n_ine') }} AS n_ine,
+   
+            {{ remove_double_quotes('extracted_at') }} AS extracted_at
+        FROM equipes_deduplicados
+    )
+
+SELECT
+    *
+FROM fato_equipes
