646bd6d32888b9fdf8526e2c3df285e86128b7c3
Pedro <pedrovitor.marques@gmail.com>
2025-06-12 15:37:16 -0300

    refactor: simplify phone number handling in whatsapp agendamentos and csat models; update date filtering logic

--- models/marts/iplanrio/whatsapp/mart_iplanrio__whatsapp_agendamentos.sql
+++ models/marts/iplanrio/whatsapp/mart_iplanrio__whatsapp_agendamentos.sql
@@ -11,89 +11,10 @@
     )
 }}
 
-WITH
-
--- ------------------------------------------------------------
--- Telefones cadastrados
--- ------------------------------------------------------------
-telefones_cadastros as (
-  select 
-    cpf as paciente_cpf,
-    numero_telefone.ddd as telefone_ddd,
-    numero_telefone.valor as telefone_valor,
-    numero_telefone.rank as telefone_rank
-  from {{ ref('mart_historico_clinico__paciente') }},
-    unnest(contato.telefone) as numero_telefone
-),
-
--- ------------------------------------------------------------
--- Telefones do SISREG
--- ------------------------------------------------------------
-telefones_sisreg as (
-  select 
-    paciente_cpf,
-    regexp_extract(telefone, r'^\(\d{2}\)', 1) as telefone_ddd,
-    regexp_replace(telefone, r'^\(\d{2}\)', '') as telefone_valor,
-    0 as telefone_rank
-  FROM {{ ref("raw_sisreg_api__marcacoes") }},
-    unnest(split(paciente_telefone, ',')) telefone
-),
-telefones_sisreg_padronizados as (
-  select 
-    paciente_cpf,
-    REGEXP_REPLACE(telefone_ddd, r'[()]', '') AS telefone_ddd,
-    {{ padronize_telefone('telefone_valor') }} as telefone_valor,
-    telefone_rank
-  FROM telefones_sisreg
-),
-
--- ------------------------------------------------------------
--- Unificação e Filtro de telefones
--- ------------------------------------------------------------
-todos_telefones as (
-  select * from telefones_cadastros
-  union all
-  select * from telefones_sisreg_padronizados
-),
-
-telefones_filtrados as (
-  select 
-    *
-  from todos_telefones
-  where
-    telefone_ddd = '21' and
-    length(telefone_valor) = 9
-),
-
-telefones_ordenados as (
-  select 
-    *
-  from telefones_filtrados
-  order by telefone_rank asc nulls last
-),
-
--- ------------------------------------------------------------
--- Agrupamentos de telefones
--- ------------------------------------------------------------
-telefone_por_cpf as (
-  select 
-    paciente_cpf,
-    array_agg(
-      struct(
-        telefone_ddd,
-        telefone_valor,
-        telefone_rank
-      )
-    ) as telefones
-  from telefones_ordenados
-  group by 1
-)
-
 SELECT
   struct(
     marcacoes.paciente_cpf as cpf,
-    marcacoes.paciente_nome as nome,
-    telefone_por_cpf.telefones[offset(0)] as telefone
+    marcacoes.paciente_nome as nome
   ) as paciente,
 
   procedimento_sigtap as nome_procedimento_geral,
@@ -111,17 +32,9 @@ SELECT
     {{ padronize_telefone('unidade_executante_telefone') }} as telefone
   ) as unidade
 FROM {{ ref("raw_sisreg_api__marcacoes") }} marcacoes
-    left join telefone_por_cpf on telefone_por_cpf.paciente_cpf = marcacoes.paciente_cpf
 WHERE 
   solicitacao_status in (
       "SOLICITAÇÃO / AGENDADA / COORDENADOR",
-      "SOLICITAÇÃO / AGENDADA / SOLICITANTE",
-      "AGENDAMENTO / PENDENTE CONFIRMAÇÃO / EXECUTANTE",
-      "AGENDAMENTO / CONFIRMADO / EXECUTANTE"
-  ) and
-  data_marcacao > current_timestamp() and
-  marcacao_executada != "1" and
-  unidade_executante_id in (
-    SELECT id_cnes
-    FROM {{ ref("dim_estabelecimento") }}
-  )
+      "SOLICITAÇÃO / AGENDADA / SOLICITANTE"
+  ) 
+  and safe_cast(data_marcacao as date) >= current_date()
