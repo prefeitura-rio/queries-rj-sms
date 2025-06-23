a60ca2e6fcc7fc368729ecbd84f38941db1d3ec9
Daniel Lira <danielllira@icloud.com>
2025-06-23 16:20:42 -0300

    feat: paciente e ficha_a

---
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__ficha_a.sql
@@ -0,0 +1,103 @@
+-- models/ficha_a.sql
+{{
+    config(
+        alias="ficha_a",
+        materialized="table",
+        schema="brutos_prontuario_vitacare_staging",
+    )
+}}
+
+WITH
+
+    source_cadastro AS (
+        SELECT
+            *
+        FROM {{ ref('raw_prontuario_vitacare_historico__cadastro') }}
+    ),
+
+    selecao_ficha_a AS (
+        SELECT
+            -- PK
+            CONCAT(
+                NULLIF(cnes_unidade, ''), 
+                '.',
+                NULLIF(ut_id, '')
+            ) AS id,
+            cpf AS cpf,
+            ut_id AS id_paciente,
+            npront AS numero_prontuario,
+
+            unidade AS unidade_cadastro, 
+            ap AS ap_cadastro,
+
+            nome AS nome,
+            sexo AS sexo,
+            obito AS obito,
+            bairro AS bairro,
+            comodos AS comodos,
+            nomemae AS nome_mae,
+            nomepai AS nome_pai,
+            racacor AS raca_cor,
+            ocupacao AS ocupacao,
+            religiao AS religiao,
+            telefone AS telefone,
+            ineequipe AS ine_equipe,
+            microarea AS microarea,
+            logradouro AS logradouro,
+            nomesocial AS nome_social,
+            destinolixo AS destino_lixo,
+            luzeletrica AS luz_eletrica,
+            codigoequipe AS codigo_equipe,
+            datacadastro AS data_cadastro,
+            escolaridade AS escolaridade,
+            tempomoradia AS tempo_moradia,
+            nacionalidade AS nacionalidade,
+            rendafamiliar AS renda_familiar,
+            tipodomicilio AS tipo_domicilio,
+            dta_nasc AS data_nascimento,
+            paisnascimento AS pais_nascimento,
+            tipologradouro AS tipo_logradouro,
+            tratamentoagua AS tratamento_agua,
+            emsituacaoderua AS em_situacao_de_rua,
+            frequentaescola AS frequenta_escola,
+            meiostransporte AS meios_transporte,
+            situacaousuario AS situacao_usuario,
+            doencascondicoes AS doencas_condicoes,
+            estadonascimento AS estado_nascimento,
+            estadoresidencia AS estado_residencia,
+            identidadegenero AS identidade_genero,
+            meioscomunicacao AS meios_comunicacao,
+            orientacaosexual AS orientacao_sexual,
+            possuifiltroagua AS possui_filtro_agua,
+            possuiplanosaude AS possui_plano_saude,
+            situacaofamiliar AS situacao_familiar,
+            territoriosocial AS territorio_social,
+            abastecimentoagua AS abastecimento_agua,
+            animaisnodomicilio AS animais_no_domicilio,
+            cadastropermanente AS cadastro_permanente,
+            familialocalizacao AS familia_localizacao,
+            emcasodoencaprocura AS em_caso_doenca_procura,
+            municipionascimento AS municipio_nascimento,
+            municipioresidencia AS municipio_residencia,
+            responsavelfamiliar AS responsavel_familiar,
+            esgotamentosanitario AS esgotamento_sanitario,
+            situacaomoradiaposse AS situacao_moradia_posse,
+            situacaoprofissional AS situacao_profissional,
+            vulnerabilidadesocial AS vulnerabilidade_social,
+            familiabeneficiariacfc AS familia_beneficiaria_cfc,
+            dataatualizacaocadastro AS data_atualizacao_cadastro,
+            participagrupocomunitario AS participa_grupo_comunitario,
+            relacaoresponsavelfamiliar AS relacao_responsavel_familiar,
+            membrocomunidadetradicional AS membro_comunidade_tradicional,
+            dataatualizacaovinculoequipe AS data_atualizacao_vinculo_equipe,
+            familiabeneficiariaauxiliobrasil AS familia_beneficiaria_auxilio_brasil,
+            criancamatriculadacrechepreescola AS crianca_matriculada_creche_pre_escola,
+            updated_at AS updated_at,
+            extracted_at AS loaded_at 
+
+        FROM source_cadastro
+    )
+
+SELECT
+    *
+FROM selecao_ficha_a
---
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__paciente.sql
@@ -0,0 +1,89 @@
+-- models/pacientes.sql
+{{
+    config(
+        alias="paciente",
+        materialized="table",
+        schema="brutos_prontuario_vitacare_historico", 
+    )
+}}
+
+WITH
+
+    source_cadastro AS (
+        SELECT
+            *
+        FROM {{ ref('raw_prontuario_vitacare_historico__cadastro') }} 
+    ),
+
+    selecao_pacientes AS (
+        SELECT
+            -- PK
+            CONCAT(
+                NULLIF(cnes_unidade, ''), 
+                '.',
+                NULLIF(ut_id, '')
+            ) AS id,
+
+            -- Outras Chaves
+            cnes_unidade AS id_cnes,
+            ut_id AS id_local,
+            npront AS numero_prontuario,
+            cpf AS cpf,
+            dnv AS dnv,
+            nis AS nis,
+            cns AS cns,
+
+            -- Informações Pessoais
+            nome AS nome,
+            nomesocial AS nome_social,
+            nomemae AS nome_mae,
+            nomepai AS nome_pai,
+            obito AS obito,
+            sexo AS sexo,
+            orientacaosexual AS orientacao_sexual,
+            identidadegenero AS identidade_genero,
+            racacor AS raca_cor,
+
+            -- Informações Cadastrais
+            situacaousuario AS situacao,
+            cadastropermanente AS cadastro_permanente,
+            datacadastro AS data_cadastro_inicial,
+            dataatualizacaocadastro AS data_ultima_atualizacao_cadastral,
+
+            -- Nascimento
+            nacionalidade AS nacionalidade,
+            dta_nasc AS data_nascimento,
+            paisnascimento AS pais_nascimento,
+            municipionascimento AS municipio_nascimento,
+            estadonascimento AS estado_nascimento,
+
+            -- Contato
+            email AS email,
+            telefone AS telefone,
+
+            -- Endereço
+            tipodomicilio AS endereco_tipo_domicilio,
+            tipologradouro AS endereco_tipo_logradouro,
+            cep AS endereco_cep,
+            logradouro AS endereco_logradouro,
+            bairro AS endereco_bairro,
+            estadoresidencia AS endereco_estado,
+            municipioresidencia AS endereco_municipio,
+
+            -- Informações da Unidade
+            ap AS ap,
+            microarea AS microarea,
+            unidade AS nome_unidade, 
+            codigoequipe AS codigo_equipe_saude,
+            ineequipe AS codigo_ine_equipe_saude,
+            dataatualizacaovinculoequipe AS data_atualizacao_vinculo_equipe,
+
+
+            extracted_at
+
+        FROM source_cadastro
+    )
+
+SELECT
+    *
+FROM selecao_pacientes
