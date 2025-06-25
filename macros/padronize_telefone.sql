e244c8096af5c1f68b8f74fb3ab67b0ce92bc93d
herianc <herian.cavalcante@live.com>
2025-06-25 16:35:29 -0300

    feat: Adiciona verificação do tamanho do número de telefone

--- macros/padronize_telefone.sql
+++ macros/padronize_telefone.sql
@@ -2,10 +2,11 @@
     case
         when
             length(trim({{ telefone_column }})) = 0
+            or length(trim( {{telefone_column }} )) < 8 
             or trim({{ telefone_column }}) in ('NONE', 'NULL', '0', "()", "")
             or regexp_contains(trim({{ telefone_column }}), r'E\+\d+')  -- Remove scientific notation
             or regexp_contains(trim({{ telefone_column }}), r'[a-zA-Z]')  -- Remove numbers that contain letters
-            or length(safe.regexp_replace(trim( {{ telefone_column }} ), substr({{ telefone_column }}, 1, 1), '')) = 0 -- Remove repeated digits
+            or length(safe.regexp_replace(trim( {{ telefone_column }} ), substr({{ telefone_column }}, 1, 1), '')) = 0 -- Remove repeated digits,
         then null
         else
             regexp_replace(
