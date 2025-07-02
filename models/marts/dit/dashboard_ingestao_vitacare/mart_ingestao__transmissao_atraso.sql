27c89ff01308aaab7f14409bede5a568fa7c69ab
Matheus Avellar <matheus.avellar@dados.rio>
2025-07-02 11:31:39 -0300

    chore: finaliza atrasos

--- models/marts/dit/dashboard_ingestao_vitacare/mart_ingestao__transmissao_atraso.sql
+++ models/marts/dit/dashboard_ingestao_vitacare/mart_ingestao__transmissao_atraso.sql
@@ -5,19 +5,25 @@
     ) 
 }}
 
-WITH
+with
 
   -- -----------------------------
   -- Dados de Unidades
   -- -----------------------------
   unidades as (
-    select 
+    select
       id_cnes,
       area_programatica,
       nome_fantasia
     from {{ ref('dim_estabelecimento') }} est
-    where est.prontuario_versao = 'vitacare' and est.prontuario_episodio_tem_dado = 'sim'
+    where est.prontuario_versao = 'vitacare'
+      and est.prontuario_episodio_tem_dado = 'sim'
+      -- Hoje (2025-07-02) aparece uma linha com CNES nulo pro
+      -- "SMS HOSPITAL MUNICIPAL ALVARO RAMOS AP 40"; filtra:
+      and id_cnes is not null
+
     union all
+
     select 'nao-informado', 'nao-se-aplica', 'CNES não informado'
   ),
 
@@ -25,66 +31,90 @@ WITH
   -- Transmissões
   -- -----------------------------
   transmissoes_individuais_paciente as (
-    SELECT 
+    select
       source_id,
-      COALESCE(nullif(json_extract_scalar(trans.data,'$.cnes'), ''), 'nao-informado') AS id_cnes,
+      coalesce(nullif(json_extract_scalar(trans.data,'$.cnes'), ''), 'nao-informado') as id_cnes,
       safe_cast(
         TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as date
       ) as dia_ocorrencia,
-      safe_cast(datalake_loaded_at as date) AS dia_ingestao,
-    FROM {{ source('brutos_prontuario_vitacare_staging', 'paciente_continuo') }} trans
-    WHERE DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+      safe_cast(datalake_loaded_at as date) as dia_ingestao,
+
+      TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as momento_ocorrencia,
+      datalake_loaded_at as momento_ingestao,
+    from {{ source('brutos_prontuario_vitacare_staging', 'paciente_continuo') }} trans
+    where DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
   ),
   transmissoes_individuais_atendimento as (
-    SELECT 
+    select
       source_id,
-      COALESCE(nullif(json_extract_scalar(trans.data,'$.unidade_cnes'), ''), 'nao-informado') AS id_cnes,
+      coalesce(nullif(json_extract_scalar(trans.data,'$.unidade_cnes'), ''), 'nao-informado') as id_cnes,
       safe_cast(
         TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as date
       ) as dia_ocorrencia,
-      safe_cast(datalake_loaded_at as date) AS dia_ingestao,
-    FROM {{ source('brutos_prontuario_vitacare_staging', 'atendimento_continuo') }} trans
-    WHERE DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+      safe_cast(datalake_loaded_at as date) as dia_ingestao,
+
+      TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as momento_ocorrencia,
+      datalake_loaded_at as momento_ingestao,
+    from {{ source('brutos_prontuario_vitacare_staging', 'atendimento_continuo') }} trans
+    where DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
   ),
 
   -- -----------------------------
   -- Junção
   -- -----------------------------
   transmissoes_individuais as (
-    select *
+    select
+      *,
+      -- DATE_DIFF(dia_ingestao, dia_ocorrencia, DAY) as atraso_transmissao
+      TIMESTAMP_DIFF(momento_ingestao, momento_ocorrencia, MINUTE) as atraso_transmissao
     from transmissoes_individuais_paciente
+
     union all
-    select *
+
+    select *,
+      -- DATE_DIFF(dia_ingestao, dia_ocorrencia, DAY) as atraso_transmissao
+      TIMESTAMP_DIFF(momento_ingestao, momento_ocorrencia, DAY) as atraso_transmissao
     from transmissoes_individuais_atendimento
   ),
 
   transmissoes_agrupadas_mes as (
-    select id_cnes, round(avg(atraso_transmissao),2) as media_atraso, max(atraso_transmissao) as max_atraso
+    select
+      id_cnes,
+      round(avg(atraso_transmissao), 0) as media_atraso,
+      max(atraso_transmissao) as max_atraso
     from transmissoes_individuais
+    -- Não precisamos filtrar por data aqui porque já estamos pegando
+    -- só dados do último mês
     group by 1
   ),
-
   transmissoes_agrupadas_semana as (
-    select id_cnes, round(avg(atraso_transmissao),2) as media_atraso, max(atraso_transmissao) as max_atraso
+    select
+      id_cnes,
+      round(avg(atraso_transmissao), 0) as media_atraso,
+      max(atraso_transmissao) as max_atraso
     from transmissoes_individuais
-    WHERE DATE(momento_ingestao) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
+    where dia_ingestao >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
     group by 1
   ),
-
   transmissoes_agrupadas_dia as (
-    select id_cnes, round(avg(atraso_transmissao),2) as media_atraso, max(atraso_transmissao) as max_atraso
+    select
+      id_cnes,
+      round(avg(atraso_transmissao), 0) as media_atraso,
+      max(atraso_transmissao) as max_atraso
     from transmissoes_individuais
-    WHERE DATE(momento_ingestao) >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)
+    where dia_ingestao >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)
     group by 1
   )
-  select 
-    unidades.area_programatica,
-    transmissoes_agrupadas_mes.id_cnes,
-    unidades.nome_fantasia,
-    transmissoes_agrupadas_dia.media_atraso as media_atraso_dia,
-    transmissoes_agrupadas_semana.media_atraso as media_atraso_semana,
-    transmissoes_agrupadas_mes.media_atraso as media_atraso_mes
-  from unidades
-    left join transmissoes_agrupadas_mes using(id_cnes)
-    left join transmissoes_agrupadas_semana using(id_cnes)
-    left join transmissoes_agrupadas_dia using(id_cnes)
+
+select
+  unidades.area_programatica,
+  unidades.id_cnes,
+  unidades.nome_fantasia,
+  transmissoes_agrupadas_dia.media_atraso as media_atraso_dia,
+  transmissoes_agrupadas_semana.media_atraso as media_atraso_semana,
+  transmissoes_agrupadas_mes.media_atraso as media_atraso_mes
+from unidades
+  left join transmissoes_agrupadas_mes using(id_cnes)
+  left join transmissoes_agrupadas_semana using(id_cnes)
+  left join transmissoes_agrupadas_dia using(id_cnes)
+order by media_atraso_dia desc
