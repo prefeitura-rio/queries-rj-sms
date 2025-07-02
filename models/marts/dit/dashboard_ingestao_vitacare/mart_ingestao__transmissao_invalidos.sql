4598619b001cc5e0c6f9ce1932a38f0bbec0d26f
Matheus Avellar <matheus.avellar@dados.rio>
2025-07-02 10:50:18 -0300

    chore: finaliza problemas

--- models/marts/dit/dashboard_ingestao_vitacare/mart_ingestao__transmissao_invalidos.sql
+++ models/marts/dit/dashboard_ingestao_vitacare/mart_ingestao__transmissao_invalidos.sql
@@ -5,56 +5,78 @@
     ) 
 }}
 
-WITH
+with
   transmissoes_individuais as (
-    SELECT 
+    select
       source_id,
 
-      nullif(payload_cnes, '') AS cnes_payload,
-      nullif(json_extract_scalar(trans.data,'$.cnes'), '') AS cnes_json,
+      nullif(payload_cnes, '') as cnes_payload,
+      nullif(json_extract_scalar(trans.data, '$.cnes'), '') as cnes_json,
 
       safe_cast(TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as date) as dia_ocorrencia,
-      safe_cast(datalake_loaded_at as date) AS dia_ingestao,
+      safe_cast(datalake_loaded_at as date) as dia_ingestao,
+
+      datalake_loaded_at as momento_ingestao,
 
       'paciente' as tipo_registro
-    FROM {{ source('brutos_prontuario_vitacare_staging', 'paciente_continuo') }} trans
+    from {{ source('brutos_prontuario_vitacare_staging', 'paciente_continuo') }} trans
 
     union all
 
-    SELECT 
+    select
       source_id,
 
-      nullif(payload_cnes, '') AS cnes_payload,
+      nullif(payload_cnes, '') as cnes_payload,
       nullif(json_extract_scalar(trans.data,'$.unidade_cnes'), '') AS cnes_json,
 
       safe_cast(TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as date) as dia_ocorrencia,
-      safe_cast(datalake_loaded_at as date) AS dia_ingestao,
+      safe_cast(datalake_loaded_at as date) as dia_ingestao,
 
       datalake_loaded_at as momento_ingestao,
 
       'atendimento' as tipo_registro
-    FROM {{ source('brutos_prontuario_vitacare_staging', 'atendimento_continuo') }} trans
+    from {{ source('brutos_prontuario_vitacare_staging', 'atendimento_continuo') }} trans
   ),
 
   analise as (
-    select 
+    select
+      tipo_registro,
       source_id,
-      coalesce(cnes_payload, cnes_json, 'nao-informado') as cnes,
-      momento_ingestao,
 
-      CASE 
-        WHEN cnes_payload != cnes_json THEN 'divergencia-cnes'
-        WHEN cnes_payload is null or cnes_json is null THEN 'cnes-nao-informado'
-        WHEN dia_ocorrencia is null THEN 'dia-ocorrencia-nao-informado'
-        WHEN dia_ingestao != dia_ocorrencia THEN 'atraso-excessivo'
-        ELSE null
-      END as problema
+      coalesce(cnes_payload, cnes_json) as cnes,
+      case
+        when cnes_payload is null and cnes_json is null
+          then 'cnes-nao-informado'
+        when cnes_payload is null
+          then 'sem-cnes-payload'
+        when cnes_json is null
+          then 'sem-cnes-json'
+        when cnes_payload != cnes_json
+          then 'divergencia-cnes'
+        else null
+      end as problema_cnes,
+
+      dia_ingestao,
+      dia_ocorrencia,
+      case
+        when dia_ocorrencia is null
+          then null
+        else DATE_DIFF(dia_ingestao, dia_ocorrencia, DAY)
+      end as dias_atraso,
+      case
+        when dia_ocorrencia is null
+          then 'dia-ocorrencia-nao-informado'
+        when dia_ingestao != dia_ocorrencia
+          then 'atraso-excessivo'
+        else null
+      end as problema_data,
+
+      momento_ingestao
 
     from transmissoes_individuais
-    where problema is not null
   )
 
 select *
 from analise
-where problema is not null
+where problema_cnes is not null or problema_data is not null
 order by momento_ingestao desc
