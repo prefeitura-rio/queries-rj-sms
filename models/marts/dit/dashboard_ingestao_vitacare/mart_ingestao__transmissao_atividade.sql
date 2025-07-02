3757fe182cffc607f5da171f80802f1d9ee219fb
Matheus Avellar <matheus.avellar@dados.rio>
2025-07-02 12:04:24 -0300

    chore: finaliza atividades

--- models/marts/dit/dashboard_ingestao_vitacare/mart_ingestao__transmissao_atividade.sql
+++ models/marts/dit/dashboard_ingestao_vitacare/mart_ingestao__transmissao_atividade.sql
@@ -6,7 +6,7 @@
 }}
 
 
-WITH
+with
 
   -- -----------------------------
   -- Dados de Unidades
@@ -26,26 +26,26 @@ WITH
   -- Transmissões
   -- -----------------------------
   transmissoes_individuais_paciente as (
-    SELECT 
+    select
       source_id,
-      COALESCE(nullif(json_extract_scalar(trans.data,'$.cnes'), ''), 'nao-informado') AS id_cnes,
+      coalesce(nullif(json_extract_scalar(trans.data,'$.cnes'), ''), 'nao-informado') AS id_cnes,
       safe_cast(
         TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as date
       ) as dia_ocorrencia,
       safe_cast(datalake_loaded_at as date) AS dia_ingestao,
-    FROM {{ source('brutos_prontuario_vitacare_staging', 'paciente_continuo') }} trans
-    WHERE DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+    from {{ source('brutos_prontuario_vitacare_staging', 'paciente_continuo') }} trans
+    where DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
   ),
   transmissoes_individuais_atendimento as (
-    SELECT 
+    select
       source_id,
-      COALESCE(nullif(json_extract_scalar(trans.data,'$.unidade_cnes'), ''), 'nao-informado') AS id_cnes,
+      coalesce(nullif(json_extract_scalar(trans.data,'$.unidade_cnes'), ''), 'nao-informado') AS id_cnes,
       safe_cast(
         TIMESTAMP(DATETIME(source_updated_at), "America/Sao_Paulo") as date
       ) as dia_ocorrencia,
       safe_cast(datalake_loaded_at as date) AS dia_ingestao,
-    FROM {{ source('brutos_prontuario_vitacare_staging', 'atendimento_continuo') }} trans
-    WHERE DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+    from {{ source('brutos_prontuario_vitacare_staging', 'atendimento_continuo') }} trans
+    where DATE(datalake_loaded_at) >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
   ),
 
   -- -----------------------------
@@ -54,7 +54,9 @@ WITH
   transmissoes_individuais as (
     select *, 'paciente' as tipo_registro
     from transmissoes_individuais_paciente
+
     union all
+
     select *, 'atendimento' as tipo_registro
     from transmissoes_individuais_atendimento
   ),
@@ -71,18 +73,20 @@ WITH
   transmissoes_agrupadas_mes as (
     select id_cnes, tipo_registro, round(avg(quantidade),2) as quantidade
     from transmissoes_agrupadas
+    -- Não precisamos filtrar por data aqui porque só estamos pegando dados
+    -- do último mês já desde o começo
     group by 1, 2
   ),
   transmissoes_agrupadas_semana as (
     select id_cnes, tipo_registro, round(avg(quantidade),2) as quantidade
     from transmissoes_agrupadas
-    WHERE DATE(dia_ingestao) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
+    where DATE(dia_ingestao) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
     group by 1, 2
   ),
   transmissoes_agrupadas_dia as (
-    select id_cnes, tipo_registro, round(avg(quantidade),2) as quantidade
+    select id_cnes, tipo_registro, sum(quantidade) as quantidade
     from transmissoes_agrupadas
-    WHERE DATE(dia_ingestao) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+    where DATE(dia_ingestao) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
     group by 1, 2
   ),
 
@@ -91,15 +95,16 @@ WITH
   -- Analise p/ Registros de Pacientes
   -- -----------------------------
   analise_paciente as (
-    select 
+    select
       unidades.area_programatica,
-      transmissoes_agrupadas_mes.id_cnes,
+      unidades.id_cnes,
       unidades.nome_fantasia,
+
+      coalesce(dia.quantidade,0) as pacientes_ultimo_dia,
       struct(
-        coalesce(dia.quantidade,0) as ultimo_dia,
         coalesce(semana.quantidade,0) as ultima_semana,
         coalesce(mes.quantidade,0) as ultimo_mes
-      ) as quantidade_media_registros_paciente
+      ) as media_pacientes
     from unidades
       left join transmissoes_agrupadas_mes as mes using(id_cnes)
       left join transmissoes_agrupadas_semana as semana
@@ -108,23 +113,29 @@ WITH
       left join transmissoes_agrupadas_dia as dia
         on semana.id_cnes = dia.id_cnes
         and semana.tipo_registro = dia.tipo_registro
+    where semana.tipo_registro = 'paciente'
     order by id_cnes asc
   ),
   analise_atendimento as (
-    select 
+    select
       unidades.area_programatica,
-      mes.id_cnes,
+      unidades.id_cnes,
       unidades.nome_fantasia,
+
+      coalesce(dia.quantidade,0) as atendimentos_ultimo_dia,
       struct(
-        coalesce(transmissoes_agrupadas_dia.quantidade,0) as ultimo_dia,
-        coalesce(transmissoes_agrupadas_semana.quantidade,0) as ultima_semana,
-        coalesce(transmissoes_agrupadas_mes.quantidade,0) as ultimo_mes
-      ) as quantidade_media_registros_atendimento
+        coalesce(semana.quantidade,0) as ultima_semana,
+        coalesce(mes.quantidade,0) as ultimo_mes
+      ) as media_atendimentos
     from unidades
-      left join transmissoes_agrupadas_mes as mes using(id_cnes, tipo_registro)
-      left join transmissoes_agrupadas_semana using(id_cnes, tipo_registro)
-      left join transmissoes_agrupadas_dia using(id_cnes, tipo_registro)
-    where tipo_registro = 'atendimento'
+      left join transmissoes_agrupadas_mes as mes using(id_cnes)
+      left join transmissoes_agrupadas_semana as semana
+        on semana.id_cnes = mes.id_cnes
+        and semana.tipo_registro = mes.tipo_registro
+      left join transmissoes_agrupadas_dia as dia
+        on semana.id_cnes = dia.id_cnes
+        and semana.tipo_registro = dia.tipo_registro
+    where semana.tipo_registro = 'atendimento'
     order by id_cnes asc
   ),
 
@@ -132,9 +143,10 @@ WITH
   -- Junção Horizontal de Analises
   -- -----------------------------
   final as (
-    select 
+    select
       analise_paciente.*,
-      analise_atendimento.quantidade_media_registros_atendimento
+      analise_atendimento.atendimentos_ultimo_dia,
+      analise_atendimento.media_atendimentos
     from analise_paciente
       inner join analise_atendimento using(area_programatica, id_cnes, nome_fantasia)
     order by id_cnes asc
@@ -142,3 +154,4 @@ WITH
 
 select *
 from final
+order by area_programatica asc, id_cnes asc
