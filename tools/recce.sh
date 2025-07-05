46fb8e5fd362416b328ea8b33453ff08a72bfb8a
Pedro <pedrovitor.marques@gmail.com>
2025-07-05 08:57:33 -0300

    Update recce.sh

--- tools/recce.sh
+++ tools/recce.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
+# Cores ANSI
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # Sem cor
+
 # Verifica se um argumento foi passado
 if [ -z "$1" ]; then
-  echo "Uso: $0 <nome-da-branch>"
+  echo -e "${YELLOW}Uso: $0 <nome-da-branch>${NC}"
   exit 1
 fi
 
 BRANCH_NAME=$1
 
 echo ""
-echo ">>>>> Checkout na branch 'master'"
+echo -e "${BLUE}>>>>> Checkout na branch 'master'${NC}"
 git checkout master
 git pull
 
 echo ""
-echo ">>>>> Gerando artefatos para o ambiente base em 'target-base'"
+echo -e "${GREEN}>>>>> Gerando artefatos para o ambiente base em 'target-base'${NC}"
 dbt docs generate --target prod --target-path target-base/
 
 echo ""
-echo ">>>>> Checkout na branch '$BRANCH_NAME'"
+echo -e "${BLUE}>>>>> Checkout na branch '$BRANCH_NAME'${NC}"
 git checkout "$BRANCH_NAME"
 
 echo ""
-echo ">>>>> Executando dbt e gerando artefatos para os ambientes de trabalho atuais"
-dbt build -s "state:modified+" --defer --state target-base/
+echo -e "${GREEN}>>>>> Executando dbt e gerando artefatos para os ambientes de trabalho atuais${NC}"
+dbt build -s "state:modified+" --defer --full-refresh --state target-base/
 dbt docs generate
 
 echo ""
-echo ">>>>> Iniciando o servidor Recce"
+echo -e "${YELLOW}>>>>> Iniciando o servidor Recce${NC}"
 recce server
