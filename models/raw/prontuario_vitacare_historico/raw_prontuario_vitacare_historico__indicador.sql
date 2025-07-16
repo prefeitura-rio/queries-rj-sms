839c56c18fc88fd191cd3aaf97e6d96d68bd0078
herianc <herian.cavalcante@live.com>
2025-07-16 14:19:49 -0300

    fix: correção na lógica de deduplicação em tabelas

--- models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__indicador.sql
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__indicador.sql
@@ -29,13 +29,8 @@ WITH
     indicadores_deduplicados AS (
         SELECT
             *
-        FROM (
-            SELECT
-                *,
-                ROW_NUMBER() OVER (PARTITION BY id_prontuario_global ORDER BY extracted_at DESC) AS rn
-            FROM source_indicadores
-        )
-        WHERE rn = 1
+        FROM source_indicadores 
+        qualify row_number() over (partition by id_prontuario_global, indicadores_nome order by extracted_at desc) = 1
     ),
 
     fato_indicadores AS (
--- models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__prescricao.sql
+++ models/raw/prontuario_vitacare_historico/raw_prontuario_vitacare_historico__prescricao.sql
@@ -29,13 +29,8 @@ WITH
     prescricoes_deduplicados AS (
         SELECT
             *
-        FROM (
-            SELECT
-                *,
-                ROW_NUMBER() OVER (PARTITION BY id_prontuario_global ORDER BY extracted_at DESC) AS rn
-            FROM source_prescricoes
-        )
-        WHERE rn = 1
+        FROM source_prescricoes
+        qualify row_number() over (partition by id_prontuario_global, cod_medicamento order by extracted_at desc) = 1
     ),
 
     fato_prescricoes AS (
