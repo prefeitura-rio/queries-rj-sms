b7d3dc564b1ae2db2d5d13cb31cfcdb18a5c4f84
vitmesquita <vitoria.leite.ismart@gmail.com>
2025-07-21 15:19:31 -0300

    changing folder of dbt model

--- models/intermediate/dit/historico_clinico/paciente/int_historico_clinico__paciente__restrito.sql
+++ models/intermediate/dit/historico_clinico/paciente/int_historico_clinico__paciente__restrito.sql
@@ -7,9 +7,8 @@
 }}
 with source as (
     select * 
-    from {{ source("intermediario_historico_clinico_staging", "pacientes_restritos") }}
+    from {{ source("intermediario_historico_clinico_staging", "paciente_restrito") }}
 )
-
 select * 
 from source
 where flag_gemini = '1'
