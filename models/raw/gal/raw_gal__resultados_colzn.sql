d5509f16cdb73ecdc87668d3e99a2cd9a4fdd02c
Pedro <pedrovitor.marques@gmail.com>
2025-07-24 13:06:46 -0300

    Add new models and sources for GAL data processing

---
+++ models/raw/gal/raw_gal__resultados_colzn.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        alias="resultados_colzn",
+        materialized="table",
+    )
+}}
+
+with
+    source as (
+        select * from {{ source("brutos_gal_staging", "colzn_resultados_2020-2024") }}
+    ),
+    cleared as (
+        select
+            {{process_null('id_requisicao')}} as id_requisicao,
+            {{process_null('resultado')}} as resultado,
+            {{process_null('cultura_prevista_para')}} as cultura_prevista_para,
+            {{process_null('aspecto_amostra_escarro')}} as aspecto_amostra_escarro,
+        from source
+    )
+
+select *
+from cleared
+order by id_requisicao
---
+++ models/raw/gal/raw_gal__resultados_tsa.sql
@@ -0,0 +1,50 @@
+{{
+    config(
+        alias="resultados_tsa",
+        materialized="table",
+    )
+}}
+
+with
+    source as (
+        select * from {{ source("brutos_gal_staging", "tsa_resultados_2020-2024") }}
+    ),
+
+    cleaned as (
+        select	
+            {{process_null('id_requisicao')}} as id_requisicao,
+            {{process_null('agente_etiologico')}} as agente_etiologico,
+            {{process_null('tecnica')}} as tecnica,
+
+            {{process_null('estreptomicina')}} as estreptomicina,
+            {{process_null('isoniazida')}} as isoniazida,
+            {{process_null('rifampicina')}} as rifampicina,
+            {{process_null('etambutol')}} as etambutol,
+            {{process_null('kanamicina')}} as kanamicina,
+            {{process_null('pirazinamida')}} as pirazinamida,
+            {{process_null('etionamida')}} as etionamida,
+            {{process_null('amicacina')}} as amicacina,
+            {{process_null('ofloxacina')}} as ofloxacina,
+            {{process_null('capreomicina')}} as capreomicina,
+            {{process_null('levofloxacina')}} as levofloxacina,
+            {{process_null('moxifloxacina')}} as moxifloxacina,
+            {{process_null('protionamida')}} as protionamida,
+
+            {{process_null('antibiotico_1')}} as antibiotico_1,
+            {{process_null('result_outro_antibiotico_1')}} as antibiotico_1_outro_resultado,
+            {{process_null('antibiotico_2')}} as antibiotico_2,
+            {{process_null('result_outro_antibiotico_2')}} as antibiotico_2_outro_resultado,
+            {{process_null('antibiotico_3')}} as antibiotico_3,
+            {{process_null('result_outro_antibiotico_3')}} as antibiotico_3_outro_resultado,
+        from source
+    ),
+
+    final as (
+        select *
+        from cleaned
+    )
+
+
+select *
+from final
+order by id_requisicao
