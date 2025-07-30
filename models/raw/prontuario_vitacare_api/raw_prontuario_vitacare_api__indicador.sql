8e81e9b5168073bbbe7e06565c461d59ae67a92d
srtapacheco <karenpacheco16@poli.ufrj.br>
2025-07-30 16:08:51 -0300

    move partition filter to bruto_atendimento CTE and cast strings in indicador model

--- models/raw/prontuario_vitacare_api/raw_prontuario_vitacare_api__indicador.sql
+++ models/raw/prontuario_vitacare_api/raw_prontuario_vitacare_api__indicador.sql
@@ -14,15 +14,18 @@ with
 
   bruto as (
     select
-      source_id                                                      as id_prontuario_local,
-      concat(nullif(payload_cnes, ''), '.', nullif(source_id, ''))  as id_prontuario_global,
-      payload_cnes                                                   as id_cnes,
+      cast(source_id as string) as id_prontuario_local,
+      cast((concat(nullif(payload_cnes, ''), '.', nullif(source_id, ''))) as string) as id_prontuario_global,
+      cast(payload_cnes as string) as id_cnes,
       safe_cast(datalake_loaded_at as datetime)                      as loaded_at,
       safe_cast(json_extract_scalar(data, '$.datahora_fim_atendimento') as datetime) as datahora_fim,
       data
     from {{ source("brutos_prontuario_vitacare_staging", "atendimento_continuo") }}
-    qualify
-      row_number() over(partition by id_prontuario_global order by datalake_loaded_at desc) = 1
+    {% if is_incremental() %}
+      where date(safe_cast(json_extract_scalar(data, '$.datahora_fim_atendimento') as datetime)) 
+            >= date_sub(current_date('America/Sao_Paulo'), interval 30 day)
+    {% endif %}
+    qualify row_number() over(partition by id_prontuario_global order by datalake_loaded_at desc) = 1
   ),
 
   indicadores_flat as (
@@ -30,8 +33,8 @@ with
       id_prontuario_global,
       id_prontuario_local,
       id_cnes,
-      json_extract_scalar(ind, '$.nome')        as indicadores_nome,
-      json_extract_scalar(ind, '$.valor')       as valor,
+      cast(json_extract_scalar(ind, '$.nome') as string) as indicadores_nome,
+      safe_cast(json_extract_scalar(ind, '$.valor') as float64) as valor,
       loaded_at,
       date(datahora_fim)                        as data_particao,
     from bruto,
@@ -58,7 +61,3 @@ select
   data_particao,
 from indicadores_dedup
 where rn = 1
-
-{% if is_incremental() %}
-  and data_particao >= date_sub(current_date('America/Sao_Paulo'), interval 30 day)
-{% endif %}
