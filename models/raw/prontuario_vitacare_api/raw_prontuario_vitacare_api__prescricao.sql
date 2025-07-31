e69303133a11853c6eaf738d00dcaeca9d379d6f
srtapacheco <karenpacheco16@poli.ufrj.br>
2025-07-31 15:32:21 -0300

    move partition filter to bruto_atendimento CTE and cast date in prescricao model

--- models/raw/prontuario_vitacare_api/raw_prontuario_vitacare_api__prescricao.sql
+++ models/raw/prontuario_vitacare_api/raw_prontuario_vitacare_api__prescricao.sql
@@ -21,6 +21,10 @@ with
       safe_cast(json_extract_scalar(data, '$.datahora_fim_atendimento') as datetime) as datahora_fim,
       data
     from {{ source("brutos_prontuario_vitacare_staging", "atendimento_continuo") }}
+    {% if is_incremental() %}
+    where date(safe_cast(json_extract_scalar(data, '$.datahora_fim_atendimento') as datetime))
+          >= date_sub(current_date('America/Sao_Paulo'), interval 30 day)
+    {% endif %}
     qualify
       row_number() over(
         partition by id_prontuario_global
@@ -63,11 +67,7 @@ select
   posologia,
   quantidade,
   uso_continuado,
-  safe_cast(loaded_at as string) as loaded_at,
+  loaded_at,
   data_particao
 from prescricoes_dedup
 where rn = 1
-
-{% if is_incremental() %}
-  and data_particao >= date_sub(current_date('America/Sao_Paulo'), interval 30 day)
-{% endif %}
