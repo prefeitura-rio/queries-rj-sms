18c7069fd594cc6b599ba15a4e661b64aa6aadc4
Daniel Lira <danielllira@icloud.com>
2025-08-07 10:45:08 -0300

    feat: model vacinacao api

---
+++ models/intermediate/dit/historico_clinico/vacinacao/int_historico_clinico__vacinacao__api.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        schema="intermediario_historico_clinico",
+        alias="vacinacao_api",
+        materialized="table",
+    )
+}}
+
+with
+
+    source_vacina as (
+        select 
+            id_cnes as cnes_unidade,
+            paciente_cpf as cpf,
+            paciente_nascimento_data as data_nascimento,
+            nome_mae as nome_mae,
+            descricao as nome_vacina,
+            dose as dose,
+            aplicacao_data as data_aplicacao,
+            date(registro_data) as data_registro
+        from {{ ref('raw_prontuario_vitacare__vacina') }} 
+    )
+
+select * from source_vacina 
