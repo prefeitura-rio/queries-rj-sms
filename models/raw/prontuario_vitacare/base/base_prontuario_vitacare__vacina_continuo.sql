0f2d3291289c4dbd6c508c5af7d2be8d08aa2b06
Daniel Lira <danielllira@icloud.com>
2025-08-11 08:04:52 -0300

    feat:vacina base models

---
+++ models/raw/prontuario_vitacare/base/base_prontuario_vitacare__vacina_continuo.sql
@@ -0,0 +1,65 @@
+{{
+    config(
+        schema="brutos_prontuario_vitacare_staging",
+        alias="_base_vacina_historico",
+        materialized="incremental",
+        incremental_strategy='merge', 
+        unique_key="id",
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "day",
+        },
+        tags=['weekly']
+    )
+}}
+{% set seven_days_ago = (
+    modules.datetime.date.today() - modules.datetime.timedelta(days=7)
+).isoformat() %}
+
+with
+
+    source_vacina as (
+        select
+            *
+        from {{ ref('raw_prontuario_vitacare_api__vacina') }} 
+        {% if is_incremental() %} where loaded_at > '{{seven_days_ago}}' {% endif %}
+    ),
+
+    selecao_vacinas as (
+        select
+            -- PK
+            id_prontuario_global as id,
+
+            -- Outras Chaves
+            id_cnes as id_cnes,
+            acto_id as id_local,
+            npront as numero_prontuario,
+
+            nome_vacina as nome_vacina,
+            cod_vacina as cod_vacina,
+            dose as dose,
+            lote as lote,
+            data_aplicacao as data_aplicacao,
+            data_registro as data_registro,
+            diff as diff,
+            calendario_vacinal_atualizado as calendario_vacinal_atualizado,
+            tipo_registro as tipo_registro,
+            estrategia_imunizacao as estrategia_imunizacao,
+            foi_aplicada as foi_aplicada,
+            justificativa as justificativa,
+
+            data_particao as data_particao,
+            loaded_at as datalake_imported_at,
+
+            greatest(
+                safe_cast(data_aplicacao as timestamp),
+                safe_cast(data_registro as timestamp)
+            ) as updated_at_rank
+
+        from source_vacina
+    )
+
+select
+    *
+from selecao_vacinas
