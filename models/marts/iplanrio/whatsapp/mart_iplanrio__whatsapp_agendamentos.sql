cdaea7a1fc96aec7dcbb05f2f52f3d1995d3154e
srtapacheco <karenpacheco16@poli.ufrj.br>
2025-08-12 16:01:17 -0300

    modifica modelo de agendamento sisreg para novo padrao da Iplan

--- models/marts/iplanrio/whatsapp/mart_iplanrio__whatsapp_agendamentos.sql
+++ models/marts/iplanrio/whatsapp/mart_iplanrio__whatsapp_agendamentos.sql
@@ -12,30 +12,48 @@
 }}
 
 SELECT
-  struct(
-    marcacoes.paciente_cpf as cpf,
-    marcacoes.paciente_nome as nome
-  ) as paciente,
-
-  procedimento_sigtap as nome_procedimento_geral,
-  procedimento_interno as nome_procedimento_especifico,
-  safe_cast(data_marcacao as datetime) as datahora_marcacao,
-
-  struct(
-    unidade_executante_nome as nome,
-    unidade_executante_id as cnes,
-    unidade_executante_logradouro as logradouro,
-    unidade_executante_complemento as complemento,
-    unidade_executante_numero as numero,
-    unidade_executante_bairro as bairro,
-    unidade_executante_municipio as municipio,
-    {{ padronize_telefone('unidade_executante_telefone') }} as telefone
-  ) as unidade,
-  safe_cast(data_marcacao as date) as data_particao
+  marcacoes.paciente_cpf as cpf,  
+  TO_JSON_STRING(
+        struct(
+            split(marcacoes.paciente_telefone, ',')[safe_offset(0)] as to_,
+            struct(
+                marcacoes.paciente_nome as nome,
+                format_date('%d/%m/%Y', cast(marcacoes.data_marcacao as date))        as data,
+                format_time('%H:%M', time(cast(marcacoes.data_marcacao as datetime))) as hora,
+                -- coalesce(marcacoes.procedimento_interno,marcacoes.procedimento_sigtap) as tipo_agendamento,
+                marcacoes.vaga_solicitada_tp as tipo_agendamento, 
+                 coalesce(
+                  initcap(estab.tipo_sms) || ' - ' || initcap(estab.nome_complemento),
+                  marcacoes.unidade_executante_nome
+                ) as unidade,
+                initcap(LOWER(
+                  array_to_string(
+                      array(
+                          select x from unnest([
+                              marcacoes.unidade_executante_logradouro,
+                              marcacoes.unidade_executante_numero,
+                              marcacoes.unidade_executante_complemento,
+                              marcacoes.unidade_executante_bairro,
+                              marcacoes.unidade_executante_municipio
+                          ]) as x
+                          where x is not null and x <> ''
+                      ),
+                      ', '
+                ) 
+              )) as endereco
+                
+            ) as vars
+        )
+    ) as destination_data,
+    safe_cast(marcacoes.data_marcacao as date) as data_particao
 FROM {{ ref("raw_sisreg_api__marcacoes") }} marcacoes
+LEFT JOIN {{ ref('dim_estabelecimento') }} estab
+  ON safe_cast(marcacoes.unidade_executante_id as string) = safe_cast(estab.id_cnes as string)
 WHERE 
-  solicitacao_status in (
-      "SOLICITAÇÃO / AGENDADA / COORDENADOR",
-      "SOLICITAÇÃO / AGENDADA / SOLICITANTE"
-  ) 
-  and safe_cast(data_marcacao as date) >= current_date()
+  marcacoes.solicitacao_status in (
+        'SOLICITAÇÃO / AGENDADA / COORDENADOR',
+        'SOLICITAÇÃO / AGENDADA / SOLICITANTE'
+  )
+  and cast(marcacoes.data_marcacao as date) >= current_date()
+  and marcacoes.paciente_telefone is not null
+  and marcacoes.paciente_cpf is not null
