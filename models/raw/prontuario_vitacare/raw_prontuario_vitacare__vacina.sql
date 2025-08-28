a386adf7271097c31f77075aacae65e4fffc75eb
Pedro <pedrovitor.marques@gmail.com>
2025-08-28 17:19:50 -0300

    refactor: rename and restructure vacina SQL model for improved clarity and source integration

--- models/raw/prontuario_vitacare/raw_prontuario_vitacare__vacina.sql
+++ models/raw/prontuario_vitacare/raw_prontuario_vitacare__vacina.sql
@@ -1,69 +1,25 @@
 {{
     config(
-        alias="vacina",
-        materialized="table",
-        tags=['daily'],
+        alias="vacinacao",
         partition_by={
-            "field": "data_particao",
+            "field": "particao_data_vacinacao",
             "data_type": "date",
-            "granularity": "day",
-        },
+            "granularity": "month",
+        }
     )
 }}
 
--- dbt run --select raw_prontuario_vitacare__vacina
 with
-    vacina as (
-        select *, 'historico' as tipo,
-        from {{ ref("base_prontuario_vitacare__vacina_historico") }}
-        union all 
-        select *, 'continuo' as tipo,
-        from {{ ref("base_prontuario_vitacare__vacina_continuo") }}
-        union all 
-        select *, 'api' as tipo,
-        from {{ ref("base_prontuario_vitacare__vacina_api") }}
-    ),
-
-    padroniza_vacinas as (
-        select
-            -- PK
-            {{ remove_accents_upper("id") }} as id,
-            {{ remove_accents_upper("id_cnes") }} as id_cnes,
-            {{
-                dbt_utils.generate_surrogate_key(
-                    [
-                        "id_cnes",
-                        "id",
-                        "dose",
-                        "data_aplicacao"
-                    ]
-                )
-            }} as id_surrogate,
-
-            {{ remove_accents_upper("nome_vacina") }} as nome_vacina,
-            {{ remove_accents_upper("dose") }} as dose,
-            data_aplicacao as aplicacao_data,
-            data_registro as registro_data,
-            {{ remove_accents_upper("diff") }} as diff,
-            {{ remove_accents_upper("lote") }} as lote,
-            {{ remove_accents_upper("tipo_registro") }} as registro_tipo,
-            {{ remove_accents_upper("estrategia_imunizacao") }} as estrategia_imunizacao,
-            data_particao as data_particao,
-            loaded_at as loaded_at,
-
-            updated_at_rank
-
-        from vacina
-    ),
-
-    vacina_deduplicado as (
-        select *
-        from padroniza_vacinas
-        qualify
-            row_number() over (
-                partition by id_surrogate order by updated_at_rank desc
-            ) = 1
+    source as (
+        select * from {{ source("brutos_prontuario_vitacare_api_centralizadora", "vacinacao_historico") }}
+        union all
+        select * from {{ ref("raw_prontuario_vitacare_api_centralizadora__vacinacao") }}
     )
 
 select *
-from vacina_deduplicado
+from source
+qualify row_number() over(
+    partition by id_surrogate
+    order by metadados.updated_at desc
+) = 1
+
