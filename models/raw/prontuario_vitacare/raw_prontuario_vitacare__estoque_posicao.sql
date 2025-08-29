fa3e14c4dacf424355f5f029b6c3009aa2c85378
Pedro <pedrovitor.marques@gmail.com>
2025-08-29 10:54:59 -0300

    refactor: update SQL models to use new source 'brutos_prontuario_vitacare_api_centralizadora' for estoque_posicao and vacinacao

--- models/raw/prontuario_vitacare/raw_prontuario_vitacare__estoque_posicao.sql
+++ models/raw/prontuario_vitacare/raw_prontuario_vitacare__estoque_posicao.sql
@@ -19,7 +19,7 @@
 
 with
     source as (
-        select * from {{ source("brutos_prontuario_vitacare_api_centralizadora_staging", "estoque_posicao_historico") }}
+        select * from {{ source("brutos_prontuario_vitacare_api_centralizadora", "estoque_posicao_historico") }}
         union all
         select * from {{ ref("raw_prontuario_vitacare_api_centralizadora__estoque_posicao") }}
     )
