8942298a400a315fb1e10f152428e7189e6e02f5
herianc <herian.cavalcante@live.com>
2025-09-18 17:42:16 -0300

    fix: retira particionamento

--- models/raw/bcadastro/raw_bcadastro__cpf.sql
+++ models/raw/bcadastro/raw_bcadastro__cpf.sql
@@ -1,12 +1,7 @@
 {{
     config(
         alias="cpf",
-        materialized="table",
-        partition_by={
-            "field": "cpf_particao",
-            "data_type": "int64",
-            "range": {"start": 0, "end": 100000000000, "interval": 34722222},
-        }
+        materialized="table"
     )
 }}
 
