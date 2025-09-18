45e4e6a580169d36e9732ebbc10622befd717b25
herianc <herian.cavalcante@live.com>
2025-09-18 17:35:05 -0300

    fix: adiciona modelo para a tabela de cpfs

---
+++ models/raw/bcadastro/raw_bcadastro__cpf.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+        alias="cnpj",
+        materialized="table",
+        cluster_by="situacao",
+        partition_by={
+            "field": "cpf_particao",
+            "data_type": "int64",
+            "range": {"start": 0, "end": 100000000000, "interval": 34722222},
+        }
+    )
+}}
+
+
+select * from {{ source("brutos_bcadastro", 'cpf') }}
