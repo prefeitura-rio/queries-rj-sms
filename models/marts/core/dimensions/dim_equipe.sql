2822e04ddbe2b641067dcbae2c34a5746929abd5
srtapacheco <karenpacheco16@poli.ufrj.br>
2025-09-18 15:54:16 -0300

    fix: correction of duplicate records with the same INE in Ficha A and dim_equipe

--- models/marts/core/dimensions/dim_equipe.sql
+++ models/marts/core/dimensions/dim_equipe.sql
@@ -99,40 +99,56 @@ dim_tipo_equipe as (
     where data_particao = (select versao from versao_atual)
 ),
 contato_equipe as (
-    select ine,  REGEXP_EXTRACT(telefone, r'\(21\)9[0-9]{4}-[0-9]{4}') as telefone
+    select ine,  REGEXP_EXTRACT(telefone, r'\(21\)9[0-9]{4}-[0-9]{4}') as telefone,
+    safe_cast(updated_at as timestamp) as updated_at_telefone
     from {{ ref("raw_plataforma_smsrio__equipe_contato") }}
     where ine is not null 
     and ine != "0" 
     and ine != "1"
-)
-select
-    distinct
-    equipe.equipe_ine as id_ine,
-    equipe.equipe_nome as nome_referencia,
-    dim_estabelecimentos.id_cnes,
-    equipe.id_tipo_equipe as id_equipe_tipo,
-    REGEXP_REPLACE(dim_tipo_equipe.equipe_descricao, r' {2,}', ' ') as equipe_tipo_descricao,
-    equipe.id_area,
-    dim_area.area_descricao,
-    contato_equipe.telefone,
-    profissionais_equipe.medicos,
-    profissionais_equipe.enfermeiros,
-    profissionais_equipe.auxiliares_tecnicos_enfermagem,
-    profissionais_equipe.agentes_comunitarios,
-    profissionais_equipe.auxiliares_tecnico_saude_bucal,
-    profissionais_equipe.dentista,
-    profissionais_equipe.outros_profissionais,
-    profissionais_equipe.ultima_atualizacao_profissionais,
-    equipe.data_atualizacao as ultima_atualizacao_infos_equipe
-from equipe
-inner join
-    dim_estabelecimentos on dim_estabelecimentos.id_unidade = equipe.id_unidade
-left join
-    profissionais_equipe
-    on (
-        profissionais_equipe.id_unidade_saude = equipe.id_unidade
-        and profissionais_equipe.equipe_sequencial = equipe.equipe_sequencial
+),
+
+    equipe_completa as (
+        select
+            equipe.equipe_ine as id_ine,
+            equipe.equipe_nome as nome_referencia,
+            dim_estabelecimentos.id_cnes,
+            equipe.id_tipo_equipe as id_equipe_tipo,
+            regexp_replace(dim_tipo_equipe.equipe_descricao, r' {2,}', ' ') as equipe_tipo_descricao,
+            equipe.id_area,
+            dim_area.area_descricao,
+            contato_equipe.telefone,
+            profissionais_equipe.medicos,
+            profissionais_equipe.enfermeiros,
+            profissionais_equipe.auxiliares_tecnicos_enfermagem,
+            profissionais_equipe.agentes_comunitarios,
+            profissionais_equipe.auxiliares_tecnico_saude_bucal,
+            profissionais_equipe.dentista,
+            profissionais_equipe.outros_profissionais,
+            profissionais_equipe.ultima_atualizacao_profissionais,
+            equipe.data_atualizacao as ultima_atualizacao_infos_equipe,
+            greatest(
+                safe_cast(equipe.data_atualizacao as timestamp),
+                safe_cast(profissionais_equipe.ultima_atualizacao_profissionais as timestamp)
+            ) as updated_at_rank,
+            contato_equipe.updated_at_telefone
+        from equipe
+        inner join dim_estabelecimentos 
+            on dim_estabelecimentos.id_unidade = equipe.id_unidade
+        left join profissionais_equipe
+            on profissionais_equipe.id_unidade_saude = equipe.id_unidade
+           and profissionais_equipe.equipe_sequencial = equipe.equipe_sequencial
+        left join dim_area 
+            on dim_area.id_area = equipe.id_area
+        left join dim_tipo_equipe 
+            on dim_tipo_equipe.id_equipe_tipo = equipe.id_tipo_equipe
+        left join contato_equipe 
+            on lpad(contato_equipe.ine, 10, '0') = equipe.equipe_ine
     )
-left join dim_area on dim_area.id_area = equipe.id_area
-left join dim_tipo_equipe on dim_tipo_equipe.id_equipe_tipo = equipe.id_tipo_equipe
-left join contato_equipe on  LPAD(contato_equipe.ine,10,'0') = equipe.equipe_ine
+
+select *
+except(updated_at_rank, updated_at_telefone)
+from equipe_completa
+qualify row_number() over (
+    partition by id_ine
+    order by updated_at_rank desc, updated_at_telefone desc
+) = 1
