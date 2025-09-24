863cc9cc2e3de4134e4005631fec6b6e6c1f9341
miloskimatheus <matheus.miloski@dados.rio>
2025-09-24 17:09:37 -0300

    fix: incremental logic

--- models/raw/sisreg_api_log/raw_sisreg_api_log__logs.sql
+++ models/raw/sisreg_api_log/raw_sisreg_api_log__logs.sql
@@ -5,19 +5,16 @@
     schema = "brutos_sisreg_api_log",
     alias = "logs",
     partition_by = {
-      "field": "data_particao",
+      "field": "data_inicial",
       "data_type": "date",
       "granularity": "month"
     },
     incremental_strategy = 'insert_overwrite',
-    partitions = ['data_particao'],
-    cluster_by = ['bq_table','run_id'],
+    partitions = ['data_inicial'],
+    cluster_by = ['tabela','run_id'],
     on_schema_change = 'sync_all_columns'
   )
 }}
-
-{%- set months_lookback = var('months_lookback', 1) -%}
-{%- set min_build_date = "DATE_TRUNC(DATE_SUB(CURRENT_DATE(), INTERVAL " ~ months_lookback ~ " MONTH), MONTH)" -%}
 
 with
 extracoes as (
@@ -26,20 +23,17 @@ extracoes as (
       datetime(as_of) as run_inicio,
       datetime(validation_date) as run_fim,
       environment as run_ambiente,
-      bq_table,
+      bq_table as tabela,
       bq_dataset,
-      data_inicial,
+      date(data_inicial) as data_inicial,
       data_final,
-      completed,
-      ano_particao,
-      mes_particao,
-      safe.parse_date('%Y-%m-%d', data_particao) as data_particao
+      completed
   from {{ source("brutos_sisreg_api_log_staging", "marcacoes") }}
-  where 1=1
-    and safe.parse_date('%Y-%m-%d', data_particao) is not null
-    {% if is_incremental() %}
-      and safe.parse_date('%Y-%m-%d', data_particao) >= {{ min_build_date }}
-    {% endif %}
+  {% if is_incremental() %}
+    where
+      date(data_particao) >= date_sub(current_date(), interval 1 month)
+      and run_id not in (select run_id from {{this}})
+  {% endif %}
 
   union all
 
@@ -48,46 +42,45 @@ extracoes as (
       datetime(as_of) as run_inicio,
       datetime(validation_date) as run_fim,
       environment as run_ambiente,
-      bq_table,
+      bq_table as tabela,
       bq_dataset,
-      data_inicial,
+      date(data_inicial) as data_inicial,
       data_final,
-      completed,
-      ano_particao,
-      mes_particao,
-      safe.parse_date('%Y-%m-%d', data_particao) as data_particao
+      completed
   from {{ source("brutos_sisreg_api_log_staging", "solicitacoes") }}
-  where 1=1
-    and safe.parse_date('%Y-%m-%d', data_particao) is not null
-    {% if is_incremental() %}
-      and safe.parse_date('%Y-%m-%d', data_particao) >= {{ min_build_date }}
-    {% endif %}
+  {% if is_incremental() %}
+  where 
+      date(data_particao) >= date_sub(current_date(), interval 1 month)
+      and run_id not in (select run_id from {{this}})
+  {% endif %}
 ),
 
 n_rows as (
   select
       "solicitacoes" as tabela,
-      safe.parse_date('%Y-%m-%d', s.data_particao) as data_particao,
       s.run_id,
       count(*) as nrows
   from {{ source('brutos_sisreg_api_staging', 'solicitacoes') }} s
   {% if is_incremental() %}
-    where safe.parse_date('%Y-%m-%d', s.data_particao) >= {{ min_build_date }}
+  where 
+      date(data_particao) >= date_sub(current_date(), interval 1 month)
+      and run_id in (select run_id from extracoes)
   {% endif %}
-  group by 1,2,3
+  group by 1,2
 
   union all
 
   select
       "marcacoes" as tabela,
-      safe.parse_date('%Y-%m-%d', m.data_particao) as data_particao,
       m.run_id,
       count(*) as nrows
   from {{ source('brutos_sisreg_api_staging', 'marcacoes') }} m
   {% if is_incremental() %}
-    where safe.parse_date('%Y-%m-%d', m.data_particao) >= {{ min_build_date }}
+  where 
+      date(data_particao) >= date_sub(current_date(), interval 1 month)
+      and run_id in (select run_id from extracoes)
   {% endif %}
-  group by 1,2,3
+  group by 1,2
 ),
 
 consolidado as (
@@ -96,11 +89,8 @@ consolidado as (
         n.nrows
     from extracoes e
     left join n_rows n
-    using (data_particao, run_id)
+    using (tabela, run_id)
 )
 
 select *
 from consolidado
-{% if is_incremental() %}
-  where data_particao >= {{ min_build_date }}
-{% endif %}
