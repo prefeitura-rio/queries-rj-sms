802b82b884a9b4e9ff06ee8f88b0d16f4b5fd376
miloskimatheus <matheus.miloski@dados.rio>
2025-10-01 15:11:38 -0300

    refactor: configs and logic

--- models/raw/minhasaude_mongodb/raw_minhasaude_mongodb__perfil_acessos.sql
+++ models/raw/minhasaude_mongodb/raw_minhasaude_mongodb__perfil_acessos.sql
@@ -2,8 +2,11 @@
     config(
         schema="brutos_minhasaude_mongodb",
         alias="perfil_acessos",
+        materialized="incremental",
+        unique_key="_id",
+        cluster_by=["_id", "idusuario"],
         partition_by={
-            "field": "data_particao",
+            "field": "createdat",
             "data_type": "date",
             "granularity": "month",
         },
@@ -12,6 +15,11 @@
 
 
 with
+    particao as (
+        select distinct data_extracao
+        from {{ ref ("raw_minhasaude_mongodb__modulos_perfil_acessos") }}
+    ),
+
     source as (
         select
             _id,
@@ -25,17 +33,30 @@ with
             safe_cast(safe_cast(logingovbr as int64) as bool) as logingovbr,
             origem,
 
-            safe_cast(createdat as datetime) as createdat,
+            safe_cast(createdat as date) as createdat,
             safe_cast(updatedat as datetime) as updatedat,
 
             __v,
 
-            safe_cast(safe_cast(data_extracao as timestamp) as date) as data_extracao,
-            safe_cast(ano_particao as int64) as ano_particao,
-            safe_cast(mes_particao as int64) as mes_particao,
-            safe_cast(data_particao as date) as data_particao
+            date(safe_cast(data_extracao as timestamp), 'America/Sao_Paulo') as data_extracao,
         from {{ source("brutos_minhasaude_mongodb_staging", "perfil_acessos") }}
+
+        {% if is_incremental() %}
+            where date(data_particao) >= (select data_extracao from particao)
+        {% endif %}
     )
 
-select distinct *
-from source
+select
+    s.*,
+    (
+        {% if is_incremental() %}
+            1
+        {% else %}
+            row_number() over (partition by _id order by updatedat desc)
+        {% endif %}
+    ) as rn
+
+from source as s
+{% if not is_incremental() %}
+    qualify rn = 1
+{% endif %}
