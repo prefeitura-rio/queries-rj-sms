28f34f7ae1a85a6f39c3eb8cd29678b2dd1b6ae3
srtapacheco <karenpacheco16@poli.ufrj.br>
2025-10-02 17:37:45 -0300

    creating anonymized ficha A table for DIT recruitment process 25/2

---
+++ models/marts/core/processo_seletivo/mart_processo_seletivo__ficha_a_anonimizada.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+    materialized="table",
+    schema="processo_seletivo_dit",
+    alias = "ficha_a_anonimizada"
+  )
+}}
+
+select
+  * except (
+    id,
+    cpf,
+    numero_prontuario,
+    id_paciente_vitacare,
+    nome,
+    nome_social,
+    nome_mae,
+    nome_pai,
+    telefone,
+    data_nascimento,
+    logradouro,
+    tipo_logradouro,
+    microarea
+  )
+from {{ ref('raw_prontuario_vitacare__ficha_a') }}
