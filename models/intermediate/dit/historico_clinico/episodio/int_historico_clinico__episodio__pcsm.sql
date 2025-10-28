ea8c23e82b1681b25f83077c54b2dbdb9c9e8a48
herianc <herian.cavalcante@live.com>
2025-10-28 18:34:54 -0300

    feat: adiciona modelo intermediuario de episodio do pcsm

--- models/intermediate/dit/historico_clinico/episodio/int_historico_clinico__episodio__pcsm.sql
+++ models/intermediate/dit/historico_clinico/episodio/int_historico_clinico__episodio__pcsm.sql
@@ -7,103 +7,45 @@
 }}
 
 with 
-   atendimento as (
+  atendimento_pcsm as (
     select
-        id_hci,
-        id_atendimento,
-        datetime(data_entrada_atendimento, parse_time('%H%M', hora_entrada_atendimento)) as data_hora_entrada,
-        datetime(data_saida_atendimento, parse_time('%H%M', hora_saida_atendimento)) as data_hora_saida,
-        ta.descricao_classificacao_atendimento as tipo_atendimento,
-        ta.descricao_tipo_atendimento as descricao_tipo_atendimento,
-        ta.descricao_acao_pos_atendimento as acao_pos_atendimento,
-        -- id_profissional_saude, Não há tabela para relacionar o profissional
-        id_paciente,
-        id_unidade_saude as id_unidade,
-        id_atividade_grupo,
-        id_encaminhamento,
-        id_unidade_saude_encaminhada as id_unidade_encaminhada,
-        data_inclusao_cadastro as data_cadastro,
-        local_atendimento,
-        descricao_local_atendimento,
-        atendimento_cancelado,
-        descricao_atendimento_cancelado,
-        lista_profissionais_atendimento
-    from {{ ref('raw_pcsm_atendimentos') }} a
-    left join {{ ref('raw_pcsm_tipos_atendimentos') }} ta on ta.id_tipo_atendimento = a.id_tipo_atendimento
-   ),
+        a.id_hci,
+        a.id_paciente,
+        p.numero_cpf_paciente as cpf,
+        p.numero_cartao_saude as cns,
+        p.nome_paciente as nome,
 
-    tipo_atendimento as (
-        select 
-            id_tipo_atendimento,
-            descricao_classificacao_atendimento as classificacao_atendimento,
-            descricao_tipo_atendimento as tipo_atendimento,
-            descricao_acao_pos_atendimento as acao_pos_atendimento
-        from {{ ref('raw_pcsm_tipos_atendimentos') }}
-    ),
+        ta.descricao_classificacao_atendimento as tipo,
+        ta.descricao_tipo_atendimento as subtipo,
+        datetime(data_entrada_atendimento, parse_time('%H%M', hora_entrada_atendimento)) as entrada_datahora, 
+        datetime(data_saida_atendimento, parse_time('%H%M', hora_saida_atendimento)) as saida_datahora,
 
-   paciente as (
-        select  
-            id_paciente,
-            numero_cpf_paciente as cpf, 
-            numero_cartao_saude as cns
-        from {{ ref('raw_pcsm_pacientes') }}
-   ),
+        -- estabelecimento
+        struct (
+        u.nome_unidade_saude as nome_estabelecimento,
+        u.codigo_nacional_estabelecimento_saude as cnes
+        ) as estabelecimento,
 
-    unidade as (
-        select
-            id_unidade_saude as id_unidade,
-            nome_unidade_saude as nome_unidade,
-            codigo_nacional_estabelecimento_saude as id_cnes,
-        from {{ref('raw_pcsm_unidades_saude')}}
-    ),
+        -- prontuario
+        struct (
+            a.id_atendimento as id_prontuario_global,
+            null as id_prontuario_local,
+            'pcsm' as fornecedor
+        ) as prontuario,
 
-    -- Atividades em grupo
-    atividades_grupo as (
-            select 
-                id_atividade_grupo as id_atividade,
-                initcap(nome_atividade_grupo) as atividade_grupo,
-                data_inicio_atividade as data_inicio,
-                data_termino_atividade as data_termino,
-                id_unidade_saude as id_unidade,
-                id_tipo_atividade_grupo as id_tipo, 
-                local_atividade_grupo as local_atividade
-            from {{ ref('raw_pcsm_atividades_grupo') }} 
-    ),
+        -- metadados
+        struct (
+            a.loaded_at as imported_at,
+            a.transformed_at as updated_at,
+            current_timestamp() as processed_at
+        ) as metadados
 
-    tipo_atividade as (
-        select 
-            id_tipo_atividade as id_tipo,
-            descricao_tipo_atividade as tipo_atividade
-        from {{ ref('raw_pcsm_tipos_atividades') }}
-    ),
-
-    atividades_grupo_detalhado as (
-        select
-        struct(ag.id_atividade,
-            ag.atividade_grupo,
-            ag.data_inicio,
-            ag.data_termino,
-            u.nome_unidade as nome_unidade_atividade,
-            u.id_cnes as id_cnes_atividade,
-            ta.tipo_atividade,
-            ag.local_atividade) as atividade_grupo
-        from atividades_grupo ag
-        left join unidade u on ag.id_unidade = u.id_unidade
-        left join tipo_atividade ta on ag.id_tipo = ta.id_tipo
-    )
+    from {{ref('raw_pcsm_atendimentos')}} a 
+    left join {{ref('raw_pcsm_pacientes')}} p on a.id_paciente = p.id_paciente
+    left join {{ref('raw_pcsm_tipos_atendimentos')}} ta on a.id_tipo_atendimento = ta.id_tipo_atendimento
+    left join {{ref('raw_pcsm_unidades_saude')}} u on a.id_unidade_saude = u.id_unidade_saude
+  )
 
 
-select
-    a.id_hci,
-    a.id_paciente,
-    p.cpf,
-    a.tipo_atendimento as tipo,
-    a.descricao_tipo_atendimento as subtipo,
-    a.data_hora_entrada as entrada_datahora,
-    a.data_hora_saida as saida_datahora,
-from atendimento a
-left join paciente p on p.id_paciente = a.id_paciente
-left join unidade u on a.id_unidade = u.id_unidade
-left join atividades_grupo_detalhado ag on a.id_atividade_grupo = ag.id
-
+select * from atendimento_pcsm
 
