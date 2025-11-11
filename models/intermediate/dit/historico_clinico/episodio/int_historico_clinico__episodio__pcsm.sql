10807cf850b39d1ec403b58ff8895c50f0092090
herianc <herian.cavalcante@live.com>
2025-11-11 14:15:02 -0300

    feat: construcao de episodio ambulatorial

--- models/intermediate/dit/historico_clinico/episodio/int_historico_clinico__episodio__pcsm.sql
+++ models/intermediate/dit/historico_clinico/episodio/int_historico_clinico__episodio__pcsm.sql
@@ -6,8 +6,10 @@
     )
 }}
 
+
 with 
-  atendimento_pcsm as (
+-- ATENDIMENTOS SIMPLIFICADOS
+  atendimento_simplificado as (
     select
         a.id_hci,
         a.id_paciente,
@@ -15,10 +17,11 @@ with
         p.numero_cartao_saude as cns,
         p.nome_paciente as nome,
 
-        ta.descricao_classificacao_atendimento as tipo,
+        'Simplificado' as tipo,
+        --ta.descricao_classificacao_atendimento as tipo,
         ta.descricao_tipo_atendimento as subtipo,
-        datetime(data_entrada_atendimento, parse_time('%H%M', hora_entrada_atendimento)) as entrada_datahora, 
-        datetime(data_saida_atendimento, parse_time('%H%M', hora_saida_atendimento)) as saida_datahora,
+        datetime(a.data_entrada_atendimento, parse_time('%H%M', a.hora_entrada_atendimento)) as entrada_datahora, 
+        datetime(a.data_saida_atendimento, parse_time('%H%M', a.hora_saida_atendimento)) as saida_datahora,
 
         -- estabelecimento
         struct (
@@ -49,8 +52,6 @@ with
     left join {{ref('raw_pcsm_unidades_saude')}} u on a.id_unidade_saude = u.id_unidade_saude
     left join {{ref('dim_estabelecimento')}} e on u.codigo_nacional_estabelecimento_saude = e.id_cnes
   ),
-
--- condicoes
     cid_4_digitos as (
         select distinct 
             id_paciente as id, 
@@ -117,13 +118,12 @@ with
         group by 1
     ),
 
-    final as (
+    simplificado_final as (
         select
             id_hci,
             id_paciente,
             cpf,
             cns,
-            nome,
             tipo, 
             subtipo,
             entrada_datahora,
@@ -134,12 +134,127 @@ with
             metadados,
             data_particao,
             cpf_particao
-        from atendimento_pcsm a
+        from atendimento_simplificado a
         left join cid_grouped cg on a.id_paciente = cg.id
         
+    ),
+
+
+-- ANTEDIMENTOS AMBULATORIAIS 
+    atendimento_ambulatorial as (
+        select 
+            null as id_hci, -- TODO
+            null as id_paciente, -- TODO
+            a.id_atendimento, 
+            a.id_paciente as id_paciente_local,
+            p.registro_prontuario as id_paciente_global,
+            a.data_atendimento as entrada_datahora,
+            a.id_unidade_saude,
+            p.altura_paciente as altura,
+            p.peso_paciente as peso,
+            
+
+        from {{ref('raw_prescricao_atendimentos')}} a 
+        left join {{ref('raw_prescricao_pacientes')}} p on a.id_paciente = p.id_paciente
+    
+    ),
+
+    prescricoes_ambulatorial as (
+        select 
+            pp.id_prescricao
+            id_atendimento, 
+            id_medicamento,
+            pm.nome_medicamento, 
+            via_administracao,
+            dose_administrada,
+            intervalo_doses,
+            observacao_administracao
+        from {{ref('raw_prescricao_prescricoes')}} pp 
+        left join {{ref('raw_prescricao_medicamentos')}} pm on cast(pm.id_prescricao as int64) = pp.id_prescricao
+    ),
+
+    prescricoes_ambulatorial_agg as  (
+        select
+            id_atendimento,
+            array_agg(
+                struct(
+                    id_medicamento,
+                    nome_medicamento,
+                    via_administracao,
+                    dose_administrada,
+                    intervalo_doses,
+                    observacao_administracao
+                ) ignore nulls
+            ) as prescricoes
+        from prescricoes_ambulatorial
+        group by 1
+    ),
+
+
+    episodio_ambulatorial as (
+        select 
+            id_hci,
+            null as id_paciente
+            'Ambulatorial' as tipo,
+            '' as subtipo,
+            a.entrada_datahora,
+            null as saida_datahora,
+            
+            -- medidas
+            struct(
+                p.altura as altura,
+                null as circunferencia_abdominal,
+                null as frequencia_cardiaca,
+                null as frequencia_respiratoria,
+                null as glicemia,
+                null as hemoglobina_glicada,
+                null as imc,
+                p.peso as peso_paciente,
+                null as pressao_sistolica,
+                null as pressao_diastolica,
+                null as pulso_ritmo,
+                null as saturacao_oxigenio,
+                null as temperatura
+            ) as medidas,
+            null as motivo_atendimento, -- Tabela de evoluções
+            null as desfecho_atendimento
+            -- condicoes
+                -- cid
+                -- descricao
+                -- situacao 'ativo' 
+                -- data_diagnostico
+                -- resumo
+            -- prescricoes 
+                -- id 
+                -- nome
+                -- concentracao
+                -- uso continuo
+            -- medicamentos_administrados
+                -- nome
+                -- quantidade
+                -- unidade_medida
+                -- uso
+                -- via_administracao
+                -- prescricao_data
+            -- estabelecimento
+                -- id_cnes
+                -- nome
+                -- estabelecimento_tipo
+            -- profissional
+                -- id
+                -- cpf
+                -- cns
+                -- nome
+                -- especialidade
+            -- prontuario
+                -- id_prontuario_global
+                -- id_prontuario_local
+                -- fornecedor
+            -- metadados
+        
+        from atendimento_ambulatorial a
     )
 
-
-select * from final
+select * from simplificado_final
 
 
--- models/raw/prescricao/raw_prescricao_medicamentos.sql
+++ models/raw/prescricao/raw_prescricao_medicamentos.sql
@@ -9,31 +9,38 @@
 }}
 
 select
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.id_medicamento'))") }} as id_medicamento,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.id_prescricao'))") }} as id_prescricao,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.nome'))") }} as nome,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.via'))") }} as via_administracao,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.dose'))") }} as dose,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.intervalo'))") }} as intervalo,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.apresentacao'))") }} as apresentacao,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.observacao'))") }} as observacao,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.previsao'))") }} as previsao,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.justificativa'))") }} as justificativa,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.soro'))") }} as soro,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.qtd_atendida'))") }} as quantidade_atendida,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.dose_tot'))") }} as dose_total_prescrita,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.dias_medic'))") }} as total_dias_tratamento,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.qtd_va'))") }} as quantidade_estoque,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.medicva'))") }} as medicamento_disponivel,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.datispens'))") }} as data_dispensacao,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.numqtddispensdia'))") }} as quantidade_dispensada_dia,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.hidratacao'))") }} as hidratacao_associada,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.numqtdposo2'))") }} as quantidade_posologia2,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.numqtdposo3'))") }} as quantidade_posologia3,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.cod_med_hospub'))") }} as codigo_medicamento,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.cpf_farmaceutico'))") }} as farmaceutico_cpf,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.nome_farmaceutico'))") }} as farmaceutico_nome,
-    {{ process_null("(json_extract_scalar(_airbyte_data, '$.etiqueta_aprasamento'))") }} as etiqueta_aprasamento,
+    safe_cast(id_medicamento as int64) as id_medicamento,                         -- Identificador único para cada medicamento
+    safe_cast(nome as string) as nome_medicamento,                                -- Nome comercial do medicamento
+    safe_cast(via as string) as via_administracao,                                -- Via de administração do medicamento
+    safe_cast(dose as numeric) as dose_administrada,                              -- Quantidade do medicamento administrada em cada tomada
+    safe_cast(intervalo as string) as intervalo_doses,                            -- Tempo entre as doses do medicamento
+    safe_cast(previsao as string) as previsao_consumo,                            -- Previsão de consumo do medicamento, em dias
+    safe_cast(justificativa as string) as justificativa_prescricao,               -- Justificativa para a prescrição do medicamento
+    safe_cast(observacoes as string) as observacao_administracao,                 -- Observações relevantes sobre a administração
+    safe_cast(id_prescricao as int64) as id_prescricao,                           -- Identificador único da prescrição médica
+    safe_cast(qtd_atendida as numeric) as quantidade_atendida,                    -- Quantidade do medicamento atendida na dispensação
+    safe_cast(cod_med_hospub as string) as codigo_medicamento,                    -- Código do medicamento no sistema do hospital
+    safe_cast(cpf_farmaceutico as string) as cpf_farmaceutico,                    -- CPF do farmacêutico que dispensou o medicamento
+    safe_cast(nome_farmaceutico as string) as nome_farmaceutico,                  -- Nome completo do farmacêutico que dispensou o medicamento
+    safe_cast(apresentacao as string) as apresentacao_medicamento,                -- Apresentação comercial do medicamento
+    safe_cast(dose_tot as string) as dose_total_prescrita,                        -- Dose total do medicamento prescrita
+    safe_cast(dias_medic as string) as total_dias_tratamento,                     -- Número total de dias de tratamento
+    safe_cast(soro as string) as administrado_soro,                               -- Nome do tipo de soro diluente usado na medicação
+    safe_cast(hidratacao as string) as hidratacao_associada,                      -- Indica se há necessidade de hidratação associada
+    case trim(safe_cast(hidratacao as string))
+        when 'S' then 'Sim'
+        when 'N' then 'Não'
+        when '' then 'Não informado'
+        when null then 'Não informado'
+        else 'Não classificado'
+    end as descricao_hidratacao_associada,                                                -- Descrição da necessidade de hidratação
+    safe_cast(qtd_va as string) as quantidade_estoque,                            -- Quantidade do medicamento em estoque
+    safe_cast(medicva as string) as medicamento_disponivel_unidade,               -- Indica se o medicamento está disponível na unidade de saúde
+    safe_cast(etiqueta_aprasamento as int64) as etiqueta_aprasamento,             -- Indica se a etiqueta de aprasamento foi aplicada
+    safe_cast(numqtdposo2 as numeric) as quantidade_posologia2,                   -- Quantidade do medicamento em segunda posologia
+    safe_cast(numqtdposo3 as numeric) as quantidade_posologia3,                   -- Quantidade do medicamento em terceira posologia
+    safe_cast(datdispens as date) as data_dispensacao,                            -- Data da dispensação do medicamento
+    safe_cast(numqtddispensdia as int64) as quantidade_dispensada_dia,            -- Quantidade dispensada por dia
     _airbyte_extracted_at as loaded_at,
     current_timestamp() as transformed_at
-from {{ source('airbyte_internal', 'brutos_prescricao_staging_raw__stream_fa_medicamento') }}
+from {{ source('brutos_prontuario_carioca_saude_mental_prescricao_staging', 'fa_medicamento') }}
