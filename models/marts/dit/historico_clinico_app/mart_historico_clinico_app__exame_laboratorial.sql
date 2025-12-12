86fb9fcb6e5c909d7fb7f83a1d3c880f5583acf6
Daniel Lira <daniellira@icloud.com>
2025-12-12 17:00:43 -0300

    feat: filtra pacientes restritos

--- models/marts/dit/historico_clinico_app/mart_historico_clinico_app__exame_laboratorial.sql
+++ models/marts/dit/historico_clinico_app/mart_historico_clinico_app__exame_laboratorial.sql
@@ -15,6 +15,34 @@ with exame_laboratorial as (
     select
         *
     from {{ ref('mart_historico_clinico__exame_laboratorial') }}
-)
+),
 
-select * from exame_laboratorial
+    pacientes_com_hiv_reagente as (
+        select distinct
+            m.id_solicitacao
+        from exame_laboratorial m
+        left join {{ ref('raw_exames_laboratoriais__exames') }} e
+            on m.id_solicitacao = e.id_solicitacao
+        left join {{ ref('raw_exames_laboratoriais__resultados') }} r
+            on e.id = r.id_exame
+        where 
+            e.codigo_lis = '0202030300'
+            and lower(trim({{ remove_accents_upper("r.resultado") }})) in (
+                'amostra reagente para hiv',
+                'reagente',
+                'amostra positiva para hiv'
+            )
+    ),
+
+    paciente_restrito as (
+        select 
+            m.* except(id_solicitacao)
+        from exame_laboratorial m
+        left join pacientes_com_hiv_reagente h
+            on m.id_solicitacao = h.id_solicitacao
+        where h.id_solicitacao IS NULL  -- EXCLUI reagentes
+    )
+
+select *
+from paciente_restrito
+
--- models/raw/exames_laboratoriais/raw_exames_laboratoriais__solicitacoes.sql
+++ models/raw/exames_laboratoriais/raw_exames_laboratoriais__solicitacoes.sql
@@ -6,34 +6,43 @@
 
 with
     source as (
-        select * from {{ source("brutos_cientificalab_staging", "solicitacoes") }}
+        select 
+            * 
+        from {{ source("brutos_exames_laboratoriais_staging", "solicitacoes") }}
     ),
-    removendo_duplicados as (
-        select * from source
-        qualify
-            row_number() over (partition by id order by datalake_loaded_at desc) = 1
+
+    dedup as (
+        select 
+            * 
+        from source
+        qualify row_number() over (partition by id order by datalake_loaded_at desc) = 1
     ),
+
     renamed as (	
         select
             safe_cast({{process_null("id")}} as string) as id,
             safe_cast({{process_null("link")}} as string) as laudo_url,
-            safe_cast({{process_null("codigoLis")}} as string) as cod_lis,
-            safe_cast({{process_null("codigoApoio")}} as string) as cod_apoio,
-            safe_cast({{process_null("codunidade")}} as string) as cod_unidade,
-            safe_cast({{process_null("unidade")}} as string) as unidade,
-            safe_cast({{process_null("codorigem")}} as string) as cod_origem,
-            safe_cast({{process_null("origem")}} as string) as origem,
-            safe_cast({{parse_datetime(process_null("dataPedido"))}} as timestamp) as datahora_pedido,
+
+            safe_cast({{process_null("codigoLis")}} as string) as codigo_lis,
+            safe_cast({{process_null("codigoApoio")}} as string) as codigo_apoio,
+            safe_cast({{process_null("unidade")}} as string) as area_programatica,
+            safe_cast({{process_null("origem")}} as string) as unidade,
+            safe_cast({{process_null("codunidade")}} as string) as codigo_unidade,
+            safe_cast({{process_null("codorigem")}} as string) as codigo_origem,
+
+            safe_cast({{parse_datetime(process_null("dataPedido"))}} as datetime) as datahora_pedido,
             safe_cast({{process_null("autorizacao")}} as string) as autorizacao,
             safe_cast({{process_null("status")}} as string) as status,
             safe_cast({{process_null("mensagem")}} as string) as mensagem,
             safe_cast({{process_null("alterado")}} as string) as alterado,
+
+            
             safe_cast({{process_null("responsaveltecnico_crm")}} as string) as responsaveltecnico_crm,
             safe_cast({{process_null("responsaveltecnico_nome")}} as string) as responsaveltecnico_nome,
 
-            safe_cast({{process_null("paciente_codigoLis")}} as string) as paciente_cod_lis,
+            safe_cast({{process_null("paciente_codigoLis")}} as string) as paciente_codigo_lis,
             safe_cast({{process_null("paciente_nome")}} as string) as paciente_nome,
-            safe_cast({{parse_date(process_null("paciente_nascimento"))}} as date) as paciente_nascimento,
+            safe_cast({{parse_date(process_null("paciente_nascimento"))}} as date) as paciente_data_nascimento,
             CASE 
                 WHEN {{process_null("paciente_sexo")}} = 'M' THEN 'male'
                 WHEN {{process_null("paciente_sexo")}} = 'F' THEN 'female'
@@ -49,9 +58,11 @@ with
             safe_cast({{process_null("lote_codigoLis")}} as string) as lote_codigo_lis,
             safe_cast({{process_null("lote_origemLis")}} as string) as lote_origem_lis,
 
-            safe_cast({{process_null("datalake_loaded_at")}} as timestamp) as loaded_at,
-            safe_cast(current_timestamp() as timestamp) as processed_at
-        from removendo_duplicados
+            safe_cast({{process_null("source")}} as string) as origem,
+            safe_cast({{process_null("datalake_loaded_at")}} as datetime) as loaded_at
+        from dedup
     )
-select *
+
+select 
+    *
 from renamed
