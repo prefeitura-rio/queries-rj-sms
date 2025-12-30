f8e1418e6326f04aed680ed593fc1ae894fa349e
Pedro <pedrovitor.marques@gmail.com>
2025-12-30 09:42:58 -0300

    refactor: streamline SQL query for vaccination transmission model
    - Removed unnecessary CTE and simplified the source selection.
    - Added ranking logic based on SEND_STATUS to improve deduplication process.

--- models/raw/prontuario_vitacare/raw_prontuario_vitacare__transmissao.sql
+++ models/raw/prontuario_vitacare/raw_prontuario_vitacare__transmissao.sql
@@ -7,12 +7,8 @@
 with
 
 source as (
-    select * from {{ source("brutos_prontuario_vitacare_staging", "transmissao_rnds") }}
-),
-
-padronizados as (
-    select PERFORMED_VACCINE_ACTION_ID, RIA_RNDS_ID, LOCAL_UUID, ENTID_ID, DATA_ADMINISTRACAO, SEND_STATUS, DATE_SEND_STATUS
-    from source
+    select PERFORMED_VACCINE_ACTION_ID, RIA_RNDS_ID, LOCAL_UUID, ENTID_ID, DATA_ADMINISTRACAO, SEND_STATUS, DATE_SEND_STATUS 
+    from {{ source("brutos_prontuario_vitacare_staging", "transmissao_rnds") }}
 ),
 
 final as (
@@ -24,14 +20,19 @@ final as (
         {{process_null('DATA_ADMINISTRACAO')}} as data_aplicacao,
         {{process_null('LOCAL_UUID')}} as uuid_local,
         {{process_null('SEND_STATUS')}} as status_envio,
+        CASE 
+            WHEN {{process_null('SEND_STATUS')}} = 'SENT' THEN 1
+            WHEN {{process_null('SEND_STATUS')}} = 'PENDING_RETRY' THEN 2
+            ELSE 3
+        END as ranking,
         safe_cast({{process_null('DATE_SEND_STATUS')}} as date) as data_envio,
-    from padronizados
+    from source
 ),
 
 deduplicated as (
     select *
     from final
-    qualify row_number() over (partition by id_vacinacao_global order by data_envio asc, data_envio desc) = 1
+    qualify row_number() over (partition by id_vacinacao_global order by ranking asc) = 1
 )
 
 select * from deduplicated
