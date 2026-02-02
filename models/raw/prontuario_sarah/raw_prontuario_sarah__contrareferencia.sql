6f9a442c4b3db2071d79c7400fe9a79703aeadb6
Matheus Avellar <matheus.avellar@dados.rio>
2026-02-02 10:39:37 -0300

    fix: contraref -> contrarref

--- models/raw/prontuario_sarah/raw_prontuario_sarah__contrareferencia.sql
+++ models/raw/prontuario_sarah/raw_prontuario_sarah__contrareferencia.sql
@@ -1,59 +1,61 @@
 {{
     config(
-        alias="contrareferencia",
+        alias="contrarreferencia",
         materialized="table",
         schema='brutos_prontuario_sarah',
     )
 }}
 
 with source as (
-    select 
-        * 
+    select
+        *
     from {{ source("brutos_prontuario_sarah_api_staging", "atendimento_continuo") }}
     qualify row_number() over (partition by source_id order by datalake_loaded_at desc) = 1
 ),
 
-    contrareferencia_filtrado as (
+    contrarreferencia_filtrado as (
         select 
             source_id,
-            doc as doc_contrareferencia
+            doc as doc_cr
         from source, 
         unnest(json_extract_array(data, '$.documentos')) AS doc
-        where json_value(doc, '$.tipo') = 'CONTRAREFERÊNCIA'
+        where json_value(doc, '$.tipo') in (
+            'CONTRAREFERÊNCIA',  -- Atualmente mandam assim
+            'CONTRARREFERÊNCIA'  -- Por precaução caso um dia consertem
+        )
     ),
 
-    contrareferencia as (
+    contrarreferencia as (
         select
             source_id,
-            json_extract_scalar(doc_contrareferencia, '$.dados.unidade.cnes') as id_cnes,
-            json_extract_scalar(doc_contrareferencia, '$.dados.unidade.nome') as unidade_nome,
-            json_extract_scalar(doc_contrareferencia, '$.dados.unidade.municipio') as unidade_municipio,
+            json_extract_scalar(doc_cr, '$.dados.unidade.cnes') as id_cnes,
+            json_extract_scalar(doc_cr, '$.dados.unidade.nome') as unidade_nome,
+            json_extract_scalar(doc_cr, '$.dados.unidade.municipio') as unidade_municipio,
 
-            json_extract_scalar(doc_contrareferencia, '$.id') as id_contrareferencia,
-            json_extract_scalar(doc_contrareferencia, '$.numero') as contraferencia_numero,
-            json_extract_scalar(doc_contrareferencia, '$.datahora') as contrareferencia_datahora,
-            
+            json_extract_scalar(doc_cr, '$.id') as id_contrarreferencia,
+            json_extract_scalar(doc_cr, '$.numero') as contrarreferencia_numero,
+            json_extract_scalar(doc_cr, '$.datahora') as contrarreferencia_datahora,
 
-            {{ base64_to_string("json_extract_scalar(doc_contrareferencia, '$.dados.motivo')") }} as motivo,
-            {{ base64_to_string("json_extract_scalar(doc_contrareferencia, '$.dados.resultados')") }} as resultados,
-            {{ base64_to_string("json_extract_scalar(doc_contrareferencia, '$.dados.conduta')") }} as conduta,
-            {{ base64_to_string("json_extract_scalar(doc_contrareferencia, '$.dados.impressao')") }} as impressao,
-            {{ base64_to_string("json_extract_scalar(doc_contrareferencia, '$.dados.conduta_seguimento')") }} as conduta_seguimento,
-            {{ base64_to_string("json_extract_scalar(doc_contrareferencia, '$.dados.resumo')") }} as resumo,
+            {{ base64_to_string("json_extract_scalar(doc_cr, '$.dados.motivo')") }} as motivo,
+            {{ base64_to_string("json_extract_scalar(doc_cr, '$.dados.resultados')") }} as resultados,
+            {{ base64_to_string("json_extract_scalar(doc_cr, '$.dados.conduta')") }} as conduta,
+            {{ base64_to_string("json_extract_scalar(doc_cr, '$.dados.impressao')") }} as impressao,
+            {{ base64_to_string("json_extract_scalar(doc_cr, '$.dados.conduta_seguimento')") }} as conduta_seguimento,
+            {{ base64_to_string("json_extract_scalar(doc_cr, '$.dados.resumo')") }} as resumo,
 
-            json_extract_scalar(doc_contrareferencia, '$.dados.cid_principal') as cid_principal,
-            json_extract_scalar(doc_contrareferencia, '$.dados.diagnostico_principal') as diagnostico_principal,
-            json_extract_scalar(doc_contrareferencia, '$.dados.encaminhamento') as encaminhamento,
+            json_extract_scalar(doc_cr, '$.dados.cid_principal') as cid_principal,
+            json_extract_scalar(doc_cr, '$.dados.diagnostico_principal') as diagnostico_principal,
+            json_extract_scalar(doc_cr, '$.dados.encaminhamento') as encaminhamento,
 
-            json_extract_scalar(doc_contrareferencia, '$.dados.problema') as problema,
-            json_extract_scalar(doc_contrareferencia, '$.dados.motivo_coicide') as motivo_coicide,
+            json_extract_scalar(doc_cr, '$.dados.problema') as problema,
+            json_extract_scalar(doc_cr, '$.dados.motivo_coicide') as motivo_coicide,
 
-            json_extract_scalar(doc_contrareferencia, '$.profissional.nome') as profissional_nome,
-            json_extract_scalar(doc_contrareferencia, '$.profissional.cpf') as profissional_cpf,
-            json_extract_scalar(doc_contrareferencia, '$.profissional.cns') as profissional_cns,
-            json_extract_scalar(doc_contrareferencia, '$.profissional.cbo') as profissional_cbo
+            json_extract_scalar(doc_cr, '$.profissional.nome') as profissional_nome,
+            json_extract_scalar(doc_cr, '$.profissional.cpf') as profissional_cpf,
+            json_extract_scalar(doc_cr, '$.profissional.cns') as profissional_cns,
+            json_extract_scalar(doc_cr, '$.profissional.cbo') as profissional_cbo
 
-        from contrareferencia_filtrado
+        from contrarreferencia_filtrado
     ),
 
     cast_rename as (
@@ -62,9 +64,9 @@ with source as (
             safe_cast({{ process_null('id_cnes') }} as string) as id_cnes,
             safe_cast({{ process_null('unidade_nome') }} as string) as unidade_nome,
             safe_cast({{ process_null('unidade_municipio') }} as string) as unidade_municipio,
-            safe_cast({{ process_null('id_contrareferencia') }} as string) as id_documento,
-            safe_cast({{ process_null('contraferencia_numero') }} as string) as contraferencia_numero,
-            safe_cast({{ process_null('contrareferencia_datahora') }} as datetime) as contrareferencia_datahora,
+            safe_cast({{ process_null('id_contrarreferencia') }} as string) as id_documento,
+            safe_cast({{ process_null('contrarreferencia_numero') }} as string) as contrarreferencia_numero,
+            safe_cast({{ process_null('contrarreferencia_datahora') }} as datetime) as contrarreferencia_datahora,
 
             safe_cast({{ process_null('motivo') }} as string) as motivo,
             safe_cast({{ process_null('resultados') }} as string) as resultados,
@@ -84,7 +86,7 @@ with source as (
             safe_cast({{ process_null('profissional_cpf') }} as string) as profissional_cpf,
             safe_cast({{ process_null('profissional_cns') }} as string) as profissional_cns,
             safe_cast({{ process_null('profissional_cbo') }} as string) as profissional_cbo
-        from contrareferencia
+        from contrarreferencia
     )
 
 select * from cast_rename
