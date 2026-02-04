a4fae5b338d7a95841a5b55f385140cdc75c5ba2
Herian <herian.cavalcante@live.com>
2026-02-04 17:33:00 -0300

    fix: rename macro to correctly reflect its purpose for future datetime parsing

--- macros/parse_and_filter_future_datetime.sql
+++ macros/parse_and_filter_future_datetime.sql
@@ -1,4 +1,4 @@
-{% macro parse_and_filter_datetime(column_name) %}
+{% macro parse_and_filter_future_datetime(column_name) %}
     case
         when safe_cast({{ column_name }} as datetime) > current_datetime('America/Sao_Paulo') then null
         else safe_cast({{ column_name }} as datetime)
