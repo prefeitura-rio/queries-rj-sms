207fa6a70b9f027044592c4b46c870fe5b28ac88
Daniel Lira <daniellira@icloud.com>
2026-02-05 10:30:42 -0300

    update sources

--- models/raw/sipni/raw_sipni__vacinacao.sql
+++ models/raw/sipni/raw_sipni__vacinacao.sql
@@ -7,49 +7,35 @@
 
 with
     source as (
-        select * from {{ source("brutos_sipni_staging", "vacinacao_pentavalente_rotina") }}
+        select * from {{ source("brutos_sipni_staging", "vacinacao_historico") }}
     ),
 
     casted as (
         select
-            nu_cns_paciente,
-            nu_cpf_paciente,
-            no_paciente,
-            TIMESTAMP_MICROS(CAST(dt_nascimento_paciente / 1000 AS INT64)) as dt_nascimento_paciente,
-            nu_idade_paciente,
-            tp_sexo_paciente,
-            no_mae_paciente,
-            no_pai_paciente,
-            no_bairro_paciente,
-            nu_cep_paciente,
-            co_municipio_paciente,
-            no_municipio_paciente,
-            no_uf_paciente,
-            co_pais_paciente,
-            no_pais_paciente,
-            co_cnes_estabelecimento,
-            no_razao_social_estabelecimento,
-            no_fantasia_estalecimento,
-            co_municipio_estabelecimento,
-            no_municipio_estabelecimento,
-            no_uf_estabelecimento,
-            ds_tipo_estabelecimento,
-            ds_sub_tipo_estabelecimento,
-            co_vacina,
-            ds_vacina,
-            sg_vacina,
-            co_dose_vacina,
-            ds_dose_vacina,
-            TIMESTAMP_MICROS(CAST(dt_vacina / 1000 AS INT64)) as dt_vacina,
-            co_lote_vacina,
-            co_estrategia_vacinacao,
-            ds_estrategia_vacinacao,
-            co_vacina_fabricante,
-            ds_vacina_fabricante,
-            co_vacina_grupo_atendimento,
-            ds_vacina_grupo_atendimento,
-            co_vacina_categoria_atendimento,
-            ds_vacina_categoria_atendimento
+            safe_cast(co_identificador_sistema as string) as id_vacinacao,
+            safe_cast(co_cnes_estabelecimento as string) as id_cnes,
+            safe_cast(nu_cns_paciente as string) as paciente_cns,
+            safe_cast(nu_cpf_paciente as string) as paciente_cpf,
+            safe_cast(no_paciente as string) as paciente_nome,
+            safe_cast(dt_nascimento_paciente as date) as paciente_nascimento_data,
+            case when tp_sexo_paciente = 'M' then 'Masculino'
+                 when tp_sexo_paciente = 'F' then 'Feminino'
+                 else null
+            end as paciente_sexo,
+            safe_cast(no_mae_paciente as string) as paciente_nome_mae,
+            safe_cast(no_pai_paciente as string) as paciente_nome_pai,
+            safe_cast(no_raca_cor_paciente as string) as paciente_raca_cor,
+            safe_cast(nu_telefone_paciente as string) as paciente_telefone,
+            safe_cast(ds_email_paciente as string) as paciente_email,
+            safe_cast(no_uf_paciente as string) as paciente_uf_nome,
+            safe_cast(ds_vacina as string) as vacina_nome,
+            safe_cast(ds_dose_vacina as string) as vacina_dose,
+            safe_cast(dt_vacina as string) as vacina_data,
+            safe_cast(co_lote_vacina as string) as vacina_lote,
+            safe_cast(ds_estrategia_vacinacao as string) as vacina_estrategia,
+            safe_cast(ds_local_aplicacao as string) as vacina_local_aplicacao,
+            safe_cast(ds_origem_registro as string) as vacina_tipo_registro,
+            safe_cast(co_profissional_vacina as string) as profissional_codigo,
         from source
     )
 select *
