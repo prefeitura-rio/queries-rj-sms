428e1f80bfa11fe32e991115d9d8d1f0fd9f8446
Daniel Lira <daniellira@icloud.com>
2026-02-10 11:31:40 -0300

    feat/ intermediario sipni

--- models/intermediate/dit/historico_clinico/vacinacao/int_historico_clinico__vacinacao__sipni.sql
+++ models/intermediate/dit/historico_clinico/vacinacao/int_historico_clinico__vacinacao__sipni.sql
@@ -41,12 +41,12 @@ with
             c.situacao_usuario,
             row_number() over (
                 partition by 
-                    safe_cast(v.nu_cpf_paciente as string),
-                    safe_cast(v.dt_vacina as date),
-                    lower(safe_cast(v.ds_vacina as string))
+                    safe_cast(v.paciente_cpf as string),
+                    safe_cast(v.vacina_aplicacao_data as date),
+                    lower(safe_cast(v.vacina_nome as string))
                 order by
                     case 
-                        when c.id_cnes = safe_cast(v.co_cnes_estabelecimento as string) then 1
+                        when c.id_cnes = v.id_cnes then 1
                         else 2
                     end,
                     greatest(
@@ -57,53 +57,50 @@ with
             ) as rn
         from vacina v
         left join cadastro_vitacare c
-            on c.cpf = safe_cast(v.nu_cpf_paciente as string)
+            on c.cpf = safe_cast(v.paciente_cpf as string)
     ),
 
     casted_normalized as (
         select 
             safe_cast(null as string) as id_vacinacao,
-            safe_cast(v.co_cnes_estabelecimento as string) as id_cnes,
+            safe_cast(v.id_cnes as string) as id_cnes,
             safe_cast(v.codigo_equipe as string) as id_equipe,
             safe_cast(v.ine_equipe as string) as id_ine_equipe,
             safe_cast(v.microarea as string) as id_microarea,
             safe_cast(null as string) as paciente_id_prontuario,
-            safe_cast(v.nu_cns_paciente as string) as paciente_cns,
-            safe_cast(v.nu_cpf_paciente as string) as paciente_cpf,
+            safe_cast(v.paciente_cns as string) as paciente_cns,
+            safe_cast(v.paciente_cpf as string) as paciente_cpf,
             {{ proper_estabelecimento("safe_cast(e.nome_limpo as string)") }} as estabelecimento_nome,
             safe_cast(v.equipe as string) as equipe_nome,   
-            safe_cast(null as string) as profissional_nome,
+            {{ proper_br("safe_cast(v.profissional_nome as string)") }} as profissional_nome,
             safe_cast(null as string) as profissional_cbo,
             safe_cast(null as string) as profissional_cns,
             safe_cast(null as string) as profissional_cpf,
-            lower(safe_cast(v.ds_vacina as string)) as vacina_descricao,
+            lower(safe_cast(v.vacina_nome as string)) as vacina_descricao,
             lower(
                 safe_cast(
-                    {{ remove_accents_upper("replace(replace(v.ds_dose_vacina, 'º', ''), 'ª', '')") }}
+                    {{ remove_accents_upper("replace(replace(v.vacina_dose, 'º', ''), 'ª', '')") }}
                 as string)
             ) as vacina_dose,
-            safe_cast(v.co_lote_vacina as string) as vacina_lote,
-            safe_cast(null as string) as vacina_registro_tipo,
-            safe_cast(null as string) as vacina_estrategia,
+            safe_cast(v.vacina_lote as string) as vacina_lote,
+            safe_cast(vacina_tipo_registro as string) as vacina_registro_tipo,
+            safe_cast(vacina_estrategia as string) as vacina_estrategia,
             safe_cast(null as string) as vacina_diff,
-            safe_cast(v.dt_vacina as date) as vacina_aplicacao_data,
-            safe_cast(null as date) as vacina_registro_data,
-            {{ proper_br("safe_cast(v.no_paciente as string)") }} as paciente_nome,
-            case when safe_cast(v.tp_sexo_paciente as string) = 'M' then 'masculino'
-                 when safe_cast(v.tp_sexo_paciente as string) = 'F' then 'feminino'
-                 else null 
-            end as paciente_sexo,
-            safe_cast(v.dt_nascimento_paciente as date) as paciente_nascimento_data,
-            {{ proper_br("safe_cast(v.no_mae_paciente as string)") }} as paciente_nome_mae,
+            safe_cast(v.vacina_aplicacao_data as date) as vacina_aplicacao_data,
+            safe_cast(v.vacina_aplicacao_data as date) as vacina_registro_data,
+            {{ proper_br("safe_cast(v.paciente_nome as string)") }} as paciente_nome,
+            safe_cast(v.paciente_sexo as string) as paciente_sexo,
+            safe_cast(v.paciente_nascimento_data as date) as paciente_nascimento_data,
+            {{ proper_br("safe_cast(v.paciente_nome_mae as string)") }} as paciente_nome_mae,
             safe_cast(null as date) as paciente_mae_nascimento_data,
             safe_cast(v.situacao_usuario as string) as paciente_situacao,
             safe_cast(null as date) as paciente_cadastro_data,
             safe_cast(null as boolean) as paciente_obito,
-            safe_cast(null as datetime) as loaded_at,
-            safe_cast(v.dt_vacina as date) as particao_registro_vacinacao
+            safe_cast(loaded_at as datetime) as loaded_at,
+            safe_cast(v.vacina_aplicacao_data as date) as particao_registro_vacinacao
         from joined v
         left join estabelecimento e
-            on e.id_cnes = safe_cast(v.co_cnes_estabelecimento as string)
+            on e.id_cnes = v.id_cnes 
         where v.rn = 1
     )
     
--- models/raw/sipni/raw_sipni__vacinacao.sql
+++ models/raw/sipni/raw_sipni__vacinacao.sql
@@ -10,33 +10,37 @@ with
         select * from {{ source("brutos_sipni_staging", "vacinacao_historico") }}
     ),
 
-    casted as (
+    casted_renamed as (
         select
-            safe_cast(co_identificador_sistema as string) as id_vacinacao,
-            safe_cast(co_cnes_estabelecimento as string) as id_cnes,
-            safe_cast(nu_cns_paciente as string) as paciente_cns,
-            safe_cast(nu_cpf_paciente as string) as paciente_cpf,
-            safe_cast(no_paciente as string) as paciente_nome,
+            safe_cast({{ process_null('co_identificador_sistema') }} as string) as id_vacinacao,
+            safe_cast({{ process_null('co_cnes_estabelecimento') }} as string) as id_cnes,
+            safe_cast({{ process_null('nu_cns_paciente') }} as string) as paciente_cns,
+            safe_cast({{ process_null('nu_cpf_paciente') }} as string) as paciente_cpf,
+            safe_cast({{ process_null('no_paciente') }} as string) as paciente_nome,
             safe_cast(dt_nascimento_paciente as date) as paciente_nascimento_data,
-            case when tp_sexo_paciente = 'M' then 'Masculino'
-                 when tp_sexo_paciente = 'F' then 'Feminino'
-                 else null
+            case 
+                when {{ process_null('tp_sexo_paciente') }} = 'M' then 'Masculino'
+                when {{ process_null('tp_sexo_paciente') }} = 'F' then 'Feminino'
+                else null
             end as paciente_sexo,
-            safe_cast(no_mae_paciente as string) as paciente_nome_mae,
-            safe_cast(no_pai_paciente as string) as paciente_nome_pai,
-            safe_cast(no_raca_cor_paciente as string) as paciente_raca_cor,
-            safe_cast(nu_telefone_paciente as string) as paciente_telefone,
-            safe_cast(ds_email_paciente as string) as paciente_email,
-            safe_cast(no_uf_paciente as string) as paciente_uf_nome,
-            safe_cast(ds_vacina as string) as vacina_nome,
-            safe_cast(ds_dose_vacina as string) as vacina_dose,
-            safe_cast(dt_vacina as string) as vacina_data,
-            safe_cast(co_lote_vacina as string) as vacina_lote,
-            safe_cast(ds_estrategia_vacinacao as string) as vacina_estrategia,
-            safe_cast(ds_local_aplicacao as string) as vacina_local_aplicacao,
-            safe_cast(ds_origem_registro as string) as vacina_tipo_registro,
-            safe_cast(co_profissional_vacina as string) as profissional_codigo,
+            safe_cast({{ process_null('no_mae_paciente') }} as string) as paciente_nome_mae,
+            safe_cast({{ process_null('no_pai_paciente') }} as string) as paciente_nome_pai,
+            safe_cast({{ process_null('no_raca_cor_paciente') }} as string) as paciente_raca_cor,
+            safe_cast({{ process_null('nu_telefone_paciente') }} as string) as paciente_telefone,
+            safe_cast({{ process_null('ds_email_paciente') }} as string) as paciente_email,
+            safe_cast({{ process_null('no_uf_paciente') }} as string) as paciente_uf_nome,
+            safe_cast({{ process_null('ds_vacina') }} as string) as vacina_nome,
+            safe_cast({{ process_null('ds_dose_vacina') }} as string) as vacina_dose,
+            safe_cast(dt_vacina as date) as vacina_aplicacao_data,
+            safe_cast({{ process_null('co_lote_vacina') }} as string) as vacina_lote,
+            safe_cast({{ process_null('ds_estrategia_vacinacao') }} as string) as vacina_estrategia,
+            safe_cast({{ process_null('ds_local_aplicacao') }} as string) as vacina_local_aplicacao,
+            safe_cast({{ process_null('ds_origem_registro') }} as string) as vacina_tipo_registro,
+            safe_cast({{ process_null('co_profissional_vacina') }} as string) as profissional_codigo,
+            safe_cast({{ process_null('no_profissional_vacina') }} as string) as profissional_nome,
+            safe_cast(dt_entrada_datalake as datetime) as loaded_at
         from source
     )
+
 select *
-from casted
+from casted_renamed
