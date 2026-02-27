d60efbd98da2b73f9be487576b020ba0720e530a
Christian Basilio <Christianbasilio97@gmail.com>
2026-02-27 10:41:33 -0300

    Adicionando os derivados ao Filtros - Correção na Contagem

--- models/marts/dit/cdi/controle_interno_externo/mart_cdi__controle_interno_externo_filtros.sql
+++ models/marts/dit/cdi/controle_interno_externo/mart_cdi__controle_interno_externo_filtros.sql
@@ -6,10 +6,64 @@
     ) 
 }}
 
-with base as (
+with regulares as (
 
-    select *
+    select
+        subsecretaria___setor,
+        data_de_entrada,
+        coalesce(manifestacao, 'Sem informação') as manifestacao,
+        status,
+        unidade_ap,
+        vencimento_1,
+        coalesce(relator_auditor, 'Sem informação ou Sem Relator') as relator_auditor,
+        data_da_ultima_atualizacao,
+        orgao_demandante,
+        coalesce(tipo_de_demanda, 'Sem informação') as tipo_de_demanda,
+        cast(id as string) as id,
+        'Regular' as tipo_registro
     from {{ ref('int_cdi__controle_interno_externo') }}
+
+),
+
+pais as (
+
+    -- enriquece os derivados com os mesmos atributos de filtro
+    select distinct
+        processorio_sei,
+        orgao_demandante,
+        manifestacao,
+        tipo_de_demanda,
+        unidade_ap
+    from {{ ref('int_cdi__controle_interno_externo') }}
+
+),
+
+derivados as (
+    -- criando a derivados com info da regular para conseguir filtrar
+    select
+        d.subsecretaria___setor,
+        d.data_de_emissao as data_de_entrada,               
+        coalesce(p.manifestacao, 'Sem informação') as manifestacao,
+        d.status,
+        p.unidade_ap,
+        d.vencimento as vencimento_1,                      
+        'Sem informação ou Sem Relator' as relator_auditor,
+        d.data_da_ultima_atualizacao,
+        p.orgao_demandante,
+        coalesce(p.tipo_de_demanda, 'Sem informação') as tipo_de_demanda,
+        cast(d.id as string) as id,
+        'Derivado' as tipo_registro
+    from {{ ref('int_cdi__controle_interno_externo_derivados') }} d
+    left join pais p
+        on d.processorio = p.processorio_sei
+
+),
+
+base_union as (
+
+    select * from regulares
+    union all
+    select * from derivados
 
 ),
 
@@ -18,19 +72,23 @@ base_tratada as (
     select
         subsecretaria___setor,
         data_de_entrada,
-        case when manifestacao is null then 'Sem informação' else manifestacao end as manifestacao,
+        manifestacao,
         status,
         unidade_ap,
         vencimento_1,
-        case when relator_auditor is null then 'Sem informação ou Sem Relator' else relator_auditor end as relator_auditor,
+        relator_auditor,
         data_da_ultima_atualizacao,
         orgao_demandante,
-        case when tipo_de_demanda is null then 'Sem informação' else tipo_de_demanda end as tipo_de_demanda,
-        count(id) as total_processos -- para a contagem ficar o mais correta possivel
-    from base
+        tipo_de_demanda,
+
+        -- total geral (regulares + derivados) isso é feito pra evitar que os ids sejam iguais 
+        count(concat(tipo_registro, '|', id)) as total_processos,
+        count(if(tipo_registro = 'Regular', id, null)) as total_regulares,
+        count(if(tipo_registro = 'Derivado', id, null)) as total_derivados
+
+    from base_union
     group by 1,2,3,4,5,6,7,8,9,10
 
 )
 
-select *
-from base_tratada
+select * from base_tratada
