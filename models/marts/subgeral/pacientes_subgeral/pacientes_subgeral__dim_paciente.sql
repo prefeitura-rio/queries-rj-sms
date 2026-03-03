c69bcea29dbbfbbf056ca845a6dfb31e56c208c3
miloskimatheus <matheus.miloski@dados.rio>
2026-03-03 16:35:56 -0300

    (vigilancia-cancer) remove patients telephones (going to use another src in the future)

--- models/marts/subgeral/pacientes_subgeral/pacientes_subgeral__dim_paciente.sql
+++ models/marts/subgeral/pacientes_subgeral/pacientes_subgeral__dim_paciente.sql
@@ -34,22 +34,21 @@ agg as (
     array_agg(distinct cast(paciente_data_nascimento as string) ignore nulls) as datas_nascimento,
     array_agg(distinct paciente_sexo ignore nulls) as sexos,
     array_agg(distinct paciente_racacor ignore nulls) as racas_cores,
+    
+    --array_agg(distinct paciente_uf_nascimento ignore nulls) as ufs_nascimento,
+    --array_agg(distinct paciente_municipio_nascimento ignore nulls) as municipios_nascimento,
 
-    /*
-    array_agg(distinct paciente_uf_nascimento ignore nulls) as ufs_nascimento,
-    array_agg(distinct paciente_municipio_nascimento ignore nulls) as municipios_nascimento,
+    --array_agg(distinct paciente_uf_residencia ignore nulls) as ufs_residencia,
+    --array_agg(distinct paciente_municipio_residencia ignore nulls) as municipios_residencia,
+    --array_agg(distinct paciente_bairro_residencia ignore nulls) as bairros_residencia,
+    --array_agg(distinct paciente_cep_residencia ignore nulls) as ceps_residencia,
+    --array_agg(distinct paciente_endereco_residencia ignore nulls) as enderecos_residencia,
+    --array_agg(distinct paciente_complemento_residencia ignore nulls) as complementos_residencia,
+    --array_agg(distinct paciente_numero_residencia ignore nulls) as numeros_residencia,
+    --array_agg(distinct paciente_tp_logradouro_residencia ignore nulls) as tipos_logradouro_residencia,
+    
 
-    array_agg(distinct paciente_uf_residencia ignore nulls) as ufs_residencia,
-    array_agg(distinct paciente_municipio_residencia ignore nulls) as municipios_residencia,
-    array_agg(distinct paciente_bairro_residencia ignore nulls) as bairros_residencia,
-    array_agg(distinct paciente_cep_residencia ignore nulls) as ceps_residencia,
-    array_agg(distinct paciente_endereco_residencia ignore nulls) as enderecos_residencia,
-    array_agg(distinct paciente_complemento_residencia ignore nulls) as complementos_residencia,
-    array_agg(distinct paciente_numero_residencia ignore nulls) as numeros_residencia,
-    array_agg(distinct paciente_tp_logradouro_residencia ignore nulls) as tipos_logradouro_residencia,
-    */
-
-    array_agg(distinct paciente_telefone ignore nulls) as telefones,
+    --array_agg(distinct paciente_telefone ignore nulls) as telefones,
     --array_agg(distinct paciente_email ignore nulls) as emails,
 
     array_agg(distinct paciente_obito_ano ignore nulls) as anos_obito,
@@ -81,22 +80,20 @@ final as (
 
     sexos,
     racas_cores,
+    
+    --ufs_nascimento,
+    --municipios_nascimento,
 
-    /*
-    ufs_nascimento,
-    municipios_nascimento,
+    --ufs_residencia,
+    --municipios_residencia,
+    --bairros_residencia,
+    --ceps_residencia,
+    --enderecos_residencia,
+    --complementos_residencia,
+    --numeros_residencia,
+    --tipos_logradouro_residencia,
 
-    ufs_residencia,
-    municipios_residencia,
-    bairros_residencia,
-    ceps_residencia,
-    enderecos_residencia,
-    complementos_residencia,
-    numeros_residencia,
-    tipos_logradouro_residencia,
-    */
-
-    telefones,
+    --telefones,
     --emails,
     
     anos_obito,
