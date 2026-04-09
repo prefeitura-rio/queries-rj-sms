d9f47198e6b3be5e6761d2e352dbb61fc22a3d8d
Daniel Lira <danielllira@icloud.com>
2026-04-09 14:05:54 -0300

    add armazen to the dedup

--- models/intermediate/dit/estoque/int_estoque__adaptador_versao_nova_posicao.sql
+++ models/intermediate/dit/estoque/int_estoque__adaptador_versao_nova_posicao.sql
@@ -27,6 +27,20 @@ with
         from estoque_posicao_formato_novo
         where material_quantidade > 0
     )
+    qualify row_number() over (
+            partition by
+                id_cnes,
+                id_lote,
+                id_material,
+                id_atc,
+                lote_status,
+                lote_data_cadastro,
+                lote_data_vencimento,
+                data_particao,
+                data_carga,
+                armazem
+            order by data_replicacao desc
+        ) = 1
 
 select *
 from final
