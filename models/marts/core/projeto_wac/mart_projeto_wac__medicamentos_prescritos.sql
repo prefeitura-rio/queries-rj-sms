9a6ebd0fbfa19aaa8395446c75382041e80cf2a3
Pedro <pedrovitor.marques@gmail.com>
2026-05-18 16:12:16 -0300

    refactor: update SQL model to use references for prescricao and acto tables

--- models/marts/core/projeto_wac/mart_projeto_wac__medicamentos_prescritos.sql
+++ models/marts/core/projeto_wac/mart_projeto_wac__medicamentos_prescritos.sql
@@ -10,8 +10,8 @@ SELECT
   posologia,
   quantidade,
   uso_continuado 
-FROM `rj-sms.brutos_prontuario_vitacare_historico.prescricao` p
-  INNER JOIN `rj-sms.brutos_prontuario_vitacare_historico.acto` a using (id_prontuario_global)
+FROM {{ ref('raw_prontuario_vitacare_historico__prescricao') }} p
+  INNER JOIN {{ ref('raw_prontuario_vitacare_historico__acto') }} a using (id_prontuario_global)
 WHERE 
   a.id_cnes = '2280787' and 
   a.patient_cpf is not null
