845dccb03083a60eabcda80a7cb4f8cf8b447ce3
Pedro <pedrovitor.marques@gmail.com>
2026-05-18 21:50:17 -0300

    feat: rename internacoes alias to urgencia_emergencia and enhance SQL model with additional fields for emergency cases

--- models/marts/core/projeto_wac/mart_projeto_wac__internacoes.sql
+++ models/marts/core/projeto_wac/mart_projeto_wac__internacoes.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        alias="internacoes"
+        alias="urgencia_emergencia"
     )
 }}
 
@@ -14,16 +14,31 @@ cadastros as (
     id_cnes = '2280787'
     and cpf is not NULL
     and ine_equipe is not NULL
+),
+
+atendimentos as (
+  select 
+    gid_boletim,
+    cid_codigo,
+    cid_nome,
+    updated_at
+  from {{ ref('raw_prontuario_vitai__atendimento') }}
+  qualify row_number() over (
+    partition by gid_boletim
+    order by updated_at desc
+  ) = 1
 )
 
 select
   SHA256(p.cpf) as paciente_id,
   trim(b.atendimento_tipo) as atendimento_tipo,
+  cid_codigo,
+  cid_nome,
   b.data_entrada as atendido_em
 from {{ ref('raw_prontuario_vitai__boletim') }} b
-  inner join {{ ref('raw_prontuario_vitai__paciente') }} p
-    on p.gid = b.gid_paciente
+  inner join {{ ref('raw_prontuario_vitai__paciente') }} p on p.gid = b.gid_paciente
+  inner join atendimentos a on b.gid = a.gid_boletim
 where
   p.cpf in (select paciente_id from cadastros)
   and p.cpf is not null 
-  and trim(b.atendimento_tipo) in ('INTERNACAO')
+  and trim(b.atendimento_tipo) in ('INTERNACAO','EMERGENCIA')
