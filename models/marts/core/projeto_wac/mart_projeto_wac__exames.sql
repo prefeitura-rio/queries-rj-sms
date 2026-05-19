51b427d347db6aaaa00303592761b82040ffb7f5
Pedro <pedrovitor.marques@gmail.com>
2026-05-19 17:22:32 -0300

    feat: add datahora_pedido field to exames SQL model for enhanced result tracking

--- models/marts/core/projeto_wac/mart_projeto_wac__exames.sql
+++ models/marts/core/projeto_wac/mart_projeto_wac__exames.sql
@@ -20,7 +20,8 @@ SELECT
     sha256(s.paciente_cpf) as paciente_id, 
     descricao_apoio as nome_exame,
     r.resultado,
-    r.unidade
+    r.unidade,
+    s.datahora_pedido
 FROM  {{ref('raw_exames_laboratoriais__resultados')}} r
   inner join {{ref('raw_exames_laboratoriais__exames')}} e on r.id_exame = e.id
   inner join  {{ref('raw_exames_laboratoriais__solicitacoes')}} s on e.id_solicitacao = s.id
