68078b1f00924f19e5f88635e20187e4d7cf0a71
Pedro <pedrovitor.marques@gmail.com>
2026-05-21 00:05:13 -0300

    feat: add common procedures aggregation to SQL model for enhanced patient data analysis, filtering procedures with over 1000 occurrences

--- models/marts/core/hackathon_anthropic/mart_hackathon_anthropic__regulacoes.sql
+++ models/marts/core/hackathon_anthropic/mart_hackathon_anthropic__regulacoes.sql
@@ -17,6 +17,20 @@ elegiveis as (
     original.cpf,
     original.shift_dias
   from {{ ref('mart_hackathon_anthropic__elegiveis') }}
+),
+
+procedimentos_comuns as (
+  select
+    coalesce(procedimento_sigtap, procedimento_interno) as procedimento,
+    count(*) as total_ocorrencias
+  from {{ ref('raw_sisreg_api__marcacoes') }} m
+    inner join elegiveis e on m.paciente_cpf = e.cpf
+    cross join constantes
+  where
+    date(data_marcacao) between (select data_minima from constantes) and (select data_maxima from constantes)
+    and coalesce(procedimento_sigtap, procedimento_interno) is not null
+  group by 1
+  having count(*) > 1000
 )
 
 select
@@ -25,6 +39,7 @@ select
   date_add(date(m.data_marcacao), interval e.shift_dias day) as data_marcacao
 from {{ ref('raw_sisreg_api__marcacoes') }} m
   inner join elegiveis e on m.paciente_cpf = e.cpf
+  inner join procedimentos_comuns pc on coalesce(m.procedimento_sigtap, m.procedimento_interno) = pc.procedimento
   cross join constantes
 where
   m.paciente_cpf is not null
