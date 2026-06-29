174d0f063f61a6ee4ec404586680b324eaaaa916
Pedro <pedrovitor.marques@gmail.com>
2026-06-29 18:46:11 -0300

    feat: aprimora modelo de profissionais com inclusão de equipe e lógica de filtragem

--- models/marts/acolher_mais/mart__acolher_mais__profissionais.sql
+++ models/marts/acolher_mais/mart__acolher_mais__profissionais.sql
@@ -13,7 +13,7 @@ ultima_particao_disponivel as (
 ),
 
 vinculos as (
-  select p.cpf, p.nome, right(v.id_unidade,7) as id_cnes, ep.id_cbo as ocupacao_cbo
+  select p.cpf, p.nome, right(v.id_unidade,7) as id_cnes, ep.id_cbo as ocupacao_cbo, e.equipe_ine, e.equipe_nome
   from {{ref('raw_gdb_cnes__vinculo')}} v
     inner join {{ref('raw_gdb_cnes__profissional')}} p using (id_profissional_sus)
     inner join {{ref('raw_gdb_cnes__equipe_profissionais')}} ep using (id_profissional_sus)
@@ -33,6 +33,12 @@ vinculos as (
           '225130', -- Medico de Fam e Comunidade
           '223116'  -- Medico de Saude da familia
         )
+      ) and
+      ep.data_desligamento_profissional is null and e.data_desativacao is null and 
+      e.id_equipe_tipo in (
+        '70', -- eSF: Equipe de Saúde da Família
+        '74', -- eABP: Equipe de Atenção Primária Prisional
+        '76' -- eAP: Equipe de Atenção Primária
       )
 ),
 
@@ -43,9 +49,32 @@ vinculos_atencao_primaria as (
   where e.tipo_sms_agrupado = 'APS'
 ),
 
-padronizando_vinculos as (
+ine_nome_equipe as (
   select 
-    cpf, nome, id_cnes,
+    equipe_ine,
+    equipe_nome
+  from vinculos_atencao_primaria
+  qualify row_number() over (
+    partition by equipe_ine
+    order by length(equipe_nome) desc
+  ) = 1
+),
+
+nome_cpf as (
+  select 
+    cpf,
+    nome
+  from vinculos_atencao_primaria
+  qualify row_number() over (
+    partition by cpf
+    order by length(nome) desc
+  ) = 1
+),
+
+padronizando_vinculos as (
+  select
+    distinct
+    cpf, id_cnes,
     case 
       when ocupacao_cbo = '515105' then 'ACS'
       when ocupacao_cbo = '322255' then 'ACS'
@@ -57,24 +86,22 @@ padronizando_vinculos as (
       when ocupacao_cbo = '225124' then 'MEDICO'
       when ocupacao_cbo = '225130' then 'MEDICO'
       when ocupacao_cbo = '223116' then 'MEDICO'
-    end as ocupacao_descricao
+    end as ocupacao_descricao,
+    equipe_ine
   from vinculos_atencao_primaria
 ),
 
-nome_cpf as (
-  select 
-    cpf,
-    nome
-  from padronizando_vinculos
-  qualify row_number() over (
-    partition by cpf
-    order by length(nome) desc
-  ) = 1
-),
-
 sem_duplicados as (
-  select distinct cpf, id_cnes, ocupacao_descricao
+  select 
+    cpf, 
+    nome_cpf.nome as nome,
+    id_cnes, 
+    ocupacao_descricao, 
+    equipe_ine,
+    ine_nome_equipe.equipe_nome as equipe_nome
   from padronizando_vinculos
+    inner join ine_nome_equipe using(equipe_ine)
+    inner join nome_cpf using(cpf)
 ),
 
 agrupado_por_pessoa as (
@@ -82,7 +109,7 @@ agrupado_por_pessoa as (
     cpf, 
     nome_cpf.nome, 
     array_agg(
-      struct(id_cnes, ocupacao_descricao)
+      struct(id_cnes, ocupacao_descricao, equipe_ine, equipe_nome)
     ) as vinculos
   from sem_duplicados
     left join nome_cpf using(cpf)
