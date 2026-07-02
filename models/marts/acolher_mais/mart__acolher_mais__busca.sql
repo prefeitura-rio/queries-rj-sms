f25dd0698a3ea82ac933f8daf1fed56e08a83651
Pedro <pedrovitor.marques@gmail.com>
2026-07-01 21:55:45 -0300

    feat: refactor busca query and add regulacoes model for patient requests

--- models/marts/acolher_mais/mart__acolher_mais__busca.sql
+++ models/marts/acolher_mais/mart__acolher_mais__busca.sql
@@ -12,10 +12,59 @@
 }}
 
 with
-    pacientes_busca as (
+    source_paciente_mart as (
         select
             *
-        from {{ ref('mart_historico_clinico_app__busca') }}
+        from {{ ref('mart_historico_clinico__paciente') }}
+    ),
+    source_paciente_app as (
+        select
+            *
+        from {{ ref('mart_historico_clinico_app__paciente') }}
+    ),
+
+    -- -----------------------------------------
+    -- Enriquecimento
+    -- -----------------------------------------
+    cns_mapeamento as (
+        select
+            cpf,
+            array_agg(valor_cns) as cns_lista 
+        from source_paciente_mart, unnest(cns) as valor_cns
+        group by 1
+    ),
+    nome_mae_mapeamento as (
+        select
+            cpf,
+            dados.mae_nome
+        from source_paciente_mart
+    ),
+
+    -- -----------------------------------------
+    -- Dados do paciente
+    -- -----------------------------------------
+    dados as (
+        select
+            cast(source_paciente_app.cpf as int64) as cpf_particao,
+
+            source_paciente_app.cpf,
+            cns_lista,
+
+            registration_name as nome,
+            social_name as nome_social,
+            birth_date as data_nascimento,
+            {{ calculate_age('cast(birth_date as date)') }} AS idade,
+            gender as genero,
+            nome_mae_mapeamento.mae_nome as nome_mae,
+            family_health_team.name as equipe_saude,
+            family_health_team.ine_code as equipe_saude_codigo_ine
+        from source_paciente_app
+            inner join cns_mapeamento on cns_mapeamento.cpf = source_paciente_app.cpf
+            inner join nome_mae_mapeamento on nome_mae_mapeamento.cpf = source_paciente_app.cpf
     )
+-- -=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
+-- FINAL
+-- -=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--
 select *
-from pacientes_busca
+from dados
+where {{ validate_cpf("cpf") }}
---
+++ models/marts/acolher_mais/mart__acolher_mais__regulacao.sql
@@ -0,0 +1,48 @@
+{{
+    config(
+        alias="regulacoes",
+        materialized="table",
+        partition_by={
+            "field": "cpf_particao",
+            "data_type": "int64",
+            "range": {"start": 0, "end": 100000000000, "interval": 34722222},
+        },
+    )
+}}
+
+
+with
+
+dados as (
+  select
+    cast(paciente_cpf as int64) as cpf_particao,
+    id_solicitacao,
+
+    solicitacao_status,
+    cid_solicitacao,
+
+    paciente_cpf,
+
+    data_solicitacao,
+    data_desejada,        
+    data_autorizacao,
+    data_execucao,
+    data_confirmacao,       
+    data_cancelamento,
+
+    cancelamento_autor,
+    justificativa_cancelamento,
+
+    procedimento_grupo,
+    procedimento,
+
+    central_solicitante,
+    unidade_executante,
+
+    profissional_solicitante_nome,
+  from {{ref('mart_sisreg__solicitacoes')}}
+  where cast(data_solicitacao as date) > date_sub(current_date(), interval 1 year)
+)
+
+select * 
+from dados
