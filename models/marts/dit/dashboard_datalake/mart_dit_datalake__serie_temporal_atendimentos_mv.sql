5778ea4ce7b906657d7babac8e2f4a1f90b2ca71
Herian <herian.cavalcante@live.com>
2026-07-17 14:35:18 -0300

    feat: add incremental models for atendimento data by unit and date

---
+++ models/marts/dit/dashboard_datalake/mart_dit_datalake__serie_temporal_atendimentos_mv.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        alias='serie_temporal_atendimentos_mv',
+        materialized='incremental',
+        incremental_strategy='insert_overwrite',
+        partition_by={
+            "field": "data_registro",
+            "data_type": "date",
+            "granularity": "month"
+        },
+        unique_key=['data_registro'],
+        description='Série temporal de atendimentos por data de entrada no prontuário MV',
+        tags=['datalake']
+    )
+}}
+
+{% set last_partition = get_last_partition_date(this) %}
+
+select   
+  date(atendimento_datahora) as data_registro,
+  count(id_atendimento) as atendimentos
+from {{ ref('raw_prontuario_mv__atendimento') }}
+{% if is_incremental() %}
+    where cast(atendimento_datahora as date) >= date('{{ last_partition }}')
+{% endif %}
+group by 1
