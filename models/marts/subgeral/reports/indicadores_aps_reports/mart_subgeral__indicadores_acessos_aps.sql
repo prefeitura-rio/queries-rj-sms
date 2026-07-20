6ea61a7a23a95a029df38cf1f5ae6533b700018f
Karen Pacheco <karenpacheco16@poli.ufrj.br>
2026-07-20 10:07:42 -0300

    define time responsavel pelo modelo

--- models/marts/subgeral/reports/indicadores_aps_reports/mart_subgeral__indicadores_acessos_aps.sql
+++ models/marts/subgeral/reports/indicadores_aps_reports/mart_subgeral__indicadores_acessos_aps.sql
@@ -3,7 +3,7 @@
     schema = "projeto_aps_reports",
     alias = "indicadores_acesso_aps",
     unique_key = "data_referencia",
-    meta={"owner": "karen"}
+    meta={"owner": "karen", "team": "subgeral"}
 ) }}
 
 -- Seleciona tipos de atendimento com pelo menos 30% realizados por médicos (CBO 225 ou 2231)
