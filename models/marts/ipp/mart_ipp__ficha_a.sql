229abf11f8c7f9399156dbf99c62284ce5b8068c
Herian <herian.cavalcante@live.com>
2026-07-29 13:16:01 -0300

    feat: update mart_ipp__ficha_a model with partitioning and detailed column descriptions

--- models/marts/ipp/mart_ipp__ficha_a.sql
+++ models/marts/ipp/mart_ipp__ficha_a.sql
@@ -2,18 +2,22 @@
     config(
         alias="ficha_a",
         schema="projeto_ipp",
-        materialized="incremental"
+        materialized="table",
+        partition_by={
+            "field": "cpf_particao",
+            "data_type": "int64",
+            "range": {"start": 0, "end": 100000000000, "interval": 34722222},
+        },
     )
 }}
 
 
--- TODO: Adiciona particionamento e desenvolver lógica incremental 
 with 
+-- As colunas comentadas foram solicitadas pelo IPP mas estão presentes apenas no ficha_a_v2 que foi descontinuado.
+
     ficha_a as (
         select
             cpf,
-            --cpf_valido,
-            nis,
             cns,
             nome,
             nome_social,
@@ -26,7 +30,7 @@ with
             territorio_social,
             familia_beneficiaria_cfc,
             familia_beneficiaria_auxilio_brasil,
-            -- hanseniase, 
+            -- hanseniase,
             -- tuberculose
         from {{ ref('raw_prontuario_vitacare__ficha_a') }}
     )
@@ -35,7 +39,6 @@ select
     cpf,
     {{validate_cpf('cpf')}} as cpf_valido,
     cns,
-    nis,
     nome,
     nome_social,
     nome_mae as mae_nome,
@@ -50,4 +53,5 @@ select
         true, 
         false
     ) as recebe_algum_beneficio,
+    safe_cast(cpf as int64) as cpf_particao
 from ficha_a
