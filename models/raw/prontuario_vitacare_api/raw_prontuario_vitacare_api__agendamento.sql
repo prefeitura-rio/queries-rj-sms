976c69711c1766f8c27415c625093ac3576e1432
Daniel Lira <danielllira@icloud.com>
2026-07-31 13:46:53 -0300

    feat: add process_null to agendamento cast CTE and fix teleconsulta yml docs

--- models/raw/prontuario_vitacare_api/raw_prontuario_vitacare_api__agendamento.sql
+++ models/raw/prontuario_vitacare_api/raw_prontuario_vitacare_api__agendamento.sql
@@ -33,28 +33,28 @@ with
 
   cast_agendamento as (
     select 
-      cast(patient_cpf as string) as patient_cpf,
-      cast(source_updated_at as datetime) as source_updated_at,
+      cast({{ process_null('patient_cpf') }} as string) as patient_cpf,
+      cast({{ process_null('source_updated_at') }} as datetime) as source_updated_at,
       cast(concat(nullif(payload_cnes,''),'.',nullif(source_id,'')) as string) as id_agendamento,
       
-      cast(json_extract_scalar(data,'$.datahora_agendamento') as datetime) as datahora_agendamento,
-      cast(json_extract_scalar(data,'$.datahora_marcacao_atendimento') as datetime) as datahora_marcacao_atendimento,
-      cast(json_extract_scalar(data,'$.profissional.cns') as string) as profissional_cns,
-      cast(json_extract_scalar(data,'$.profissional.cpf') as string) as profissional_cpf,
-      cast(json_extract_scalar(data,'$.profissional.nome') as string) as profissional_nome,
-      cast(json_extract_scalar(data,'$.profissional.cbo') as string) as profissional_cbo,
-      cast(json_extract_scalar(data,'$.profissional.cbo_descricao') as string) as profissional_cbo_descricao,
-      cast(json_extract_scalar(data,'$.profissional.equipe.nome') as string) as profissional_equipe_nome,
-      cast(json_extract_scalar(data,'$.profissional.equipe.cod_equipe') as string) as profissional_equipe_codigo,
-      cast(json_extract_scalar(data,'$.profissional.equipe.cod_ine') as string) as profissional_equipe_ine,
-      cast(json_extract_scalar(data,'$.motivo_cancelamento') as string) as motivo_cancelamento,
-      cast(json_extract_scalar(data,'$.consulta_realizada') as string) as consulta_realizada,
-      cast(json_extract_scalar(data,'$.tipo_atendimento') as string) as tipo_atendimento,
-      cast(json_extract_scalar(data,'$.estado_marcacao') as string) as estado_marcacao,
-      cast(json_extract_scalar(data,'$.unidade_cnes') as string) as id_cnes,
-      cast(json_extract_scalar(data,'$.ut_id') as string) as ut_id,
+      cast({{ process_null("json_extract_scalar(data,'$.datahora_agendamento')") }} as datetime) as datahora_agendamento,
+      cast({{ process_null("json_extract_scalar(data,'$.datahora_marcacao_atendimento')") }} as datetime) as datahora_marcacao_atendimento,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.cns')") }} as string) as profissional_cns,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.cpf')") }} as string) as profissional_cpf,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.nome')") }} as string) as profissional_nome,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.cbo')") }} as string) as profissional_cbo,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.cbo_descricao')") }} as string) as profissional_cbo_descricao,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.equipe.nome')") }} as string) as profissional_equipe_nome,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.equipe.cod_equipe')") }} as string) as profissional_equipe_codigo,
+      cast({{ process_null("json_extract_scalar(data,'$.profissional.equipe.cod_ine')") }} as string) as profissional_equipe_ine,
+      cast({{ process_null("json_extract_scalar(data,'$.motivo_cancelamento')") }} as string) as motivo_cancelamento,
+      cast({{ process_null("json_extract_scalar(data,'$.consulta_realizada')") }} as string) as consulta_realizada,
+      cast({{ process_null("json_extract_scalar(data,'$.tipo_atendimento')") }} as string) as tipo_atendimento,
+      cast({{ process_null("json_extract_scalar(data,'$.estado_marcacao')") }} as string) as estado_marcacao,
+      cast({{ process_null("json_extract_scalar(data,'$.unidade_cnes')") }} as string) as id_cnes,
+      cast({{ process_null("json_extract_scalar(data,'$.ut_id')") }} as string) as ut_id,
 
-      cast(datalake_loaded_at as date) as data_particao
+      cast({{ process_null('datalake_loaded_at') }} as date) as data_particao
     from source_agendamento
   ),
 
